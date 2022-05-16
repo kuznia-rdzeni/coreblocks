f61666cf99b9f94836aa0e43c0ff1125e2f73ae9
Michal Opanowicz <michal.opanowicz@gmail.com>
2022-05-16 18:47:33 +0200

    working and implemented roughly as intended

--- coreblocks/reorder_buffer.py
+++ coreblocks/reorder_buffer.py
@@ -2,15 +2,27 @@ from . import transactions as ts
 from .transactions import lib as tl
 from amaranth import *
 
+log_rob_len = 7
+log_reg_cnt = 5
+log_phys_reg_cnt = 8
 
 in_layout = [
-    ("dst_reg", 5),
-    ("phys_dst_reg", 8),
+    ("dst_reg", log_reg_cnt),
+    ("phys_dst_reg", log_phys_reg_cnt),
+]
+
+id_layout = [
+    ("rob_id", log_rob_len),
+]
+
+out_layout = [
+    ("dst_reg", log_rob_len),
+    ("phys_dst_reg", log_phys_reg_cnt),
 ]
 
 internal_layout = [
-    ("dst_reg", 5),
-    ("phys_dst_reg", 8),
+    ("dst_reg", log_rob_len),
+    ("phys_dst_reg", log_phys_reg_cnt),
     ("done", 1),
 ]
 
@@ -19,37 +31,33 @@ class ReorderBuffer(Elaboratable):
     def __init__(self) -> None:
         super().__init__()
 
-        self.put = ts.Method(i=in_layout)
-        self.mark_done = ts.Method(i=in_layout)
+        self.put = ts.Method(i=in_layout, o=id_layout)
+        self.mark_done = ts.Method(i=id_layout)
         self.retire = ts.Method(o=in_layout)
-        self.data = Array(Record(internal_layout) for _ in range(128))
+        self.data = Array(Record(internal_layout) for _ in range(2**log_rob_len))
 
-        self.cnt = Signal(7)
+        self.start_cnt = Signal(log_rob_len)
+        self.end_cnt = Signal(log_rob_len)
 
     def elaborate(self, platform) -> Module:
         m = Module()
 
-        put_possible = self.cnt < 128
-
-        retiring = Signal(1)
+        put_possible = self.end_cnt != self.start_cnt - 1
 
         output = Record(in_layout)
 
-        with self.retire.body(m, ready=self.data[0].done, out=output):
-            m.d.comb += output.eq(self.data[0])
-            for i in range(127):
-                m.d.sync += self.data[i].eq(self.data[i + 1])
-            # m.d.sync += self.data[:-1].eq(self.data[1:])
-            m.d.sync += self.cnt.eq(self.cnt - 1)
-            m.d.comb += retiring.eq(1)
+        with self.retire.body(m, ready=self.data[self.start_cnt].done, out=output):
+            m.d.comb += output.eq(self.data[self.start_cnt])
+            m.d.sync += self.start_cnt.eq(self.start_cnt + 1)
 
-        with self.put.body(m, ready=put_possible & (retiring == 0)) as arg:
-            m.d.sync += self.data[self.cnt].eq(arg)
-            m.d.sync += self.cnt.eq(self.cnt + 1)
+        put_output = Record(id_layout)
 
-        with self.mark_done.body(m, ready=retiring == 0) as arg:
-            for i in range(128):
-                with m.If(self.data[i].dst_reg == arg.dst_reg):
-                    m.d.sync += self.data[i].done.eq(1)
+        with self.put.body(m, ready=put_possible, out=put_output) as arg:
+            m.d.sync += self.data[self.end_cnt].eq(arg)
+            m.d.sync += self.end_cnt.eq(self.end_cnt + 1)
+            m.d.comb += put_output.rob_id.eq(self.end_cnt)
+
+        with self.mark_done.body(m) as arg:
+            m.d.sync += self.data[arg.rob_id].done.eq(1)
 
         return m
