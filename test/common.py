a89f2ae2c6bbeacc18591846c35186c9ca2bdd26
Kristopher38 <kristopher38@wp.pl>
2022-05-16 12:15:38 +0200

    Merge TestbenchIO into scheduler

--- test/common.py
+++ test/common.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 
 from amaranth import *
 from amaranth.sim import *
+from coreblocks.transactions.lib import AdapterTrans
 
 class TestCaseWithSimulator(unittest.TestCase):
     @contextmanager
@@ -11,3 +12,59 @@ class TestCaseWithSimulator(unittest.TestCase):
         yield sim
         with sim.write_vcd("test.vcd", "test.gtkw"):
             sim.run()
+
+class TestbenchIO(Elaboratable):
+    def __init__(self, *args, **kwargs):
+        self.adapter = AdapterTrans(*args, **kwargs)
+
+    def elaborate(self, platform):
+        m = Module()
+        m.submodules += self.adapter
+        return m
+
+    def _enable(self):
+        yield self.adapter.en.eq(1)
+
+    def _disable(self):
+        yield self.adapter.en.eq(0)
+
+    def _wait_until_done(self):
+        while (yield self.adapter.done) != 1:
+            yield
+
+    def _set_inputs(self, values: dict, field=None):
+        if field is None:
+            field = self.adapter.data_in
+        for name, value in values.items():
+            if isinstance(value, dict):
+                yield from self._set_inputs(value, getattr(field, name))
+            else:
+                yield getattr(field, name).eq(value)
+
+    def _get_outputs(self, field=None):
+        if field is None:
+            field = self.adapter.data_out
+        if isinstance(field, Signal):
+            return (yield field)
+        else: # field is a Record
+            result = {}
+            for name, bits, _ in field.layout:
+                result[name] = yield from self._get_outputs(getattr(field, name))
+            return result
+
+    # accept data as dict to be consistent with get()
+    def put(self, data: dict):
+        yield from self._enable()
+        yield from self._set_inputs(data)
+        yield
+        yield from self._wait_until_done()
+        yield from self._disable()
+
+    def get(self):
+        yield from self._enable()
+        yield
+        yield from self._wait_until_done()
+        yield from self._disable()
+        # return dict of all signal values in a record because amaranth doesn't support yielding records -
+        # - they're neither Value nor Statement and amaranth implementation only supports these two
+        return (yield from self._get_outputs())
