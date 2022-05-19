0efbbc8abfcb0def641366a4191a069d6275c98c
Marek Materzok <tilk@tilk.eu>
2022-05-19 16:11:21 +0200

    More types for checking

---
+++ coreblocks/_typing.py
@@ -0,0 +1,11 @@
+from typing import Union, List, Tuple, Type, Any
+from enum import Enum
+from amaranth import *
+from amaranth.hdl.ast import ValueCastable
+from amaranth.hdl.rec import Direction, Layout
+
+FragmentLike = Union[Fragment, Elaboratable]
+ValueLike = Union[Value, int, Enum, ValueCastable]
+ShapeLike = Union[Shape, int, range, Type[Enum]]
+# TODO: recursive definition: LayoutLike instead of Any
+LayoutLike = Union[Layout, List[Union[Tuple[str, Union[ShapeLike, Any]], Tuple[str, Union[ShapeLike, Any], Direction]]]]
