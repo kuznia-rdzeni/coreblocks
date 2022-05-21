e6a8323130761e487deb52e9bc4bb53ec02e3cd5
Szpila123 <bartoszszpila98@gmail.com>
2022-05-21 21:48:08 +0200

    pre-commit-configuration: configured pre-commit for repository

---
+++ docs/Development_environment.md
@@ -0,0 +1,8 @@
+# Development environment
+
+In order to prepare development environment, please follow steps below:
+
+1. Install python 3.10 interpreter and pip package manager
+    * Optionally create python virtual environment with `python -m venv venv` in project directory
+2. Install all required libraries with `pip install -r requirements-dev.txt`
+3. Install all precommit hooks with `pre-commit install`
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,7 +5,7 @@ MAX_LINE_LENGTH=120
 prog_name=$(basename $0)
   
 sub_help(){
-    echo "Usage: $prog_name [subcommand]\n"
+    echo "Usage: $prog_name subcommand [filename ...]\n"
     echo "Subcommands:"
     echo "    format   Format the code"
     echo "    verify   Verify formatting without making any changes"
@@ -15,32 +15,35 @@ sub_help(){
 sub_verify() {
     python3 -m flake8 \
       --max-line-length=$MAX_LINE_LENGTH \
-      --extend-ignore=F401,F403,F405,E203 .
-    exit $?
+      --extend-ignore=F401,F403,F405,E203 $@
 }
 
 sub_format(){
     python3 -m black \
-      --line-length $MAX_LINE_LENGTH .
+      --line-length $MAX_LINE_LENGTH $@
 
     sub_verify
 }
   
 subcommand=$1
 case $subcommand in
-    "")
-        sub_format
-        ;;
-    "-h" | "--help")
+    "" | "-h" | "--help")
         sub_help
         ;;
     *)
         shift
-        sub_${subcommand} $@
-        if [ $? = 127 ]; then
+
+        FILES=${@:-"."}
+        sub_${subcommand} $FILES
+        RETVAL=$?
+
+        if [ $RETVAL = 127 ]; then
             echo "Error: '$subcommand' is not a known subcommand." >&2
             echo "       Run '$prog_name --help' for a list of known subcommands." >&2
-            exit 1
         fi
+
         ;;
 esac
+
+RETVAL=${RETVAL:=$?}
+exit $RETVAL
