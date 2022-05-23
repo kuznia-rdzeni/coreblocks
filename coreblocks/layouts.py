ea247491b936c264c2230de664beb76ee63c2101
Michal Opanowicz <michal.opanowicz@gmail.com>
2022-05-24 00:09:07 +0200

    upgraded to work with the new master, hopefully fixed bugs

---
+++ coreblocks/layouts.py
@@ -0,0 +1,21 @@
+from coreblocks import genparams
+from .genparams import GenParams
+
+
+class ROBLayouts:
+    def __init__(self, gen_params: GenParams):
+        self.data_layout = [
+            ("rl_dst", gen_params.log_regs_bits),
+            ("rp_dst", gen_params.phys_regs_bits),
+        ]
+
+        self.id_layout = [
+            ("rob_id", gen_params.rob_entries_bits),
+        ]
+
+        self.internal_layout = [
+            ("rob_data", self.data_layout),
+            ("done", 1),
+        ]
+
+
--- test/test_reorder_buffer.py
+++ test/test_reorder_buffer.py
@@ -2,10 +2,13 @@ from amaranth import *
 from amaranth.sim import *
 
 from coreblocks.transactions.core import TransactionModule
+from coreblocks.transactions.lib import AdapterTrans
 
 from .common import TestCaseWithSimulator, TestbenchIO
 
-from coreblocks.reorder_buffer import ReorderBuffer, in_layout, id_layout
+from coreblocks.reorder_buffer import ReorderBuffer
+from coreblocks.layouts import ROBLayouts
+from coreblocks.genparams import GenParams
 
 from queue import Queue
 from random import randint, seed
@@ -13,13 +16,16 @@ from random import randint, seed
 
 class TestElaboratable(Elaboratable):
     def elaborate(self, platform):
+        gp = GenParams()
         m = Module()
         tm = TransactionModule(m)
+
         with tm.transactionContext():
-            rb = ReorderBuffer()
-            self.io_in = TestbenchIO(rb.put, i=in_layout, o=id_layout)
-            self.io_update = TestbenchIO(rb.mark_done, i=id_layout)
-            self.io_out = TestbenchIO(rb.retire, o=in_layout)
+            rb = ReorderBuffer(gp)
+
+            self.io_in = TestbenchIO(AdapterTrans(rb.put))
+            self.io_update = TestbenchIO(AdapterTrans(rb.mark_done))
+            self.io_out = TestbenchIO(AdapterTrans(rb.retire))
 
             m.submodules.rb = rb
             m.submodules.io_in = self.io_in
@@ -38,8 +44,8 @@ class TestReorderBuffer(TestCaseWithSimulator):
                 log_reg = randint(0, 31)
                 phys_reg = self.regs_left_queue.get()
                 # print(log_reg, phys_reg)
-                regs = {"dst_reg": log_reg, "phys_dst_reg": phys_reg}
-                rob_id = yield from self.m.io_in.putget(regs)
+                regs = {"rl_dst": log_reg, "rp_dst": phys_reg}
+                rob_id = yield from self.m.io_in.call(regs)
                 self.to_execute_list.append((rob_id, phys_reg))
                 self.retire_queue.put(regs)
 
@@ -53,7 +59,7 @@ class TestReorderBuffer(TestCaseWithSimulator):
                 idx = randint(0, len(self.to_execute_list) - 1)
                 rob_id, executed = self.to_execute_list.pop(idx)
                 self.executed_list.append(executed)
-                yield from self.m.io_update.put(rob_id)
+                yield from self.m.io_update.call(rob_id)
 
     def do_retire(self):
         cnt = 0
@@ -62,9 +68,9 @@ class TestReorderBuffer(TestCaseWithSimulator):
                 yield
             else:
                 regs = self.retire_queue.get()
-                results = yield from self.m.io_out.get()
+                results = yield from self.m.io_out.call()
                 # print(results)
-                phys_reg = results["phys_dst_reg"]
+                phys_reg = results["rp_dst"]
                 assert phys_reg in self.executed_list
                 self.executed_list.remove(phys_reg)
                 # print(self.executed_list)
@@ -85,8 +91,8 @@ class TestReorderBuffer(TestCaseWithSimulator):
         self.to_execute_list = []
         self.executed_list = []
         self.retire_queue = Queue()
-
-        for i in range(128):
+        gp = GenParams()
+        for i in range(2**gp.phys_regs_bits):
             self.regs_left_queue.put(i)
 
         seed(0)
