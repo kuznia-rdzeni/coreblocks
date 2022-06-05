69d549cc4b9d572dc80bb6116cb2cfd581ffb701
lekcyjna <309016@uwr.edu.pl>
2022-06-05 16:04:01 +0200

    Renaming of functions in script.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,31 +7,31 @@ prog_name=$(basename $0)
 sub_help(){
     echo "Usage: $prog_name subcommand [filename ...]\n"
     echo "Subcommands:"
-    echo "    format          Format the code"
-    echo "    verify          Verify formatting without making any changes"
-    echo "    verify_flake8   Verify formatting using flake8 only"
-    echo "    verify_black    Verify formatting using black only"
-    echo "    types           Verify typing"
-    echo "    all             Run all checks"
+    echo "    format                Format the code"
+    echo "    check_format          Verify formatting without making any changes"
+    echo "    check_format_flake8   Verify formatting using flake8 only"
+    echo "    check_format_black    Verify formatting using black only"
+    echo "    check_types           Verify typing"
+    echo "    verify                Run all checks"
     echo ""
 }
 
-sub_verify_flake8() {
+sub_check_format_flake8() {
     python3 -m flake8 \
       --max-line-length=$MAX_LINE_LENGTH \
       --exclude ".env,.venv,env,venv,ENV,env.bak,venv.bak,ci,stubs" \
       --extend-ignore=F401,F403,F405,E203 $@
 }
 
-sub_verify_black() {
+sub_check_format_black() {
     python3 -m black \
         --line-length $MAX_LINE_LENGTH \
         --extend-exclude "stubs|ci" $@ \
         --check $@
 }
 
-sub_verify() {
-    sub_verify_flake8 $@ && sub_verify_black $@
+sub_check_format() {
+    sub_check_format_flake8 $@ && sub_check_format_black $@
 }
 
 sub_format(){
@@ -39,16 +39,16 @@ sub_format(){
       --line-length $MAX_LINE_LENGTH \
       --extend-exclude "stubs|ci" $@
 
-    sub_verify_flake8 $@
+    sub_check_format_flake8 $@
 }
 
-sub_types(){
+sub_check_types(){
     pyright
 }
 
-sub_all(){
-  sub_format $@
-  sub_types
+sub_verify(){
+  sub_check_format $@
+  sub_check_types
 }
 
 subcommand=$1
