7a8a37bd70fe80cb8011a7354b63b0acb6e4aecd
Wojciech Jasiński <ilikeheaps@gmail.com>
2022-06-23 13:02:31 +0200

    Format code

--- test/test_adapter.py
+++ test/test_adapter.py
@@ -7,78 +7,82 @@ from coreblocks.transactions.lib import AdapterTrans
 
 from .common import TestCaseWithSimulator, TestbenchIO
 
+
 class Echo(Elaboratable):
-  def __init__(self):
-      self.data_bits = 8
+    def __init__(self):
+        self.data_bits = 8
 
-      self.layout_in = [("data", self.data_bits)]
-      self.layout_out = [("data", self.data_bits)]
+        self.layout_in = [("data", self.data_bits)]
+        self.layout_out = [("data", self.data_bits)]
 
-      self.action = Method(i = self.layout_in, o = self.layout_out)
+        self.action = Method(i=self.layout_in, o=self.layout_out)
 
-  def elaborate(self, platform):
-      m = Module()
+    def elaborate(self, platform):
+        m = Module()
 
-      # because amaranth requires synchronous elements when using clock
-      dummySync = Signal()
-      m.d.sync += dummySync.eq(dummySync + 1)
+        # because amaranth requires synchronous elements when using clock
+        dummySync = Signal()
+        m.d.sync += dummySync.eq(dummySync + 1)
 
-      @def_method(m, self.action, ready=C(1))
-      def _(arg):
-        return arg
+        @def_method(m, self.action, ready=C(1))
+        def _(arg):
+            return arg
 
-      return m
+        return m
+
 
 class Consumer(Elaboratable):
-  def __init__(self):
-      self.data_bits = 8
+    def __init__(self):
+        self.data_bits = 8
 
-      self.layout_in = [("data", self.data_bits)]
-      self.layout_out = []
+        self.layout_in = [("data", self.data_bits)]
+        self.layout_out = []
 
-      self.action = Method(i = self.layout_in, o = self.layout_out)
+        self.action = Method(i=self.layout_in, o=self.layout_out)
 
-  def elaborate(self, platform):
-      m = Module()
+    def elaborate(self, platform):
+        m = Module()
 
-      # because amaranth requires synchronous elements when using clock
-      dummySync = Signal()
-      m.d.sync += dummySync.eq(dummySync + 1)
+        # because amaranth requires synchronous elements when using clock
+        dummySync = Signal()
+        m.d.sync += dummySync.eq(dummySync + 1)
 
-      @def_method(m, self.action, ready=C(1))
-      def _(arg):
-        return C(0)
+        @def_method(m, self.action, ready=C(1))
+        def _(arg):
+            return C(0)
 
-      return m
+        return m
+
 
 class TestElaboratable(Elaboratable):
-  def __init__(self):
-    self.m = Module()
-    self.tm = TransactionModule(self.m)
-    
-    self.echo = Echo()
-    self.consumer = Consumer()
-    self.io_echo = TestbenchIO(AdapterTrans(self.echo.action))
-    self.io_consume = TestbenchIO(AdapterTrans(self.consumer.action))
-    
-  def elaborate(self, platform):
-    m = self.m
+    def __init__(self):
+        self.m = Module()
+        self.tm = TransactionModule(self.m)
 
-    m.submodules.echo = self.echo
-    m.submodules.io_echo = self.io_echo
-    m.submodules.consumer = self.consumer
-    m.submodules.io_consume = self.io_consume
+        self.echo = Echo()
+        self.consumer = Consumer()
+        self.io_echo = TestbenchIO(AdapterTrans(self.echo.action))
+        self.io_consume = TestbenchIO(AdapterTrans(self.consumer.action))
 
-    return self.tm
+    def elaborate(self, platform):
+        m = self.m
+
+        m.submodules.echo = self.echo
+        m.submodules.io_echo = self.io_echo
+        m.submodules.consumer = self.consumer
+        m.submodules.io_consume = self.io_consume
+
+        return self.tm
+
 
 class TestAdapterTrans(TestCaseWithSimulator):
     def proc(self):
         for _ in range(3):
-          # this would previously timeout if the output layout was empty (as is in this case)
-          yield from self.t.io_consume.call()
+            # this would previously timeout if the output layout was empty (as is in this case)
+            yield from self.t.io_consume.call()
         for expected in [4, 1, 0]:
-          obtained = (yield from self.t.io_echo.call({"data": expected}))["data"]
-          self.assertEqual(expected, obtained)
+            obtained = (yield from self.t.io_echo.call({"data": expected}))["data"]
+            self.assertEqual(expected, obtained)
 
     def test_single(self):
         self.t = t = TestElaboratable()
