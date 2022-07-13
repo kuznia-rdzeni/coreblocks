a18294b457ff49a863662192cfedbe648b1c7bda
Kristopher38 <kristopher38@wp.pl>
2022-07-13 21:19:15 +0200

    Scheduler layout changes in preparation for integration

--- coreblocks/layouts.py
+++ coreblocks/layouts.py
@@ -31,47 +31,63 @@ class CommonLayouts:
 
 class SchedulerLayouts:
     def __init__(self, gen_params: GenParams):
+        common = gen_params.get(CommonLayouts)
         self.reg_alloc_in = [
-            ("rl_s1", gen_params.isa.reg_cnt_log),
-            ("rl_s2", gen_params.isa.reg_cnt_log),
-            ("rl_dst", gen_params.isa.reg_cnt_log),
-            # Note: using ilen here and in the following records is currently a placeholder
-            ("opcode", gen_params.isa.ilen),
+            ("opcode", Opcode),
+            ("illegal", 1),
+            ("exec_fn", common.exec_fn),
+            ("regs_l", common.regs_l),
+            ("imm", gen_params.isa.xlen),
         ]
         self.reg_alloc_out = self.renaming_in = [
-            ("rl_s1", gen_params.isa.reg_cnt_log),
-            ("rl_s2", gen_params.isa.reg_cnt_log),
-            ("rl_dst", gen_params.isa.reg_cnt_log),
-            ("rp_dst", gen_params.phys_regs_bits),
-            ("opcode", gen_params.isa.ilen),
+            ("opcode", Opcode),
+            ("illegal", 1),
+            ("exec_fn", common.exec_fn),
+            ("regs_l", common.regs_l),
+            ("regs_p", [
+                ("rp_dst", gen_params.phys_regs_bits)
+            ]),
+            ("imm", gen_params.isa.xlen),
         ]
         self.renaming_out = self.rob_allocate_in = [
-            ("rp_s1", gen_params.phys_regs_bits),
-            ("rp_s2", gen_params.phys_regs_bits),
-            ("rl_dst", gen_params.isa.reg_cnt_log),
-            ("rp_dst", gen_params.phys_regs_bits),
-            ("opcode", gen_params.isa.ilen),
+            ("opcode", Opcode),
+            ("illegal", 1),
+            ("exec_fn", common.exec_fn),
+            ("regs_l", [
+                ("rl_dst", gen_params.isa.reg_cnt_log),
+                ("rl_dst_v", 1),
+            ]),
+            ("regs_p", [
+                ("rp_dst", gen_params.phys_regs_bits),
+                ("rp_s1", gen_params.phys_regs_bits),
+                ("rp_s2", gen_params.phys_regs_bits),
+            ]),
+            ("imm", gen_params.isa.xlen),
         ]
         self.rob_allocate_out = self.rs_select_in = [
-            ("rp_s1", gen_params.phys_regs_bits),
-            ("rp_s2", gen_params.phys_regs_bits),
-            ("rp_dst", gen_params.phys_regs_bits),
+            ("opcode", Opcode),
+            ("illegal", 1),
+            ("exec_fn", common.exec_fn),
+            ("regs_p", [
+                ("rp_dst", gen_params.phys_regs_bits),
+                ("rp_s1", gen_params.phys_regs_bits),
+                ("rp_s2", gen_params.phys_regs_bits),
+            ]),
             ("rob_id", gen_params.rob_entries_bits),
-            ("opcode", gen_params.isa.ilen),
+            ("imm", gen_params.isa.xlen),
         ]
         self.rs_select_out = self.rs_insert_in = [
-            ("rp_s1", gen_params.phys_regs_bits),
-            ("rp_s2", gen_params.phys_regs_bits),
-            ("rp_dst", gen_params.phys_regs_bits),
+            ("opcode", Opcode),
+            ("illegal", 1),
+            ("exec_fn", common.exec_fn),
+            ("regs_p", [
+                ("rp_dst", gen_params.phys_regs_bits),
+                ("rp_s1", gen_params.phys_regs_bits),
+                ("rp_s2", gen_params.phys_regs_bits),
+            ]),
             ("rob_id", gen_params.rob_entries_bits),
-            ("opcode", gen_params.isa.ilen),
             ("rs_entry_id", gen_params.rs_entries_bits),
-        ]
-        self.instr_layout = [
-            ("rl_s1", gen_params.isa.reg_cnt_log),
-            ("rl_s2", gen_params.isa.reg_cnt_log),
-            ("rl_dst", gen_params.isa.reg_cnt_log),
-            ("opcode", gen_params.isa.ilen),
+            ("imm", gen_params.isa.xlen),
         ]
 
 
