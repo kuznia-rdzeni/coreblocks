f297259908b0287bf7ddd4b9ad178272dfa0b816
wiktorkuchta <35867657+wiktorkuchta@users.noreply.github.com>
2022-07-13 12:47:25 +0200

    Basic wakeup/select (#62)

---
+++ coreblocks/wakeup_select.py
@@ -0,0 +1,26 @@
+from amaranth import *
+
+from coreblocks.transactions.core import *
+
+__all__ = ["WakeupSelect"]
+
+
+class WakeupSelect(Elaboratable):
+    def __init__(self, *, get_ready: Method, take_row: Method, issue: Method):
+        self.get_ready = get_ready  # assumption: ready only if nonzero result
+        self.take_row = take_row
+        self.issue = issue
+
+    def elaborate(self, platform):
+        m = Module()
+
+        with Transaction().body(m):
+            ready = self.get_ready(m)
+            ready_width = len(ready)
+            last = Signal(range(ready_width))
+            for i in range(ready_width):
+                with m.If(ready[i]):
+                    m.d.comb += last.eq(i)
+            self.issue(m, self.take_row(m, last))
+
+        return m
