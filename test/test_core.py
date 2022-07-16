5b319a5aa294fbf4431aa29b387f878bf629847b
Kristopher38 <kristopher38@wp.pl>
2022-07-16 23:00:57 +0200

    Simplify and fix RF write, simplify free reg fifo insertion logic

--- test/test_core.py
+++ test/test_core.py
@@ -24,13 +24,15 @@ class TestElaboratable(Elaboratable):
 
         self.fifo_in = FIFO(self.gp.get(FetchLayouts).raw_instr, 2)
         self.core = Core(gen_params=self.gp, get_raw_instr=self.fifo_in.read)
-        self.reg_feed_in = TestbenchIO(AdapterTrans(self.core.put_reg))
+        self.reg_feed_in = TestbenchIO(AdapterTrans(self.core.free_rf_fifo.write))
         self.io_in = TestbenchIO(AdapterTrans(self.fifo_in.write))
+        self.rf_write = TestbenchIO(AdapterTrans(self.core.RF.write))
 
         m.submodules.fifo_in = self.fifo_in
         m.submodules.reg_feed_in = self.reg_feed_in
         m.submodules.c = self.core
         m.submodules.io_in = self.io_in
+        m.submodules.rf_write = self.rf_write
 
         return tm
 
@@ -50,7 +52,7 @@ class TestCore(TestCaseWithSimulator):
         # as I am not sure if we support OpImm instructions or not.
 
         for i in range(2**self.m.gp.phys_regs_bits - 1):
-            yield from self.m.reg_feed_in.call({"reg": i + 1})
+            yield from self.m.reg_feed_in.call({"data": i + 1})
 
         # provoking allocation of physical register
         for i in range(self.m.gp.isa.reg_cnt - 1):
@@ -67,9 +69,9 @@ class TestCore(TestCaseWithSimulator):
             print("r", i, (yield self.m.core.RRAT.entries[i]))
 
         # writing values to physical registers
-        yield self.m.core.RF.entries[(yield self.m.core.RRAT.entries[1])].reg_val.eq(1)
-        yield self.m.core.RF.entries[(yield self.m.core.RRAT.entries[2])].reg_val.eq(2)
-        yield self.m.core.RF.entries[(yield self.m.core.RRAT.entries[3])].reg_val.eq(3)
+        yield from self.m.rf_write.call({"reg_id": (yield self.m.core.RRAT.entries[1]), "reg_val": 1})
+        yield from self.m.rf_write.call({"reg_id": (yield self.m.core.RRAT.entries[2]), "reg_val": 2})
+        yield from self.m.rf_write.call({"reg_id": (yield self.m.core.RRAT.entries[3]), "reg_val": 3})
 
         for i in range(self.m.gp.isa.reg_cnt):
             print(i, (yield self.m.core.RF.entries[(yield self.m.core.RRAT.entries[i])].reg_val))
