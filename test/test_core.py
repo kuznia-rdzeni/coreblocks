3d818348bb5f646fbe22bbca23b6cb297b5d66af
Kristopher38 <kristopher38@wp.pl>
2022-07-18 11:49:16 +0200

    Replace debug prints with assertions

--- test/test_core.py
+++ test/test_core.py
@@ -42,57 +42,83 @@ def gen_riscv_add_instr(dst, src1, src2):
 
 
 class TestCore(TestCaseWithSimulator):
-    def run_test(self):
+    def check_RAT_alloc(self, rat, expected_alloc_count=None):
+        allocated = []
+        for i in range(self.m.gp.isa.reg_cnt):
+            allocated.append((yield rat.entries[i]))
+        filtered_zeros = list(filter(lambda x: x != 0, allocated))
 
+        # check if 0th register is set to 0
+        self.assertEqual(allocated[0], 0)
+        # check if there are no duplicate physical registers allocated for two different architectural registers
+        self.assertEqual(len(filtered_zeros), len(set(filtered_zeros)))
+        # check if the expected number of allocated registers matches reality
+        if expected_alloc_count:
+            self.assertEqual(len(filtered_zeros), expected_alloc_count)
+
+    def get_phys_reg_rrat(self, reg_id):
+        return (yield self.m.core.RRAT.entries[reg_id])
+
+    def get_phys_reg_frat(self, reg_id):
+        return (yield self.m.core.FRAT.entries[reg_id])
+
+    def get_arch_reg_val(self, reg_id):
+        return (yield self.m.core.RF.entries[(yield from self.get_phys_reg_rrat(reg_id))].reg_val)
+
+    def get_phys_reg_val(self, reg_id):
+        return (yield self.m.core.RF.entries[reg_id].reg_val)
+
+    def push_instr(self, opcode):
+        yield from self.m.io_in.call({"data": opcode})
+
+    def run_test(self):
         # this test first provokes allocation of physical registers,
         # then sets the values in those registers, and finally runs
         # an actual computation.
 
-        # The test sets values in the reg file by hand,
-        # as I am not sure if we support OpImm instructions or not.
+        # The test sets values in the reg file by hand
 
         for i in range(2**self.m.gp.phys_regs_bits - 1):
             yield from self.m.reg_feed_in.call({"data": i + 1})
 
         # provoking allocation of physical register
         for i in range(self.m.gp.isa.reg_cnt - 1):
-            yield from self.m.io_in.call({"data": gen_riscv_add_instr(i + 1, 0, 0)})
+            yield from self.push_instr(gen_riscv_add_instr(i + 1, 0, 0))
 
         # waiting for the retirement rat to be set
-        for i in range(100):
+        for i in range(50):
             yield
 
-        for i in range(self.m.gp.isa.reg_cnt):
-            print("f", i, (yield self.m.core.FRAT.entries[i]))
-
-        for i in range(self.m.gp.isa.reg_cnt):
-            print("r", i, (yield self.m.core.RRAT.entries[i]))
+        # checking if all registers have been allocated
+        yield from self.check_RAT_alloc(self.m.core.FRAT, 31)
+        yield from self.check_RAT_alloc(self.m.core.RRAT, 31)
 
         # writing values to physical registers
-        yield from self.m.rf_write.call({"reg_id": (yield self.m.core.RRAT.entries[1]), "reg_val": 1})
-        yield from self.m.rf_write.call({"reg_id": (yield self.m.core.RRAT.entries[2]), "reg_val": 2})
-        yield from self.m.rf_write.call({"reg_id": (yield self.m.core.RRAT.entries[3]), "reg_val": 3})
+        yield from self.m.rf_write.call({"reg_id": (yield from self.get_phys_reg_rrat(1)), "reg_val": 1})
+        yield from self.m.rf_write.call({"reg_id": (yield from self.get_phys_reg_rrat(2)), "reg_val": 2})
+        yield from self.m.rf_write.call({"reg_id": (yield from self.get_phys_reg_rrat(3)), "reg_val": 3})
 
-        yield Settle()
-        for i in range(self.m.gp.isa.reg_cnt):
-            print(i, (yield self.m.core.RF.entries[(yield self.m.core.RRAT.entries[i])].reg_val))
-
-        # issuing actual instructions for the test
-        yield from self.m.io_in.call({"data": gen_riscv_add_instr(4, 1, 2)})
-        yield from self.m.io_in.call({"data": gen_riscv_add_instr(4, 3, 4)})
-
-        # waiting for the instructions to be processed
-        for i in range(100):
+        # waiting for potential conflicts on rf_write
+        for i in range(10):
             yield
 
-        for i in range(self.m.gp.isa.reg_cnt):
-            print(i, (yield self.m.core.RF.entries[(yield self.m.core.RRAT.entries[i])].reg_val))
+        self.assertEqual((yield from self.get_arch_reg_val(1)), 1)
+        self.assertEqual((yield from self.get_arch_reg_val(2)), 2)
+        self.assertEqual((yield from self.get_arch_reg_val(3)), 3)
 
-        for i in range(10):
-            print((yield self.m.core.RRAT.entries[i]))
+        # issuing actual instructions for the test
+        yield from self.push_instr(gen_riscv_add_instr(4, 1, 2))
+        yield from self.push_instr(gen_riscv_add_instr(4, 3, 4))
 
+        # waiting for the instructions to be processed
+        for i in range(50):
+            yield
+
+        self.assertEqual((yield from self.get_arch_reg_val(1)), 1)
+        self.assertEqual((yield from self.get_arch_reg_val(2)), 2)
+        self.assertEqual((yield from self.get_arch_reg_val(3)), 3)
         # 1 + 2 + 3 = 6
-        self.assertEqual((yield self.m.core.RF.entries[(yield self.m.core.RRAT.entries[4])].reg_val), 6)
+        self.assertEqual((yield from self.get_arch_reg_val(4)), 6)
 
     def test_single(self):
         gp = GenParams("rv32i", phys_regs_bits=6, rob_entries_bits=7)
