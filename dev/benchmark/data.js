19d066d14c10ca59d6a8006f8a15f17036cb2c35
github-action-benchmark <github@users.noreply.github.com>
2022-11-19 18:18:26 +0000

    add Fmax and LCs (customBiggerIsBetter) benchmark result for f83c2e6dc7f9704180ef659e96a1ecf192a919ad

---
+++ dev/benchmark/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1668881906296,
+  "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
+  "entries": {
+    "Fmax and LCs": [
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f83c2e6dc7f9704180ef659e96a1ecf192a919ad",
+          "message": "Merge branch 'master' into benchmarks-ci",
+          "timestamp": "2022-11-13T15:34:18+01:00",
+          "tree_id": "932867cdcbe4b343231eff018fda2cdc7724eb30",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/f83c2e6dc7f9704180ef659e96a1ecf192a919ad"
+        },
+        "date": 1668881904680,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": "119.79",
+            "unit": "MHZ"
+          },
+          {
+            "name": "Device utilisation: (ICE40)",
+            "value": "119.79",
+            "unit": "LCs"
+          }
+        ]
+      }
+    ]
+  }
+}
