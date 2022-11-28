979fb958575bd0c79b10062b7542b8e07ab0e62a
Marek Bauer <marekbauer07@gmail.com>
2022-11-28 20:59:00 +0100

    fixed docs strings + enabled 1 clock pipelined multiplication (#106)

--- coreblocks/genparams.py
+++ coreblocks/genparams.py
@@ -27,7 +27,7 @@ class GenParams(DependentCache):
         rob_entries_bits: int = 8,
         rs_entries: int = 4,
         start_pc: int = 0,
-        mul_unit_params: MulUnitParams = MulUnitParams.ShiftMultiplicator()
+        mul_unit_params: MulUnitParams = MulUnitParams.ShiftMultiplier()
     ):
         super().__init__()
         self.isa = ISA(isa_str)
