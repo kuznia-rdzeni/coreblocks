ee0364c4313175c558d5815f968df34839a682ce
Marek Materzok <tilk@tilk.eu>
2022-12-21 09:38:38 +0100

    Remove bad data from testing synthesis CI

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -16,40 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "f83c2e6dc7f9704180ef659e96a1ecf192a919ad",
-          "message": "Merge branch 'master' into benchmarks-ci",
-          "timestamp": "2022-11-13T15:34:18+01:00",
-          "tree_id": "932867cdcbe4b343231eff018fda2cdc7724eb30",
-          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/f83c2e6dc7f9704180ef659e96a1ecf192a919ad"
-        },
-        "date": 1668881904680,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "Max clock frequency (Fmax)",
-            "value": "119.79",
-            "unit": "MHZ"
-          },
-          {
-            "name": "Device utilisation: (ICE40)",
-            "value": "119.79",
-            "unit": "LCs"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "tilk@tilk.eu",
-            "name": "Marek Materzok",
-            "username": "tilk"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
           "id": "759a5c0183898735e822d5ef21b2329ada3972bf",
           "message": "Fix benchmark action (#153)",
           "timestamp": "2022-12-20T10:16:57+01:00",
