880a8ada6d314c2649e7516a9305732bdcc4b9ed
Marek Bauer <marekbauer07@gmail.com>
2023-01-31 18:53:12 +0100

    import fix (#207)

--- coreblocks/params/fu_params.py
+++ coreblocks/params/fu_params.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Iterable, Any
+from typing import Iterable
 
 import coreblocks.params.genparams as gp
 import coreblocks.params.optypes as optypes
