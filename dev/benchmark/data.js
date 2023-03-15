abdc0e999ad64fc68220613bbaac9c6f4428504c
github-action-benchmark <github@users.noreply.github.com>
2023-03-15 09:13:31 +0000

    add Fmax and LCs (customBiggerIsBetter) benchmark result for 1f5064a3f796015e2425e597220b2f6f63745afe

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678871476705,
+  "lastUpdate": 1678871611191,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Fmax and LCs": [
@@ -2865,6 +2865,55 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/ce7cfb53f43a18b7c59a31f0df17488e2d13d82d"
         },
         "date": 1678871475368,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 56.63,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 17461,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 480,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 812,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 5453,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1f5064a3f796015e2425e597220b2f6f63745afe",
+          "message": "Make Sphinx CI fail on errors and warnings (#241)",
+          "timestamp": "2023-03-15T10:07:21+01:00",
+          "tree_id": "8f0fdf9ec0c88770ca066c3e386ed10237ef6726",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/1f5064a3f796015e2425e597220b2f6f63745afe"
+        },
+        "date": 1678871609186,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
