1f5064a3f796015e2425e597220b2f6f63745afe
Marek Materzok <tilk@tilk.eu>
2023-03-15 10:07:21 +0100

    Make Sphinx CI fail on errors and warnings (#241)

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 ROOT_PATH=$(dirname $0)/..
 
 # Add coreblocks to PYTHONPATH for sphinx-apidoc
@@ -57,4 +60,4 @@ $ROOT_PATH/scripts/core_graph -p -f mermaid $DOCS_DIR/auto_graph.rst
 sed -i -e '1i\.. mermaid::\n' -e 's/^/   /' $DOCS_DIR/auto_graph.rst
 
 sphinx-apidoc -o $DOCS_DIR $ROOT_PATH/coreblocks/
-sphinx-build -M html $DOCS_DIR $BUILD_DIR
+sphinx-build -b html -W $DOCS_DIR $BUILD_DIR
