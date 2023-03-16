91dd3e637345e65185dfeeff2737a72c7e77905f
Jacob Urbanczyk <jacob@urbanczyk.dev>
2023-03-16 23:08:54 +0100

    Make wire names in the top module more descriptive

--- scripts/gen_verilog.py
+++ scripts/gen_verilog.py
@@ -6,35 +6,49 @@ import argparse
 
 from amaranth.build import Platform
 from amaranth.back import verilog
-from amaranth import Module, Elaboratable
+from amaranth import Module, Elaboratable, Record, Signal
 
 
 class Top(Elaboratable):
     def __init__(self, gen_params):
         from coreblocks.params.genparams import GenParams
-        from coreblocks.peripherals.wishbone import WishboneMaster, WishboneParameters
+        from coreblocks.peripherals.wishbone import WishboneParameters, WishboneLayout
 
         self.gp: GenParams = gen_params
 
-        wb_params = WishboneParameters(data_width=32, addr_width=30)
+        self.wb_params = WishboneParameters(data_width=32, addr_width=30)
 
-        self.wb_master_instr = WishboneMaster(wb_params=wb_params)
-        self.wb_master_data = WishboneMaster(wb_params=wb_params)
+        wb_layout = WishboneLayout(self.wb_params).wb_layout
+
+        # We create separate records, so the wire names in the generated Verilog are more descriptive.
+        self.wb_instr = Record(wb_layout)
+        self.wb_data = Record(wb_layout)
 
     def elaborate(self, platform: Platform):
         from coreblocks.core import Core
         from coreblocks.transactions import TransactionModule
+        from coreblocks.peripherals.wishbone import WishboneMaster
 
         m = Module()
         tm = TransactionModule(m)
 
-        self.core = Core(gen_params=self.gp, wb_master_instr=self.wb_master_instr, wb_master_data=self.wb_master_data)
+        wb_master_instr = WishboneMaster(wb_params=self.wb_params)
+        wb_master_data = WishboneMaster(wb_params=self.wb_params)
 
-        m.submodules.wb_master_instr = self.wb_master_instr
-        m.submodules.wb_master_data = self.wb_master_data
+        self.core = Core(gen_params=self.gp, wb_master_instr=wb_master_instr, wb_master_data=wb_master_data)
+
+        m.d.comb += wb_master_instr.wbMaster.connect(self.wb_instr)
+        m.d.comb += wb_master_data.wbMaster.connect(self.wb_data)
+
+        m.submodules.wb_master_instr = wb_master_instr
+        m.submodules.wb_master_data = wb_master_data
         m.submodules.c = self.core
 
         return tm
+
+
+def record_to_signals(record: Record) -> list[Signal]:
+    return list(record.fields.values())
 
 
 def gen_verilog():
@@ -44,7 +58,7 @@ def gen_verilog():
     top = Top(GenParams("rv32i", basic_configuration))
 
     with open("core.v", "w") as f:
-        signals = list(top.wb_master_instr.wbMaster.fields.values()) + list(top.wb_master_data.wbMaster.fields.values())
+        signals = record_to_signals(top.wb_instr) + record_to_signals(top.wb_data)
 
         f.write(verilog.convert(top, ports=signals, strip_internal_attrs=True))
 
