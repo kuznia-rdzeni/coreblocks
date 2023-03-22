18e8bd1cc94d382984ab3b874749562dc6c4d75c
Lekcyjna <309016@uwr.edu.pl>
2023-03-22 16:46:38 +0100

    Multiport fifo
    Added basic multiport fifo tests
    
    Small set of fixes. Currently there is still bug with ready signal for 2'nd and next ports
    
    Fixes
    
    Fixes
    
    Some changes
    
    BOOM! The wall is hard
    
    Remove already merged changes.
    
    Remove already merged changes.
    
    Start refactor after merge
    
    PriorityOrderingProxy
    
    Remove PriorityOrderingProxy because it was moved to separate PR

--- coreblocks/utils/fifo.py
+++ coreblocks/utils/fifo.py
@@ -1,7 +1,10 @@
 from amaranth import *
-from coreblocks.transactions import Method, def_method, Priority, TModule
+from coreblocks.transactions import Method, def_method, Priority, TModule, Transaction
+from amaranth.utils import log2_int
 from coreblocks.transactions._utils import MethodLayout
-from coreblocks.utils._typing import ValueLike
+from coreblocks.transactions.lib import MethodProduct, PriorityOrderingProxy
+from coreblocks.utils._typing import ValueLike, ModuleLike
+from coreblocks.utils.utils import popcount
 
 
 class BasicFifo(Elaboratable):
@@ -54,6 +57,18 @@ class BasicFifo(Elaboratable):
         self.clear.add_conflict(self.read, Priority.LEFT)
         self.clear.add_conflict(self.write, Priority.LEFT)
 
+    def get_read(self):
+        """
+        Return `read` method. This function is created for interface compatibility with MultiportFifo.
+        """
+        return self.read
+
+    def get_write(self):
+        """
+        Return `write` method. This function is created for interface compatibility with MultiportFifo.
+        """
+        return self.write
+
     def elaborate(self, platform):
         def mod_incr(sig: Value, mod: int) -> Value:
             # perform (sig+1)%mod operation
@@ -98,5 +113,143 @@ class BasicFifo(Elaboratable):
         def _() -> None:
             m.d.sync += self.read_idx.eq(0)
             m.d.sync += self.write_idx.eq(0)
+
+        return m
+
+
+class MultiportFifo(Elaboratable):
+    def __init__(self, layout: MethodLayout, depth: int, port_count: int, fifo_count: int) -> None:
+        self.layout = layout
+        self.width = len(Record(self.layout))
+        self.depth = depth
+        self.port_count = port_count
+        self.fifo_count = fifo_count
+
+        self.connected_reads = 0
+        self.connected_writes = 0
+
+        self._read_methods = [Method(o=self.layout) for _ in range(self.port_count)]
+        self._write_methods = [Method(i=self.layout) for _ in range(self.port_count)]
+        self.clear = Method()
+
+        self._selection_layout = [("valid", 1), ("data", self.layout)]
+
+        if self.depth % self.fifo_count != 0:
+            raise ValueError("MultiportFifo depth must be divisable by fifo_count")
+        if self.fifo_count < self.port_count:
+            raise ValueError("MultiportFifo requires fifo_count >= port_count")
+
+        for method in self._read_methods:
+            self.clear.add_conflict(method, Priority.LEFT)
+        for method in self._write_methods:
+            self.clear.add_conflict(method, Priority.LEFT)
+
+    def get_read(self):
+        method = self._read_methods[self.connected_reads]
+        self.connected_reads = (self.connected_reads + 1) % self.port_count
+        return method
+
+    def get_write(self):
+        method = self._write_methods[self.connected_writes]
+        self.connected_writes = (self.connected_writes + 1) % self.port_count
+        return method
+
+    def order(
+        self, m: ModuleLike, to_order: Array, ready_ordering: Signal, data_direction_from_out: bool = True
+    ) -> Array:
+        """
+        Put all ready elements from `to_order` to the begining of `out`
+        """
+        count = len(to_order)
+        selected_counters = [Signal(range(count)) for _ in range(count + 1)]
+        out = Array([Record(self._selection_layout) for _ in range(count)])
+
+        for j in range(count):
+            with m.If(ready_ordering[j]):
+                m.d.comb += out[selected_counters[j]].valid.eq(1)
+                if data_direction_from_out:
+                    m.d.comb += to_order[j].eq(out[selected_counters[j]].data)
+                else:
+                    m.d.comb += out[selected_counters[j]].data.eq(to_order[j])
+                m.d.comb += selected_counters[j + 1].eq(selected_counters[j] + 1)
+        return out
+
+    def elaborate(self, platform) -> TModule:
+        m = TModule()
+
+        clear_signal = Signal()
+
+        sub_fifo_depth = self.depth // self.fifo_count
+        sub_fifos = [BasicFifo(self.layout, sub_fifo_depth) for i in range(self.fifo_count)]
+        for i, sub_fifo in enumerate(sub_fifos):
+            setattr(m.submodules, f"sub_fifo_{i}", sub_fifo)
+
+        read_ready_list = [Signal() for _ in range(self.port_count)]
+        read_grants = Signal(len(self._read_methods))
+        read_grants_count = popcount(m, read_grants)
+        read_outs = [Record(self.layout) for _ in self._read_methods]
+        ordered_read_outs = self.order(m, Array(read_outs), read_grants)
+        ordered_read_ready = self.order(m, Array(read_ready_list), read_grants)
+
+        with m.FSM():
+            for i in range(self.fifo_count):
+                with m.State(f"current_read_{i}"):
+                    for j in range(self.port_count):
+                        selected_sub_fifo = sub_fifos[(i + j) % self.fifo_count]
+                        # m.d.comb += ordered_read_ready[j].eq(selected_sub_fifo.read.ready) <- should be
+                        m.d.comb += read_ready_list[j].eq(selected_sub_fifo.read.ready)
+                        with Transaction().body(m, request=ordered_read_outs[j].valid):
+                            m.d.comb += ordered_read_outs[j].data.eq(selected_sub_fifo.read(m))
+                    for j in range(self.port_count + 1):
+                        with m.Switch(read_grants_count):
+                            with m.Case(j):
+                                name_of_next = f"current_read_{(i+j)%self.fifo_count}"
+                                m.next = name_of_next
+                    with m.If(clear_signal):
+                        m.next = "current_read_0"
+
+        for i in range(self.port_count):
+
+            @def_method(m, self._read_methods[i], ready=read_ready_list[i])
+            def _() -> ValueLike:
+                m.d.comb += read_grants[i].eq(1)
+                return read_outs[i]
+
+        write_ready_list = [Signal() for _ in range(self.port_count)]
+        write_granttts = Signal(len(self._write_methods), reset=0)
+        write_granttts_count = popcount(m, write_granttts)
+        write_ins = [Record(self.layout) for _ in self._write_methods]
+        ordered_write_ins = self.order(m, Array(write_ins), write_granttts, data_direction_from_out=False)
+        ordered_write_ready = self.order(m, Array(write_ready_list), write_granttts)
+
+        write_start_state = "current_write_0"
+        with m.FSM(write_start_state):
+            for i in range(self.fifo_count):
+                with m.State(f"current_write_{i}"):
+                    for j in range(self.port_count):
+                        selected_sub_fifo = sub_fifos[(i + j) % self.fifo_count]
+                        # m.d.comb += ordered_write_ready[j].eq(selected_sub_fifo.write.ready) <- should be
+                        m.d.comb += write_ready_list[j].eq(selected_sub_fifo.write.ready)
+                        with Transaction().body(m, request=ordered_write_ins[j].valid):
+                            selected_sub_fifo.write(m, ordered_write_ins[j].data)
+                    for j in range(self.port_count + 1):
+                        with m.Switch(write_granttts_count):
+                            with m.Case(j):
+                                m.next = f"current_write_{(i+j)%self.fifo_count}"
+                    with m.If(clear_signal):
+                        m.next = "current_write_0"
+
+        for i in range(self.port_count):
+
+            @def_method(m, self._write_methods[i], ready=write_ready_list[i])
+            def _(data: Record) -> None:
+                m.d.comb += write_granttts[i].eq(1)
+                m.d.comb += write_ins[i].eq(data)
+
+        @def_method(m, self.clear)
+        def _():
+            m.submodules.clear_product = clear_product = MethodProduct([sub_fifo.clear for sub_fifo in sub_fifos])
+            clear_product.method(m)
+            m.d.comb += clear_signal.eq(1)
 
         return m
--- test/utils/test_fifo.py
+++ test/utils/test_fifo.py
@@ -1,78 +1,179 @@
 from amaranth import *
+from amaranth.sim import Settle, Passive
 
-from coreblocks.utils.fifo import BasicFifo
+from coreblocks.utils.fifo import BasicFifo, MultiportFifo
+from coreblocks.transactions import TransactionModule
 from coreblocks.transactions.lib import AdapterTrans
+from coreblocks.utils._typing import LayoutLike
 
 from test.common import TestCaseWithSimulator, TestbenchIO, data_layout
 from collections import deque
 from parameterized import parameterized_class
 import random
+from typing import Callable
 
 
-class BasicFifoTestCircuit(Elaboratable):
-    def __init__(self, depth):
+# class FifoTestCircuit(Elaboratable):
+#    def __init__(self, depth, init, fifo_constructor):
+#        self.depth = depth
+#        self.init = init
+#        self.fifo_constructor = fifo_constructor
+#
+#    def elaborate(self, platform):
+#        m = Module()
+#        tm = TransactionModule(m)
+#
+#        m.submodules.fifo = self.fifo = self.fifo_constructor(layout=data_layout(8), depth=self.depth, init=self.init)
+#
+#        m.submodules.fifo_read = self.fifo_read = TestbenchIO(AdapterTrans(self.fifo.get_read()))
+#        m.submodules.fifo_write = self.fifo_write = TestbenchIO(AdapterTrans(self.fifo.get_write()))
+#        m.submodules.fifo_clear = self.fifo_clear = TestbenchIO(AdapterTrans(self.fifo.clear))
+#
+#        return tm
+
+
+class FifoTestCircuit(Elaboratable):
+    def __init__(self, depth, port_count, fifo_count, fifo_constructor):
         self.depth = depth
+        self.port_count = port_count
+        self.fifo_count = fifo_count
+        self.fifo_constructor = fifo_constructor
 
     def elaborate(self, platform):
         m = Module()
 
-        m.submodules.fifo = self.fifo = BasicFifo(layout=data_layout(8), depth=self.depth)
+        m.submodules.fifo = self.fifo = self.fifo_constructor(
+            layout=data_layout(8), depth=self.depth, fifo_count=self.fifo_count, port_count=self.port_count
+        )
 
-        m.submodules.fifo_read = self.fifo_read = TestbenchIO(AdapterTrans(self.fifo.read))
-        m.submodules.fifo_write = self.fifo_write = TestbenchIO(AdapterTrans(self.fifo.write))
+        self.fifo_reads = []
+        self.fifo_writes = []
+        for i in range(self.port_count):
+            self.fifo_reads.append(TestbenchIO(AdapterTrans(self.fifo.get_read())))
+            self.fifo_writes.append(TestbenchIO(AdapterTrans(self.fifo.get_write())))
+            setattr(m.submodules, f"fifo_read_{i}", self.fifo_reads[i])
+            setattr(m.submodules, f"fifo_write_{i}", self.fifo_writes[i])
         m.submodules.fifo_clear = self.fifo_clear = TestbenchIO(AdapterTrans(self.fifo.clear))
 
         return m
 
 
+params_dinit = [
+    ("notpower", 12),
+    ("power", 8),
+]
+
+params_c = [
+    (
+        "basic",
+        lambda self, layout, depth, port_count, fifo_count: BasicFifo(layout=layout, depth=depth),
+    ),
+    (
+        "multi",
+        lambda self, layout, depth, port_count, fifo_count: MultiportFifo(
+            layout=layout, depth=depth, port_count=port_count, fifo_count=fifo_count
+        ),
+    ),
+]
+
+
 @parameterized_class(
-    ("name", "depth"),
-    [
-        ("notpower", 5),
-        ("power", 4),
-    ],
+    ("name", "depth", "port_count", "fifo_count", "name_constr", "fifo_constructor"),
+    [dinit + (1, 1) + constr for dinit in params_dinit for constr in params_c]
+    + [dinit + (4, 4) + params_c[1] for dinit in params_dinit],
 )
 class TestBasicFifo(TestCaseWithSimulator):
     depth: int
+    port_count: int
+    fifo_count: int
+    fifo_constructor: Callable[[LayoutLike, int, int], Elaboratable]
 
     def test_randomized(self):
-        fifoc = BasicFifoTestCircuit(depth=self.depth)
-        expq = deque()
+        fifoc = FifoTestCircuit(
+            depth=self.depth,
+            port_count=self.port_count,
+            fifo_count=self.fifo_count,
+            fifo_constructor=self.fifo_constructor,
+        )
+        writed: list[tuple[int, int, int]] = []  # (cycle_id, port_id, value)
+        readed = []
+        clears = []
+
+        dones = [False for _ in range(self.port_count)]
 
         cycles = 256
-        random.seed(42)
+        random.seed(44)
 
-        self.done = False
+        def source_generator(port_id: int):
+            def source():
+                cycle = 0
+                for _ in range(cycles):
+                    cycle += 1
+                    if random.randint(0, 1):
+                        cycle += 1
+                        yield  # random delay
 
-        def source():
-            for _ in range(cycles):
-                if random.randint(0, 1):
-                    yield  # random delay
+                    if port_id == 0 and random.random() < 0.005:
+                        if (yield from fifoc.fifo_clear.call_try()) is None:
+                            assert "Clearing failed"
+                        clears.append((cycle, port_id))
+                        cycle += 1
 
-                v = random.randint(0, (2**fifoc.fifo.width) - 1)
-                yield from fifoc.fifo_write.call(data=v)
-                expq.appendleft(v)
+                    v = random.randrange(2**fifoc.fifo.width)
+                    yield Settle()
+                    while (yield from fifoc.fifo_writes[port_id].call_try(data=v)) is None:
+                        cycle += 1
+                    writed.append((cycle, port_id, v))
+                dones[port_id] = True
 
-                if random.random() < 0.005:
-                    yield from fifoc.fifo_clear.call()
-                    expq.clear()
+            return source
 
-            self.done = True
+        def target_generator(port_id: int):
+            def target():
+                yield Passive()
+                cycle = 0
+                while True:
+                    cycle += 1
+                    if random.randint(0, 1):
+                        cycle += 1
+                        yield
 
-        def target():
-            while not self.done or expq:
-                if random.randint(0, 1):
-                    yield
+                    v = yield from fifoc.fifo_reads[port_id].call_try()
+                    if v is not None:
+                        readed.append((cycle, port_id, v["data"]))
 
-                yield from fifoc.fifo_read.call_init()
+            return target
+
+        def checker():
+            while not all(dones):
                 yield
+            readed.sort()
+            writed.sort()
+            INF_INT = 1000000000
+            clears.append((INF_INT, -1))
+            #            print(readed)
+            #            print(writed)
 
-                v = yield from fifoc.fifo_read.call_result()
-                if v is not None:
-                    self.assertEqual(v["data"], expq.pop())
-
-                yield from fifoc.fifo_read.disable()
+            write_it = 0
+            clear_it = 0
+            for i, (cycle, port_id, val) in enumerate(readed):
+                while cycle > clears[clear_it][0]:
+                    while writed[write_it][0] < clears[clear_it][0]:
+                        # print(writed[write_it],clears[clear_it])
+                        write_it += 1
+                    if write_it >= len(writed):
+                        break
+                    clear_it += 1
+                # print(clears)
+                # print("odczytana:",val, "zapisana:",writed[write_it], "cykl_odczytu:", cycle, "port", port_id)
+                self.assertEqual(val, writed[write_it][2])
+                write_it += 1
 
         with self.run_simulation(fifoc) as sim:
-            sim.add_sync_process(source)
-            sim.add_sync_process(target)
+            sim.add_sync_process(checker)
+            #            sim.add_sync_process(source_generator(0))
+            #            sim.add_sync_process(source_generator(1))
+            #            sim.add_sync_process(target_generator(0))
+            for i in range(self.port_count):
+                sim.add_sync_process(source_generator(i))
+                sim.add_sync_process(target_generator(i))
