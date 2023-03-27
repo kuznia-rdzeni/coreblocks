f20b4fd0115232060f64c0baa5d364e91e2acc23
Piotro <piotro888@wp.pl>
2023-03-27 19:34:13 +0200

    csr: doublecounter docstrings + regs rename

--- coreblocks/structs_common/csr_generic.py
+++ coreblocks/structs_common/csr_generic.py
@@ -14,30 +14,51 @@ class CSRAddress(BitEnum, width=12):
 
 
 class DoubleCounterCSR(Elaboratable):
+    """DoubleCounterCSR
+    Groups two `CSRRegisters` to form counter with double `isa.xlen` width.
+
+    Attributes
+    ----------
+    increment: Method
+        Increments the counter by 1. At overflow, counter value is set to 0.
+    """
+
     def __init__(self, gen_params: GenParams, low_addr: CSRAddress, high_addr: Optional[CSRAddress] = None):
+        """
+        Parameters
+        ----------
+        gen_params: GenParams
+            Core generation parameters.
+        low_addr: CSRAddress
+            Address of the CSR register representing lower part of the counter (bits `[isa.xlen-1 : 0]`).
+        high_addr: CSRAddress or None, optional
+            Address of the CSR register representing higher part of the counter (bits `[2*isa.xlen-1 : isa.xlen]`).
+            If high_addr is None or not provided, then higher CSR is not synthetised and only the width of
+            low_addr CSR is available to the counter.
+        """
         self.gen_params = gen_params
 
         self.increment = Method()
 
-        self.register = CSRRegister(low_addr, gen_params)
-        self.high_register = (
+        self.register_low = CSRRegister(low_addr, gen_params)
+        self.register_high = (
             CSRRegister(high_addr, gen_params) if gen_params.isa.xlen == 32 and high_addr is not None else None
         )
 
     def elaborate(self, platform):
         m = Module()
 
-        m.submodules.register = self.register
-        if self.high_register is not None:
-            m.submodules.high_register = self.high_register
+        m.submodules.register_low = self.register_low
+        if self.register_high is not None:
+            m.submodules.register_high = self.register_high
 
         @def_method(m, self.increment)
         def _():
-            register_read = self.register.read(m).data
-            self.register.write(m, data=register_read + 1)
+            register_read = self.register_low.read(m).data
+            self.register_low.write(m, data=register_read + 1)
 
-            if self.high_register is not None:
+            if self.register_high is not None:
                 with m.If(register_read == (1 << self.gen_params.isa.xlen) - 1):
-                    self.high_register.write(m, data=self.high_register.read(m).data + 1)
+                    self.register_high.write(m, data=self.register_high.read(m).data + 1)
 
         return m
