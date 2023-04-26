39cf38128e9b336e407daeab45f1e71862a33141
Lekcyjna <309016@uwr.edu.pl>
2023-04-26 10:41:34 +0200

    Doc strings

--- coreblocks/transactions/lib.py
+++ coreblocks/transactions/lib.py
@@ -88,7 +88,35 @@ class FIFO(Elaboratable):
 
 
 class MemoryBank(Elaboratable):
+    """MemoryBank module.
+
+    Provides a transactional interface to synchronous Amaranth Memory with one
+    read port and one write port. It supports optionally writing with given granularity.
+
+    Attributes
+    ----------
+    read_req: Method
+        The read request method. Accepts an `addr` from which data should be read.
+        Ready only if there is no response pending to be read or response is currently being read.
+    read_resp: Method
+        The read response method. Return `data_layout` Record which was saved on `addr` given by last
+        `read_req` method call. Ready only after `read_req` call.
+    write: Method
+        The write method. Accepts `addr` where data should be saved, `data` in form of `data_layout`
+        and optionaly `mask` if `granularity` is not None. `1` in mask means that apropriate part should be written.
+    """
     def __init__(self, *, data_layout: MethodLayout, elem_count: int, granularity: Optional[int] = None):
+        """
+        Parameters
+        ----------
+        data_layout: record layout
+            The format of records stored in the Memory.
+        elem_count: int
+            Number of elements stored in Memory.
+        granularity: Optional[int]
+            Granularity of write, forwarded to Amaranth. If `None` the whole record is always saved at once.
+            If not the width of `data_layout` is split into `granularity` parts, which can be saved independently.
+        """
         self.data_layout = data_layout
         self.elem_count = elem_count
         self.granularity = granularity
@@ -784,6 +812,24 @@ class CatTrans(Elaboratable):
 
 
 class Serializer(Elaboratable):
+    """Module to serialize request-response methods.
+
+    Provides a transactional interface to connect many client `Module`\\s (which request somethig using method call)
+    with a server `Module` which deliver method to request operation and method to get response.
+
+    Requests are being serialized from many clients and forwarded to a server which can process only one request
+    at the time. Responses from server are deserialized and passed to proper clients. `Serializer` assumes, that
+    responses from the server are in-order, so the order of responses is the same as order of requests.
+
+
+    Attributes
+    ----------
+    serialize_in: list[Method]
+        List of request methods. Data layouts are the same as for `serialized_req_method`.
+    serialize_out: list[Method]
+        List of response methods. Data layouts are the same as for `serialized_resp_method`.
+        `i`-th response method provides responses for requests from `i`-th `serialize_in` method.
+    """
     def __init__(
         self,
         *,
@@ -792,6 +838,19 @@ class Serializer(Elaboratable):
         serialized_resp_method: Method,
         depth: int = 4,
     ):
+        """
+        Parameters
+        ----------
+        port_count: int
+            Number of ports, which should be generated. `len(serialize_in)=len(serialize_out)=port_count`
+        serialized_req_method: Method
+            Request method provided by server `Module`.
+        serialized_resp_method: Method
+            Response method provided by server `Module`.
+        depth: Optional[int]
+            Number of requests which can be forwarded to server, before server provides first response. Describe
+            the resistance of `Serializer` to latency of server in case when server is fully pipelined.
+        """
         self.port_count = port_count
         self.serialized_req_method = serialized_req_method
         self.serialized_resp_method = serialized_resp_method
@@ -804,7 +863,7 @@ class Serializer(Elaboratable):
         self.serialize_in = [Method.like(self.serialized_req_method) for _ in range(self.port_count)]
         self.serialize_out = [Method.like(self.serialized_resp_method) for _ in range(self.port_count)]
 
-    def elaborate(self, platform):
+    def elaborate(self, platform) -> Module:
         m = Module()
 
         pending_requests = BasicFifo(self.id_layout, self.depth)
