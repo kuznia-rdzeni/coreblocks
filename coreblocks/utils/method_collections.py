1405bef3814354042680f3e8b01ce01dbf63ac75
Lekcyjna <309016@uwr.edu.pl>
2023-04-27 19:34:16 +0200

    Added alternative proposition

--- coreblocks/utils/method_collections.py
+++ coreblocks/utils/method_collections.py
@@ -1,16 +1,19 @@
 from amaranth import *
 
 from coreblocks.transactions import Method
-from typing import Optional, TypeVar, Callable, Union, Generic
+from typing import Optional, TypeVar, Callable, Union, Generic, Type
 from ._typing import HasElaborate, SignalBundle
 from .protocols import MethodCollection, MethodCollectionElement, MethodCollectionOutput
 from abc import abstractmethod, ABC
+from collections.abc import Collection
 
 
 __all__ = ["ListMethodCollection", "DictMethodCollection"]
 
 T = TypeVar('T')
+T2 = TypeVar('T2')
 _T_nested_dict = dict[str, Union[T, '_T_nested_dict']]
+_T_nested_collection = T | list['_T_nested_collection[T]'] | dict[str, '_T_nested_collection[T]']
 
 class MethodCollectionBase(ABC, MethodCollection):
     @abstractmethod
@@ -59,3 +62,35 @@ class DictMethodCollection(MethodCollectionBase):
             else:
                 result[key] = elem.get_all_methods()
         return result
+
+# Second proposition:
+
+def transform_collection_deep(container : _T_nested_collection[T], f : Callable[[str, T], T2],*, list_prefix : str="") -> _T_nested_collection[T2]:
+    if isinstance(container, list):
+        return [transform_collection_deep(elem, lambda n, x: f(list_prefix+str(i)+n, x), list_prefix="") for i,elem in enumerate(container)]
+    elif isinstance(container, dict):
+        return dict([ (name, transform_collection_deep(elem, lambda n, x: f(name+n, x), list_prefix=list_prefix)) for name, elem in container.items() ])
+    else:
+        return f("", container)
+
+def transform_type_collection_deep(type : Type, container : _T_nested_collection[T], f : Callable[[str, Method], T2],*, list_prefix : str="") -> _T_nested_collection[Optional[T2]]:
+    def f_type(name, x):
+        if isinstance(x, type):
+            return f(name, x)
+        return None
+    return transform_collection_deep(container, f_type, list_prefix=list_prefix)
+
+
+# Usage example:
+#
+# Original code:
+# if isinstance(attr, list):
+#     for i, elem in enumerate(attr):
+#         if isinstance(elem, Method):
+#             self._io[name + str(i)] = TestbenchIO(AdapterTrans(elem))
+#             m.submodules[name + str(i)] = self._io[name + str(i)]
+#
+# New code:
+# if isinstance(attr, Collection):
+#   self._io[name] = transform_type_collection_deep(Method, attr, lambda _, elem:TestbenchIO(AdapterTrans(elem)))
+#   transform_type_collection_deep(Method, self._io[name], lambda n, elem: m.submodules[n] = elem)
