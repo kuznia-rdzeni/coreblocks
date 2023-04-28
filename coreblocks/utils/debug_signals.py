a753ab8e4ca849bd44b4318588539c6260198334
Lekcyjna <309016@uwr.edu.pl>
2023-04-28 20:08:17 +0200

    Added mappings

--- coreblocks/utils/debug_signals.py
+++ coreblocks/utils/debug_signals.py
@@ -1,10 +1,10 @@
 from amaranth import *
 from ._typing import SignalBundle, HasDebugSignals
-from collections.abc import Collection
+from collections.abc import Collection, Mapping
 import coreblocks.transactions.core
 
 
-def auto_debug_signals(thing) -> SignalBundle:
+def auto_debug_signals(thing, *, visited: set = set()) -> SignalBundle:
     """Automatic debug signal generation.
 
     Exposes class attributes with debug signals (Amaranth `Signal`\\s,
@@ -22,10 +22,22 @@ def auto_debug_signals(thing) -> SignalBundle:
     if "__dict__" not in dir(thing):
         return []
 
+    visited.add(id(thing))
+
     for v in vars(thing):
         a = getattr(thing, v)
+
+        # Check for reference cycles e.g. Amaranth's MustUse
+        if id(a) in visited:
+            continue
+        visited.add(id(a))
+
+        # ignore private fields (mostly to ignore _MustUse_context to get preaty print)
+        if v[0] == "_":
+            continue
+
         if isinstance(a, Elaboratable) or isinstance(a, coreblocks.transactions.core.TransactionBase):
-            smap[v] = auto_debug_signals(a)
+            smap[v] = auto_debug_signals(a, visited=visited)
         elif isinstance(a, Array):
             for i, e in enumerate(a):
                 if isinstance(e, Record):
@@ -33,10 +45,18 @@ def auto_debug_signals(thing) -> SignalBundle:
             smap[v] = a
         elif isinstance(a, Signal) or isinstance(a, Record):
             slist.append(a)
+        elif isinstance(a, Mapping):
+            submap = {}
+            for i, e in a.items():
+                sublist = auto_debug_signals(e, visited=visited)
+                if sublist:
+                    submap[f"{v}[{i}]"] = sublist
+            if submap:
+                smap[v] = submap
         elif isinstance(a, Collection):
             submap = {}
             for i, e in enumerate(a):
-                sublist = auto_debug_signals(e)
+                sublist = auto_debug_signals(e, visited=visited)
                 if sublist:
                     submap[f"{v}[{i}]"] = sublist
             if submap:
