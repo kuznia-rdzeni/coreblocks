bec61d142cb38cf5a672207d92b8e7f1a43f4fae
Lekcyjna <309016@uwr.edu.pl>
2023-05-01 09:46:09 +0200

    Adder pseudocode

--- coreblocks/fu/vector_unit/flexible_alu.py
+++ coreblocks/fu/vector_unit/flexible_alu.py
@@ -33,16 +33,48 @@ class FlexibleAluFn(DecoderManager):
         OR = auto()  # Bitwise or
         AND = auto()  # Bitwise and
 
-    class Width(IntFlag):
-        w8 = auto()
-
     @classmethod
     def get_instructions(cls) -> Sequence[tuple]:
         return [
                 #TODO fill after extending instruction decoder
         ]
 
-class FlexibleAdder
+class FlexibleAdder(Elaboratable):
+    def __init__(self, v_params : VectorParameters, out_width : EEW):
+        self.v_params = v_params
+        self.out_width = out_width
+        self.out_width_bits : int = transform(out_width)
+        self.eew = Signal(EEW)
+        self.in1 = Signal(self.out_width_bits)
+        self.in2 = Signal(self.out_width_bits)
+
+        self.out_data = Signal(self.out_width_bits)
+        self.out_carry = Signal()
+
+    def elaborate(self, platform) -> Module:
+        m = Module()
+
+        if self.out_width == EEW.w8:
+            result = self.in1 + self.in2
+            assert result.shape().width == self.out_width_bits+1
+            m.d.comb += self.out_data.eq(result[:-1])
+            m.d.comb += self.out_carry.eq(result[-1])
+        else:
+            smaller_out_width = TODO
+            #TODO connect self.EEW, in1, in2
+            m.submodules.adder_down = adder_down = FlexibleAdder(self.v_params, smaller_out_width)
+            m.submodules.adder_high = adder_high = FlexibleAdder(self.v_params, smaller_out_width)
+
+            with m.If(self.eew <= self.out_width):
+                high_with_down_carry = whole_high + adder_down.out_carry
+                result = (high_with_down_carry << bits_smaller) | adder_down.out_data
+                m.d.comb += self.out_data.eq(result[:-1])
+                m.d.comb += self.out_carry.eq(result[-1])
+            with m.Else():
+                result = Cat(adder_high.out_data, adder_down.out_data)
+                m.d.comb += self.out_data.eq(result[:-1])
+
+        return m
 
 class BasicFlexibleAlu(Elaboratable):
     """
--- coreblocks/fu/vector_unit/utils.py
+++ coreblocks/fu/vector_unit/utils.py
@@ -5,7 +5,7 @@ from coreblocks.params import *
 from coreblocks.params.vector_params import VectorParameters
 from coreblocks.fu.vector_unit.v_layouts import RegisterLayouts
 
-__all__ = ["EEW"]
+__all__ = ["EEW", "eew_to_bits"]
 
 class EEW(IntEnum):
     w8 = auto()
@@ -13,4 +13,14 @@ class EEW(IntEnum):
     w32 = auto()
     w64 = auto()
 
-
+def eew_to_bits(eew : EEW) -> int:
+    if eew == EEW.w8:
+        return 8
+    elif eew == EEW.w16:
+        return 16
+    elif eew == EEW.w32:
+        return 32
+    elif eew == EEW.w64:
+        return 64
+    else:
+        raise ValueError(f"Not known EEW: {eew}")
