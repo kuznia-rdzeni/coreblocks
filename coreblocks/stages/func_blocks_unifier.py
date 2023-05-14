kuznia-rdzeni/coreblocks

commit e95f7a962aa054c93e4a447d7aaae6df9ab1816a

Optype refactor (#334)
* Clean up optype stuff

* Derive from protocols for early checking

* Fixed test (avoid noqa please...)

* Cleanup

coreblocks/stages/func_blocks_unifier.py
@@ -20,13 +20,13 @@ class FuncBlocksUnifier(Elaboratable):
         blocks: Iterable[BlockComponentParams],
         extra_methods_required: Iterable[UnifierKey],
     ):
-        self.rs_blocks = [block.get_module(gen_params) for block in blocks]
+        self.rs_blocks = [(block.get_module(gen_params), block.get_optypes()) for block in blocks]
         self.extra_methods_required = extra_methods_required
 
-        self.result_collector = Collector([block.get_result for block in self.rs_blocks])
+        self.result_collector = Collector([block.get_result for block, _ in self.rs_blocks])
         self.get_result = self.result_collector.method
 
-        self.update_combiner = MethodProduct([block.update for block in self.rs_blocks])
+        self.update_combiner = MethodProduct([block.update for block, _ in self.rs_blocks])
         self.update = self.update_combiner.method
 
         self.unifiers: dict[str, Unifier] = {}
@@ -48,7 +48,7 @@ class FuncBlocksUnifier(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        for n, unit in enumerate(self.rs_blocks):
+        for n, (unit, _) in enumerate(self.rs_blocks):
             m.submodules[f"rs_block_{n}"] = unit
 
         m.submodules["result_collector"] = self.result_collector

coreblocks/fu/alu.py
@@ -103,9 +103,7 @@ class Alu(Elaboratable):
         return m
 
 
-class AluFuncUnit(Elaboratable):
-    optypes = AluFn.get_op_types()
-
+class AluFuncUnit(FuncUnit, Elaboratable):
     def __init__(self, gen: GenParams):
         self.gen = gen
 
@@ -143,4 +141,4 @@ class ALUComponent(FunctionalComponentParams):
         return AluFuncUnit(gen_params)
 
     def get_optypes(self) -> set[OpType]:
-        return AluFuncUnit.optypes
+        return AluFn.get_op_types()

coreblocks/fu/jumpbranch.py
@@ -110,9 +110,7 @@ class JumpBranch(Elaboratable):
         return m
 
 
-class JumpBranchFuncUnit(Elaboratable):
-    optypes = JumpBranchFn.get_op_types()
-
+class JumpBranchFuncUnit(FuncUnit, Elaboratable):
     def __init__(self, gen: GenParams):
         self.gen = gen
 
@@ -165,4 +163,4 @@ class JumpComponent(FunctionalComponentParams):
         return unit
 
     def get_optypes(self) -> set[OpType]:
-        return JumpBranchFuncUnit.optypes
+        return JumpBranchFn.get_op_types()

coreblocks/fu/mul_unit.py
@@ -77,7 +77,7 @@ class MulType(IntEnum):
     RECURSIVE_MUL = 2
 
 
-class MulUnit(Elaboratable):
+class MulUnit(FuncUnit, Elaboratable):
     """
     Module responsible for handling every kind of multiplication based on selected unsigned integer multiplication
     module. It uses standard FuncUnitLayout.
@@ -89,8 +89,6 @@ class MulUnit(Elaboratable):
     accept: Method(i=gen.get(FuncUnitLayouts).accept)
         Method used for getting result of requested computation.
     """
-
-    optypes = MulFn.get_op_types()
 
     def __init__(self, gen: GenParams, mul_type: MulType, dsp_width: int = 32):
         """
@@ -218,4 +216,4 @@ class MulComponent(FunctionalComponentParams):
         return MulUnit(gen_params, self.mul_unit_type, self.dsp_width)
 
     def get_optypes(self) -> set[OpType]:
-        return MulUnit.optypes
+        return MulFn.get_op_types()

coreblocks/fu/zbs.py
@@ -79,7 +79,7 @@ class Zbs(Elaboratable):
         return m
 
 
-class ZbsUnit(Elaboratable):
+class ZbsUnit(FuncUnit, Elaboratable):
     """
     Module responsible for executing Zbs instructions.
 
@@ -90,8 +90,6 @@ class ZbsUnit(Elaboratable):
     accept: Method(i=FuncUnitLayouts.accept)
         Method used for getting result of requested computation.
     """
-
-    optypes = ZbsFunction.get_op_types()
 
     def __init__(self, gen_params: GenParams):
         layouts = gen_params.get(FuncUnitLayouts)
@@ -129,4 +127,4 @@ class ZbsComponent(FunctionalComponentParams):
         return ZbsUnit(gen_params)
 
     def get_optypes(self) -> set[OpType]:
-        return ZbsUnit.optypes
+        return ZbsFunction.get_op_types()

coreblocks/lsu/dummyLsu.py
@@ -186,7 +186,7 @@ class LSUDummyInternals(Elaboratable):
         return m
 
 
-class LSUDummy(Elaboratable):
+class LSUDummy(FuncBlock, Elaboratable):
     """
     Very simple LSU, which serializes all stores and loads.
     It isn't fully compliant with RiscV spec. Doesn't support checking if
@@ -210,8 +210,6 @@ class LSUDummy(Elaboratable):
     commit : Method
         Used to inform LSU that new instruction have been retired.
     """
-
-    optypes = {OpType.LOAD, OpType.STORE}
 
     def __init__(self, gen_params: GenParams, bus: WishboneMaster) -> None:
         """
@@ -294,4 +292,4 @@ class LSUBlockComponent(BlockComponentParams):
         return unit
 
     def get_optypes(self) -> set[OpType]:
-        return LSUDummy.optypes
+        return {OpType.LOAD, OpType.STORE}

coreblocks/params/fu_params.py
@@ -37,5 +37,5 @@ class FunctionalComponentParams(ABC):
         raise NotImplementedError()
 
 
-def optypes_supported(block_components: Iterable[BlockComponentParams]) -> set["OpType"]:
-    return {optype for block in block_components for optype in block.get_optypes()}
+def optypes_supported(components: Iterable[BlockComponentParams | FunctionalComponentParams]) -> set["OpType"]:
+    return {optype for component in components for optype in component.get_optypes()}

coreblocks/scheduler/scheduler.py
@@ -361,7 +361,7 @@ class Scheduler(Elaboratable):
         rob_put: Method,
         rf_read1: Method,
         rf_read2: Method,
-        reservation_stations: Sequence[FuncBlock],
+        reservation_stations: Sequence[tuple[FuncBlock, set[OpType]]],
         gen_params: GenParams
     ):
         """
@@ -429,13 +429,13 @@ class Scheduler(Elaboratable):
         m.submodules.rs_selector = RSSelection(
             gen_params=self.gen_params,
             get_instr=reg_alloc_out_buf.read,
-            rs_select=[(rs.select, rs.optypes) for rs in self.rs],
+            rs_select=[(rs.select, optypes) for rs, optypes in self.rs],
             push_instr=rs_select_out_buf.write,
         )
 
         m.submodules.rs_insertion = RSInsertion(
             get_instr=rs_select_out_buf.read,
-            rs_insert=[rs.insert for rs in self.rs],
+            rs_insert=[rs.insert for rs, _ in self.rs],
             rf_read1=self.rf_read1,
             rf_read2=self.rf_read2,
             gen_params=self.gen_params,

commit 5eec61d37d2786c2cd2af7954d2897e4b01ec03e

Fix debug_signals method bug introduced in #334

coreblocks/stages/func_blocks_unifier.py
@@ -64,5 +64,5 @@ class FuncBlocksUnifier(Elaboratable):
         return {
             "get_result": self.get_result.debug_signals(),
             "update": self.update.debug_signals(),
-            "rs_blocks": {i: auto_debug_signals(b) for i, b in enumerate(self.rs_blocks)},
+            "rs_blocks": {i: auto_debug_signals(b) for i, (b,_) in enumerate(self.rs_blocks)},
         }