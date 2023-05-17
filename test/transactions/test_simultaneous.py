7502b77876ff7d476d342a7770f76a7ae8868145
Marek Materzok <tilk@tilk.eu>
2023-05-17 12:46:44 +0200

    Additional test for simultaneous transactions

--- test/transactions/test_simultaneous.py
+++ test/transactions/test_simultaneous.py
@@ -98,46 +98,78 @@ class UnsatisfiableTriangleTest(TestCaseWithSimulator):
 
 
 class HelperConnect(Elaboratable):
-    def __init__(self, source: Method, target: Method, request: Signal):
+    def __init__(self, source: Method, target: Method, request: Signal, data: int):
         self.source = source
         self.target = target
         self.request = request
+        self.data = data
 
     def elaborate(self, platform):
         m = TModule()
 
         with Transaction().body(m, request=self.request):
-            self.target(m, self.source(m))
+            self.target(m, self.data ^ self.source(m))
 
         return m
 
 
 class TransitivityTestCircuit(Elaboratable):
-    def __init__(self):
-        self.source1 = TestbenchIO(AdapterTrans(Method(i=[('data', 2)])))
-        self.source2 = TestbenchIO(AdapterTrans(Method(i=[('data', 2)])))
-        self.target = TestbenchIO(Adapter(i=[('data', 2)]))
-        self.req1 = Signal()
-        self.req2 = Signal()
+    def __init__(self, target: Method, req1: Signal, req2: Signal):
+        self.source1 = Method(i=[('data', 2)])
+        self.source2 = Method(i=[('data', 2)])
+        self.target = target
+        self.req1 = req1
+        self.req2 = req2
 
     def elaborate(self, platform):
-        m = Module()
+        m = TModule()
 
         m.submodules.c1 = c1 = Connect([('data', 2)])
         m.submodules.c2 = c2 = Connect([('data', 2)])
-        m.submodules.ct1 = ConnectTrans(self.source1.adapter.iface, c1.write)
-        m.submodules.ct2 = ConnectTrans(self.source2.adapter.iface, c1.write)
-        m.submodules.ct = ConnectTrans(c2.read, self.target.adapter.iface)
-        m.submodules.hc1 = HelperConnect(c1.read, c2.write, self.req1)
-        m.submodules.hc2 = HelperConnect(c1.read, c2.write, self.req2)
-        m.submodules.tgt = self.target
+        self.source1.proxy(m, c1.write)
+        self.source2.proxy(m, c1.write)
+        m.submodules.ct = ConnectTrans(c2.read, self.target)
+        m.submodules.hc1 = HelperConnect(c1.read, c2.write, self.req1, 1)
+        m.submodules.hc2 = HelperConnect(c1.read, c2.write, self.req2, 2)
 
         return m
 
 
 class TransitivityTest(TestCaseWithSimulator):
     def test_transitivity(self):
-        pass
+        target = TestbenchIO(Adapter(i=[("data", 2)]))
+        req1 = Signal()
+        req2 = Signal()
+        
+        circ = SimpleTestCircuit(TransitivityTestCircuit(target.adapter.iface, req1, req2))
+        m = ModuleConnector(test_circuit=circ, target=target)
+        
+        result: Optional[int]
+
+        @def_method_mock(lambda: target)
+        def target_process(data):
+            nonlocal result
+            result = data
+        
+        def process():
+            nonlocal result
+            for source, data, reqv1, reqv2 in product([circ.source1, circ.source2], [0, 1, 2, 3], [0, 1], [0, 1]):
+                result = None
+                yield req1.eq(reqv1)
+                yield req2.eq(reqv2)
+                call_result = yield from source.call_try(data=data)
+
+                if not reqv1 and not reqv2:
+                    self.assertIsNone(call_result)
+                    self.assertIsNone(result)
+                else:
+                    self.assertIsNotNone(call_result)
+                    possibles = reqv1 * [data ^ 1] + reqv2 * [data ^ 2]
+                    self.assertIn(result, possibles)
+
+        with self.run_simulation(m) as sim:
+            sim.add_sync_process(target_process)
+            sim.add_sync_process(process)
 
 
 class ConditionTestCircuit(Elaboratable):
