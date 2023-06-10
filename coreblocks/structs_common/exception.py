b685e82f1d3cf047721d7dff35a4f5b04157de3d
Piotro <piotro888@wp.pl>
2023-06-10 16:51:47 +0200

    Refactor priority checking to Ifs

--- coreblocks/structs_common/exception.py
+++ coreblocks/structs_common/exception.py
@@ -8,7 +8,7 @@ from coreblocks.transactions.core import Priority, TModule, def_method, Method
 
 class Cause(BitEnum, width=31):
     INSTRUCTION_ADDRESS_MISALIGNED = 0
-    INSTRUCTION_ADDRESS_FAULT = 1
+    INSTRUCTION_ACCESS_FAULT = 1
     ILLEGAL_INSTRUCTION = 2
     BREAKPOINT = 3
     LOAD_ADDRESS_MISALIGNED = 4
@@ -23,23 +23,30 @@ class Cause(BitEnum, width=31):
     STORE_PAGE_FAULT = 15
 
 
-def cause_priority(current_cause: Value, new_cause: Value) -> Value:
-    return (
-        (new_cause == Cause.BREAKPOINT)
-        | (
-            (
-                (current_cause == Cause.LOAD_ACCESS_FAULT)
-                | (current_cause == Cause.STORE_ACCESS_FAULT)
-                | (current_cause == Cause.LOAD_PAGE_FAULT)
-                | (current_cause == Cause.STORE_PAGE_FAULT)
-            )
-            & ((new_cause == Cause.LOAD_ADDRESS_MISALIGNED) | (new_cause == Cause.STORE_ADDRESS_MISALIGNED))
-        )
-        | (
-            (current_cause == Cause.ILLEGAL_INSTRUCTION)
-            & ((new_cause == Cause.INSTRUCTION_PAGE_FAULT) | (new_cause == Cause.INSTRUCTION_ADDRESS_FAULT))
-        )
-    )
+def should_update_prioriy(m: TModule, current_cause: Value, new_cause: Value) -> Value:
+    # Comparing all priorities would be expensive, this function only checks conditions that could happen in hardware
+    _update = Signal()
+
+    # All breakpoint kinds have the highest priority in conflicting cases
+    with m.If(new_cause == Cause.BREAKPOINT):
+        m.d.comb += _update.eq(1)
+
+    with m.If((new_cause == Cause.INSTRUCTION_PAGE_FAULT) | (new_cause == Cause.INSTRUCTION_ACCESS_FAULT)):
+        m.d.comb += _update.eq(1)
+
+    with m.If(
+        (new_cause == Cause.LOAD_ADDRESS_MISALIGNED)
+        & ((current_cause == Cause.LOAD_ACCESS_FAULT) | (current_cause == Cause.LOAD_PAGE_FAULT))
+    ):
+        m.d.comb += _update.eq(1)
+
+    with m.If(
+        (new_cause == Cause.STORE_ADDRESS_MISALIGNED)
+        & ((current_cause == Cause.STORE_ACCESS_FAULT) | (current_cause == Cause.STORE_PAGE_FAULT))
+    ):
+        m.d.comb += _update.eq(1)
+
+    return _update
 
 
 class ExceptionCauseRegister(Elaboratable):
@@ -65,7 +72,7 @@ class ExceptionCauseRegister(Elaboratable):
             should_write = Signal()
 
             with m.If(self.valid & (self.rob_id == rob_id)):
-                m.d.comb += should_write.eq(cause_priority(current_cause=self.cause, new_cause=cause))
+                m.d.comb += should_write.eq(should_update_prioriy(m, current_cause=self.cause, new_cause=cause))
             with m.Elif(self.valid):
                 cmp_result = self.rob_order_comparator(m, first_rob_id=rob_id, second_rob_id=self.rob_id)
                 m.d.comb += should_write.eq(cmp_result.less)
