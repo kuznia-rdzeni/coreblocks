6fc5e4e908e4b1bec617edcca8cc722cb62cb82f
Piotro <piotro888@wp.pl>
2023-06-27 23:11:31 +0200

    Remove uneeded fields again

--- coreblocks/params/layouts.py
+++ coreblocks/params/layouts.py
@@ -1,4 +1,4 @@
-from coreblocks.params import GenParams, OpType, Funct7, Funct3, Opcode
+from coreblocks.params import GenParams, OpType, Funct7, Funct3
 from coreblocks.params.isa import ExceptionCause
 from coreblocks.utils.utils import layout_subset
 
@@ -45,7 +45,6 @@ class SchedulerLayouts:
     def __init__(self, gen_params: GenParams):
         common = gen_params.get(CommonLayouts)
         self.reg_alloc_in = [
-            ("opcode", Opcode),
             ("exec_fn", common.exec_fn),
             ("regs_l", common.regs_l),
             ("imm", gen_params.isa.xlen),
@@ -53,7 +52,6 @@ class SchedulerLayouts:
             ("pc", gen_params.isa.xlen),
         ]
         self.reg_alloc_out = self.renaming_in = [
-            ("opcode", Opcode),
             ("exec_fn", common.exec_fn),
             ("regs_l", common.regs_l),
             ("regs_p", [("rp_dst", gen_params.phys_regs_bits)]),
@@ -62,7 +60,6 @@ class SchedulerLayouts:
             ("pc", gen_params.isa.xlen),
         ]
         self.renaming_out = self.rob_allocate_in = [
-            ("opcode", Opcode),
             ("exec_fn", common.exec_fn),
             (
                 "regs_l",
@@ -77,7 +74,6 @@ class SchedulerLayouts:
             ("pc", gen_params.isa.xlen),
         ]
         self.rob_allocate_out = self.rs_select_in = [
-            ("opcode", Opcode),
             ("exec_fn", common.exec_fn),
             ("regs_p", common.regs_p),
             ("rob_id", gen_params.rob_entries_bits),
@@ -86,7 +82,6 @@ class SchedulerLayouts:
             ("pc", gen_params.isa.xlen),
         ]
         self.rs_select_out = self.rs_insert_in = [
-            ("opcode", Opcode),
             ("exec_fn", common.exec_fn),
             ("regs_p", common.regs_p),
             ("rob_id", gen_params.rob_entries_bits),
