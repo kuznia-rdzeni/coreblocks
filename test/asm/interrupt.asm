2e2270e8c97d6e0e0ff221ed530f33e55b3edd25
Kristopher38 <Kristopher38@wp.pl>
2023-06-30 22:35:47 +0200

    Test with conflicting registers

--- test/asm/interrupt.asm
+++ test/asm/interrupt.asm
@@ -1,10 +1,13 @@
-# fibonacci spiced with interrupt handler
-# TODO: register collisions, memory handling, various interrupt frequency
+# fibonacci spiced with interrupt handler (also with fibonacci)
+    li x30, 0     # interrupt count
     li x31, 0xde  # branch guard
     li x1, 0x100
     csrrw x0, mtvec, x1
     li x1, 0
     li x2, 1
+    li x5, 4
+    li x6, 7
+    li x7, 0
 loop:
     add x3, x2, x1
     mv x1, x2
@@ -14,18 +17,29 @@ infloop:
     j infloop
 
 int_handler:
-    # execute some dummy instructions
-    li x5, 420
-    li x6, 1337
-    li x7, -42
-    li x8, 38
-do_ops:
-    add x5, x5, x6
-    xor x6, x5, x7
-    sub x7, x5, x6
-    and x6, x7, x6
-    slli x6, x6, 3 
-    blt x0, x6, do_ops
+    # save main loop register state
+    mv x9, x1
+    mv x10, x2
+    mv x11, x3
+    addi x30, x30, 1
+    # load state
+    mv x1, x5
+    mv x2, x6
+    mv x3, x7
+    # fibonacci step
+    beq x3, x8, skip
+    add x3, x2, x1
+    mv x1, x2
+    mv x2, x3
+    # store state
+    mv x5, x1
+    mv x6, x2
+    mv x7, x3
+skip:
+    # restore main loop register state
+    mv x1, x9
+    mv x2, x10
+    mv x3, x11
     mret
 
 .org 0x100
