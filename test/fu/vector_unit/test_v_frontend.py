6e7c4fb89416f99c8b430eb9e8bc5b853eb53c9a
Lekcyjna <309016@uwr.edu.pl>
2023-07-07 19:50:51 +0200

    There is probably a bug in FifoRS and it push newest instruction when full.

--- test/fu/vector_unit/test_v_frontend.py
+++ test/fu/vector_unit/test_v_frontend.py
@@ -18,7 +18,8 @@ import copy
 class TestVectorFrontend(TestCaseWithSimulator):
     def setUp(self):
         random.seed(15)
-        self.test_number = 200
+        #  216 - OK, 217 NOK
+        self.test_number = 217
         self.gen_params = GenParams(test_core_config)
         self.v_params = VectorParameters(vlen = 1024, elen = 32)
         self.layouts = VectorFrontendLayouts(self.gen_params, self.v_params)
@@ -59,6 +60,8 @@ class TestVectorFrontend(TestCaseWithSimulator):
         self.received_block_interrupt = deque()
         self.received_retire = deque()
         self.received_mult = deque()
+        self._robs = deque()
+        self._org_robs = deque()
         
 
     @def_method_mock(lambda self: self.exception_report)
@@ -68,11 +71,14 @@ class TestVectorFrontend(TestCaseWithSimulator):
     @def_method_mock(lambda self: self.put_vvrs)
     def put_vvrs_process(self, arg):
         self.to_dealocate.append(arg["rp_dst"]["id"])
+        #print("rob_id", arg["rob_id"])
+        self._robs.append(arg["rob_id"])
         self.received_instr.append(arg)
 
     @def_method_mock(lambda self: self.put_mem)
     def put_mem_process(self, arg):
         self.to_dealocate.append(arg["rp_dst"]["id"])
+        self._robs.append(arg["rob_id"])
         self.received_instr_mem.append(arg)
 
     @def_method_mock(lambda self: self.rob_block_interrupt)
@@ -89,19 +95,42 @@ class TestVectorFrontend(TestCaseWithSimulator):
 
 
     def input_process(self):
-        for _ in range(self.test_number):
+        for i in range(self.test_number):
             instr, vtype = self.generate_vector_instr(self.gen_params, self.v_params, self.layouts.verification_in)
             while instr["exec_fn"]["op_type"] == OpType.V_CONTROL and instr["rp_s1"]["id"] == instr["rp_s2"]["id"]:
                 instr, vtype = self.generate_vector_instr(self.gen_params, self.v_params, self.layouts.verification_in)
+            print(i)
+            print(instr)
+            print(vtype)
             self.orginal_instr.append((instr, vtype))
+            if instr["exec_fn"]["op_type"] != OpType.V_CONTROL:
+                self._org_robs.append(instr["rob_id"])
             yield from self.circ.select.call()
             yield from self.circ.insert.call(rs_entry_id=0, rs_data = instr)
             yield from self.circ.update.call(tag = instr["rp_s1"], value = instr["s1_val"])
             yield from self.circ.update.call(tag = instr["rp_s2"], value = instr["s2_val"])
             yield from self.tick(random.randrange(3))
+        print("KONIEC WEJSCIA")
+
+    def remove_duplicates(self, lista):
+        nowa =[lista[0]]
+        for e in lista:
+            if nowa[-1]!=e:
+                nowa.append(e)
+        return nowa
+
 
     def checker(self):
         while len(self.received_mult) + len(self.received_retire) < self.test_number:
+            if (yield Now())==4900:
+                print(len(self.received_mult) , len(self.received_retire))
+                print(len(self.received_instr), len(self.received_instr_mem))
+                print(self.received_mult)
+                print(sum(self.received_mult))
+                print(self._robs)
+                print(self.remove_duplicates(self._robs)[200:])
+                print(list(self._org_robs)[200:])
+                #self.assertIterableEqual(self.remove_duplicates(self._robs), self._org_robs)
             yield
         # be sure that there is no other instructions in pipeline
         yield from self.tick(10)
@@ -135,7 +164,7 @@ class TestVectorFrontend(TestCaseWithSimulator):
             #yield from self.tick(random.randrange(3))
 
     def test_random(self):
-        with self.run_simulation(self.m, 15000) as sim:
+        with self.run_simulation(self.m, 5000) as sim:
             sim.add_sync_process(self.checker)
             sim.add_sync_process(self.input_process)
             sim.add_sync_process(self.put_vvrs_process)
