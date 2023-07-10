918b9d5e1a7fdf25361b14a51b35afaf1ec932d2
github-action-benchmark <github@users.noreply.github.com>
2023-07-10 10:28:23 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 6344da745a97aa22bec2c6dad67357c97e001494

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688984425146,
+  "lastUpdate": 1688984903712,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -1791,6 +1791,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "statemate",
             "value": 0.21143582629957916,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Kristopher38@wp.pl",
+            "name": "Krzysztof Obłonczek",
+            "username": "Kristopher38"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6344da745a97aa22bec2c6dad67357c97e001494",
+          "message": "Use all available cores when compiling cocotb regression tests with verilator (#430)",
+          "timestamp": "2023-07-10T12:07:24+02:00",
+          "tree_id": "493a60b72edba7b70036367f533ccb07467a123d",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/6344da745a97aa22bec2c6dad67357c97e001494"
+        },
+        "date": 1688984902164,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "statemate",
+            "value": 0.21143582629957916,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.287102112304355,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.15476224925165344,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.36868634053500093,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.24225926200771405,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.2159196483640026,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.29403051593959734,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.38400652367450455,
             "unit": "Instructions Per Cycle"
           }
         ]
