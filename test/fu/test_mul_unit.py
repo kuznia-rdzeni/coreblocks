fcba37657d00309fee22f63d45d1341918d2012d
Marek Materzok <tilk@tilk.eu>
2023-07-11 21:34:19 +0200

    Improve GenericFunctionalTestUnit (#433)

--- test/fu/test_mul_unit.py
+++ test/fu/test_mul_unit.py
@@ -1,46 +1,15 @@
 from parameterized import parameterized_class
-from typing import Dict
 
 from coreblocks.params import *
 from coreblocks.fu.mul_unit import MulFn, MulComponent, MulType
-from coreblocks.params.configurations import test_core_config
-from coreblocks.params.fu_params import FunctionalComponentParams
 
 from test.common import signed_to_int, int_to_signed
 
-from test.fu.functional_common import GenericFunctionalTestUnit
-
-
-def compute_result(i1: int, i2: int, i_imm: int, pc: int, fn: MulFn.Fn, xlen: int) -> Dict[str, int]:
-    signed_i1 = signed_to_int(i1, xlen)
-    signed_i2 = signed_to_int(i2, xlen)
-    if fn == MulFn.Fn.MUL:
-        return {"result": (i1 * i2) % (2**xlen)}
-    elif fn == MulFn.Fn.MULH:
-        return {"result": int_to_signed(signed_i1 * signed_i2, 2 * xlen) // (2**xlen)}
-    elif fn == MulFn.Fn.MULHU:
-        return {"result": i1 * i2 // (2**xlen)}
-    elif fn == MulFn.Fn.MULHSU:
-        return {"result": int_to_signed(signed_i1 * i2, 2 * xlen) // (2**xlen)}
-    else:
-        signed_half_i1 = signed_to_int(i1 % (2 ** (xlen // 2)), xlen // 2)
-        signed_half_i2 = signed_to_int(i2 % (2 ** (xlen // 2)), xlen // 2)
-        return {"result": int_to_signed(signed_half_i1 * signed_half_i2, xlen)}
-
-
-ops = {
-    MulFn.Fn.MUL: {"op_type": OpType.MUL, "funct3": Funct3.MUL, "funct7": Funct7.MULDIV},
-    MulFn.Fn.MULH: {"op_type": OpType.MUL, "funct3": Funct3.MULH, "funct7": Funct7.MULDIV},
-    MulFn.Fn.MULHU: {"op_type": OpType.MUL, "funct3": Funct3.MULHU, "funct7": Funct7.MULDIV},
-    MulFn.Fn.MULHSU: {"op_type": OpType.MUL, "funct3": Funct3.MULHSU, "funct7": Funct7.MULDIV},
-    #  Prepared for RV64
-    #
-    #  MulFn.Fn.MULW: {"op_type": OpType.ARITHMETIC_W, "funct3": Funct3.MULW, "funct7": Funct7.MULDIV},
-}
+from test.fu.functional_common import ExecFn, FunctionalUnitTestCase
 
 
 @parameterized_class(
-    ("name", "mul_unit"),
+    ("name", "func_unit"),
     [
         (
             "recursive_multiplier",
@@ -56,19 +25,35 @@ ops = {
         ),
     ],
 )
-class MultiplierUnitTest(GenericFunctionalTestUnit):
-    mul_unit: FunctionalComponentParams
+class MultiplierUnitTest(FunctionalUnitTestCase[MulFn.Fn]):
+    ops = {
+        MulFn.Fn.MUL: ExecFn(OpType.MUL, Funct3.MUL, Funct7.MULDIV),
+        MulFn.Fn.MULH: ExecFn(OpType.MUL, Funct3.MULH, Funct7.MULDIV),
+        MulFn.Fn.MULHU: ExecFn(OpType.MUL, Funct3.MULHU, Funct7.MULDIV),
+        MulFn.Fn.MULHSU: ExecFn(OpType.MUL, Funct3.MULHSU, Funct7.MULDIV),
+        #  Prepared for RV64
+        #
+        #  MulFn.Fn.MULW: ExecFn(OpType.ARITHMETIC_W, Funct3.MULW, Funct7.MULDIV),
+    }
 
-    def test_test(self):
-        self.run_pipeline()
+    @staticmethod
+    def compute_result(i1: int, i2: int, i_imm: int, pc: int, fn: MulFn.Fn, xlen: int) -> dict[str, int]:
+        signed_i1 = signed_to_int(i1, xlen)
+        signed_i2 = signed_to_int(i2, xlen)
 
-    def __init__(self, method_name: str = "runTest"):
-        super().__init__(
-            ops,
-            self.mul_unit,
-            compute_result,
-            gen=GenParams(test_core_config),
-            number_of_tests=600,
-            seed=32323,
-            method_name=method_name,
-        )
+        match fn:
+            case MulFn.Fn.MUL:
+                return {"result": (i1 * i2) % (2**xlen)}
+            case MulFn.Fn.MULH:
+                return {"result": int_to_signed(signed_i1 * signed_i2, 2 * xlen) // (2**xlen)}
+            case MulFn.Fn.MULHU:
+                return {"result": i1 * i2 // (2**xlen)}
+            case MulFn.Fn.MULHSU:
+                return {"result": int_to_signed(signed_i1 * i2, 2 * xlen) // (2**xlen)}
+            case _:
+                signed_half_i1 = signed_to_int(i1 % (2 ** (xlen // 2)), xlen // 2)
+                signed_half_i2 = signed_to_int(i2 % (2 ** (xlen // 2)), xlen // 2)
+                return {"result": int_to_signed(signed_half_i1 * signed_half_i2, xlen)}
+
+    def test_fu(self):
+        self.run_standard_fu_test()
