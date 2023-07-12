463116fc75c0864da79d70b7a04edb4acfacfcaf
github-action-benchmark <github@users.noreply.github.com>
2023-07-12 08:30:48 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 5bf55b86d82d6fb4f4e163c33754466c993e4224

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689150533137,
+  "lastUpdate": 1689150648420,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -1978,6 +1978,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "aha-mont64",
             "value": 0.287102112304355,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.24225926200771405,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "piotro888@wp.pl",
+            "name": "piotro888",
+            "username": "piotro888"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5bf55b86d82d6fb4f4e163c33754466c993e4224",
+          "message": "DummyLSU: add FENCE (#441)",
+          "timestamp": "2023-07-12T10:10:13+02:00",
+          "tree_id": "8bc43b04a38aa1a8e2bf0e1e4364af706bc91af9",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/5bf55b86d82d6fb4f4e163c33754466c993e4224"
+        },
+        "date": 1689150646898,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "slre",
+            "value": 0.2159196483640026,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.36868634053500093,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.21143582629957916,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.29403051593959734,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.38400652367450455,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.287102112304355,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.15476224925165344,
             "unit": "Instructions Per Cycle"
           },
           {
