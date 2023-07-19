62a33fb1a642f1cd03ceb32fe3adb5340cd789ae
Lekcyjna <309016@uwr.edu.pl>
2023-07-19 16:41:26 +0200

    Fix VectorCore tests.

--- test/fu/vector_unit/common.py
+++ test/fu/vector_unit/common.py
@@ -13,11 +13,10 @@ def generate_vsetvl(
     last_vl: int = 0,
     const_lmul: Optional[LMUL] = None,
     allow_illegal: bool = False,
-    max_vl : Optional[int] = None,
-    **kwargs
+    max_vl : Optional[int] = None
 ):
     v_params = gen_params.v_params
-    instr = generate_instr(gen_params, layout, max_vl = max_vl, **kwargs)
+    instr = generate_instr(gen_params, layout, max_vl = max_vl)
     vtype = generate_vtype(gen_params, max_vl = max_vl)
     if const_lmul is not None:
         vtype["lmul"] = const_lmul
@@ -82,16 +81,16 @@ def get_vector_instr_generator():
         nonlocal first_instr
         nonlocal last_vl
         if first_instr:
-            instr, last_vtype = generate_vsetvl(gen_params, layout, const_lmul=const_lmul, max_vl = max_vl, **kwargs)
+            instr, last_vtype = generate_vsetvl(gen_params, layout, const_lmul=const_lmul, max_vl = max_vl)
             first_instr = False
             instr = edit_rob_id(instr)
             return instr, last_vtype
 
         instr = generate_instr(gen_params, layout, max_vl = max_vl, **kwargs)
         if instr["exec_fn"]["op_type"] == OpType.V_CONTROL or (not_balanced_vsetvl and random.randrange(2)):
-            instr, last_vtype = generate_vsetvl(gen_params, layout, const_lmul=const_lmul, max_vl = max_vl, last_vl = last_vl, **kwargs)
+            instr, last_vtype = generate_vsetvl(gen_params, layout, const_lmul=const_lmul, max_vl = max_vl, last_vl = last_vl)
             while vsetvl_different_rp_id and instr["rp_s1"]["id"] == instr["rp_s2"]["id"]:
-                instr, last_vtype = generate_vsetvl(gen_params, layout, const_lmul=const_lmul, max_vl = max_vl, last_vl = last_vl, **kwargs)
+                instr, last_vtype = generate_vsetvl(gen_params, layout, const_lmul=const_lmul, max_vl = max_vl, last_vl = last_vl)
             last_vl = last_vtype["vl"]
 
         instr = edit_rob_id(instr)
