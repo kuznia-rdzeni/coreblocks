07f0907f84c2715aa5f2f13a3aaadb8be24c66d5
Lekcyjna <309016@uwr.edu.pl>
2023-07-19 10:45:22 +0200

    Issues with VCD.

--- coreblocks/structs_common/scoreboard.py
+++ coreblocks/structs_common/scoreboard.py
@@ -47,7 +47,7 @@ class Scoreboard(Elaboratable):
         )
 
         if self.data_forward:
-            data_forward = Signal(self.entries_number, name= "dato_forward")
+            data_forward = Signal(self.entries_number, name= "data_forward")
             data_forward_valid = Signal(self.entries_number, name="data_forward_valid")
 
         @loop_def_method(m, self.set_dirty_list, ready_list=checker.valids)
