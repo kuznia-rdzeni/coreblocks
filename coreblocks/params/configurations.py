3e74048b7224dc4f292105300d70ccaa0b3fd37e
Lekcyjna <309016@uwr.edu.pl>
2023-07-30 17:09:59 +0200

    Bump number of lines to 2

--- coreblocks/params/configurations.py
+++ coreblocks/params/configurations.py
@@ -191,7 +191,7 @@ test_vector_core_config = CoreConfiguration(
 # Core configuration with vector extension
 vector_core_config = CoreConfiguration(
     allow_partial_extensions=True,
-    vector_config=VectorUnitConfiguration(register_bank_count=1),
+    vector_config=VectorUnitConfiguration(register_bank_count=2),
     func_units_config=(
         RSBlockComponent(
             [
