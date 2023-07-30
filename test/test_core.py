bc9fa74a179489bd74d6c1ac36f6d600da7fa36e
Lekcyjna <309016@uwr.edu.pl>
2023-07-30 13:28:51 +0200

    Cut critical path in ALU.

--- test/test_core.py
+++ test/test_core.py
@@ -240,7 +240,7 @@ class TestCoreRandomized(TestCoreBase):
 @parameterized_class(
     ("name", "source_file", "cycle_count", "expected_regvals", "configuration"),
     [
-        ("fibonacci", "fibonacci.asm", 830, {2: 2971215073}, basic_core_config),
+        ("fibonacci", "fibonacci.asm", 900, {2: 2971215073}, basic_core_config),
         ("fibonacci_mem", "fibonacci_mem.asm", 570, {3: 55}, basic_core_config),
         ("csr", "csr.asm", 200, {1: 1, 2: 4}, full_core_config),
         (
