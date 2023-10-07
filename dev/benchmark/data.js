93dd88cf98b8c7a668c16ad1c03082567d54dc88
github-action-benchmark <github@users.noreply.github.com>
2023-10-07 13:12:49 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for aff89a1d51d0e2d9e788998fb755fc9fd1c3da22

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696683426346,
+  "lastUpdate": 1696684368993,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -2559,6 +2559,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "statemate",
             "value": 0.21143582629957916,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aff89a1d51d0e2d9e788998fb755fc9fd1c3da22",
+          "message": "Autumn cleaning part 2 - split transactron/lib.py (#464)",
+          "timestamp": "2023-10-07T14:47:50+02:00",
+          "tree_id": "e6ddc0deae7e6a4b9a730a6639b1d996bcb6f0d2",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/aff89a1d51d0e2d9e788998fb755fc9fd1c3da22"
+        },
+        "date": 1696684367272,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.287102112304355,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.21143582629957916,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.29403051593959734,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.36868634053500093,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.15476224925165344,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.2159196483640026,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.38400652367450455,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.24225926200771405,
             "unit": "Instructions Per Cycle"
           }
         ]
