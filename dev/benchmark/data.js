fc1d7a74eef5a7329a5719fc11dd833f73ba5f81
github-action-benchmark <github@users.noreply.github.com>
2023-11-05 20:35:32 +0000

    add Fmax and LCs (basic) (customBiggerIsBetter) benchmark result for d0ebfc0b2a19a710c1a537e0ddcfb8b9fe888752

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698942218390,
+  "lastUpdate": 1699216531963,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -12320,6 +12320,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 438,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 884,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 5619,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d0ebfc0b2a19a710c1a537e0ddcfb8b9fe888752",
+          "message": "Fix zbs and DecoderManager docstrings (#470)",
+          "timestamp": "2023-11-05T21:29:29+01:00",
+          "tree_id": "6fea5f7feeaac6511c81a90a5b1de5764d38dd9b",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/d0ebfc0b2a19a710c1a537e0ddcfb8b9fe888752"
+        },
+        "date": 1699216529532,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 56.3,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 17433,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 470,
             "unit": "LUT"
           },
           {
