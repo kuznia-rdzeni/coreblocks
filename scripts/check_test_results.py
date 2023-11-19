eef936e518f8e71bc46e0e2287dae8f55e3cb7c2
Maksymilian Komarnicki <maksymilian.komarnicki@wp.pl>
2023-11-19 02:44:56 +0100

    Add prints

--- scripts/check_test_results.py
+++ scripts/check_test_results.py
@@ -10,8 +10,13 @@ TOP_DIR = pathlib.Path(__file__).parent.parent
 TEST_RESULTS_FILE = TOP_DIR.joinpath("test/regression/cocotb/results.xml")
 
 if not os.path.exists(TEST_RESULTS_FILE):
+    print("File not found: ", TEST_RESULTS_FILE)
     sys.exit(1)
 
 tree = eT.parse(TEST_RESULTS_FILE)
+
 if len(tree.findall(FAILURE_TAG)) > 0:
+    print("Some regression tests failed")
     sys.exit(1)
+
+print("All regression tests pass")
