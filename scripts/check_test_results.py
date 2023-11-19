9fe1d26d80522c4acdacd68238e0883ae9b2407e
Maksymilian Komarnicki <maksymilian.komarnicki@wp.pl>
2023-11-19 01:54:57 +0100

    Change the way of checking regression test results

---
+++ scripts/check_test_results.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import pathlib
+import xml.etree.ElementTree as ET
+
+FAILURE_TAG = 'failure'
+TOP_DIR = pathlib.Path(__file__).parent.parent
+TEST_RESULTS_FILE = TOP_DIR.joinpath('test/regression/cocotb/results.xml')
+
+if not os.path.exists(TEST_RESULTS_FILE):
+    sys.exit(1)
+
+tree = ET.parse(TEST_RESULTS_FILE)
+if len(tree.findall(FAILURE_TAG)) > 0:
+    sys.exit(1)
