5a007d7ea90bfcf187ff7f31746a50b6fa861166
Maksymilian Komarnicki <maksymilian.komarnicki@wp.pl>
2023-11-19 02:10:17 +0100

    Fix formatting

--- scripts/check_test_results.py
+++ scripts/check_test_results.py
@@ -3,15 +3,15 @@
 import sys
 import os
 import pathlib
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as eT
 
-FAILURE_TAG = 'failure'
+FAILURE_TAG = "failure"
 TOP_DIR = pathlib.Path(__file__).parent.parent
-TEST_RESULTS_FILE = TOP_DIR.joinpath('test/regression/cocotb/results.xml')
+TEST_RESULTS_FILE = TOP_DIR.joinpath("test/regression/cocotb/results.xml")
 
 if not os.path.exists(TEST_RESULTS_FILE):
     sys.exit(1)
 
-tree = ET.parse(TEST_RESULTS_FILE)
+tree = eT.parse(TEST_RESULTS_FILE)
 if len(tree.findall(FAILURE_TAG)) > 0:
     sys.exit(1)
