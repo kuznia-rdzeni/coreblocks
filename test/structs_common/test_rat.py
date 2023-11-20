55c11a4223195ad975fa29f83688dca1bf497450
JumiDeluxe <jmatuszewska00e@gmail.com>
2023-11-20 15:50:07 +0100

    RAT tests: run gen_input() before simulation

--- test/structs_common/test_rat.py
+++ test/structs_common/test_rat.py
@@ -10,8 +10,6 @@ from random import Random
 
 class TestFrontendRegisterAliasTable(TestCaseWithSimulator):
     def gen_input(self):
-        while len(self.expected_entries) < self.log_regs:
-            yield
         for _ in range(self.test_steps):
             rl = self.rand.randrange(self.gen_params.isa.reg_cnt)
             rp = self.rand.randrange(1, 2**self.gen_params.phys_regs_bits) if rl != 0 else 0
@@ -22,8 +20,6 @@ class TestFrontendRegisterAliasTable(TestCaseWithSimulator):
 
     def do_rename(self):
         for _ in range(self.test_steps):
-            while len(self.to_execute_list) == 0:
-                yield
             to_execute = self.to_execute_list.pop()
             res = yield from self.m.rename.call(
                 rl_dst=to_execute["rl"], rp_dst=to_execute["rp"], rl_s1=to_execute["rl_s1"], rl_s2=to_execute["rl_s2"]
@@ -46,15 +42,13 @@ class TestFrontendRegisterAliasTable(TestCaseWithSimulator):
         self.to_execute_list = deque()
         self.expected_entries = [0 for _ in range(self.log_regs)]
 
+        self.gen_input()
         with self.run_simulation(m) as sim:
-            sim.add_sync_process(self.gen_input)
             sim.add_sync_process(self.do_rename)
 
 
 class TestRetirementRegisterAliasTable(TestCaseWithSimulator):
     def gen_input(self):
-        while len(self.expected_entries) < self.log_regs:
-            yield
         for _ in range(self.test_steps):
             rl = self.rand.randrange(self.gen_params.isa.reg_cnt)
             rp = self.rand.randrange(1, 2**self.gen_params.phys_regs_bits) if rl != 0 else 0
@@ -64,8 +58,6 @@ class TestRetirementRegisterAliasTable(TestCaseWithSimulator):
 
     def do_commit(self):
         for _ in range(self.test_steps):
-            while len(self.to_execute_list) == 0:
-                yield
             to_execute = self.to_execute_list.pop()
             res = yield from self.m.commit.call(
                 rl_dst=to_execute["rl"], rp_dst=to_execute["rp"], side_fx=to_execute["side_fx"]
@@ -88,6 +80,6 @@ class TestRetirementRegisterAliasTable(TestCaseWithSimulator):
         self.to_execute_list = deque()
         self.expected_entries = [0 for _ in range(self.log_regs)]
 
+        self.gen_input()
         with self.run_simulation(m) as sim:
-            sim.add_sync_process(self.gen_input)
             sim.add_sync_process(self.do_commit)
