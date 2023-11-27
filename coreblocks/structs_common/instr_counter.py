742b1d9f417f653cabddf3139179d8f724d6b253
Piotr Wegrzyn <piotro@piotro.eu>
2023-11-27 22:39:56 +0100

    Instr counter docstring + local field

--- coreblocks/structs_common/instr_counter.py
+++ coreblocks/structs_common/instr_counter.py
@@ -7,23 +7,33 @@ from transactron.core import Method, TModule, def_method
 class CoreInstructionCounter(Elaboratable):
     """
     Counts instructions currently processed in core.
-    Used in exception handling, to wait for core flush to finsh.
+    Used in exception handling, to wait for core flush to finish.
+
+    Attributes
+    ----------
+    increment : Method
+        Increments the counter. Should be called when new instruction leaves fetch stage.
+    decrement : Method
+        Decrements the counter, and returns if the counter will be equal to zero after that cycle (it was the
+        last instruction in core and no new instruction is fetched). Should be called when instruction is retired.
     """
 
     def __init__(self, gp: GenParams):
+        self.gp = gp
+
         self.increment = Method()
         self.decrement = Method(o=gp.get(CoreInstructionCounterLayouts).decrement)
-
-        self.count = Signal(gp.rob_entries_bits + 1)
 
     def elaborate(self, platform) -> TModule:
         m = TModule()
 
+        count = Signal(self.gp.rob_entries_bits + 1)
+
         with m.If(self.increment.run & ~self.decrement.run):
-            m.d.sync += self.count.eq(self.count + 1)
+            m.d.sync += count.eq(count + 1)
 
         with m.If(self.decrement.run & ~self.increment.run):
-            m.d.sync += self.count.eq(self.count - 1)
+            m.d.sync += count.eq(count - 1)
 
         @def_method(m, self.increment)
         def _():
@@ -31,6 +41,6 @@ class CoreInstructionCounter(Elaboratable):
 
         @def_method(m, self.decrement)
         def _():
-            return self.count == 1 & ~self.increment.run
+            return count == 1 & ~self.increment.run
 
         return m
