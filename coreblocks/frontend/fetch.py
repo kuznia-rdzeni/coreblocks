95c0dde93ab984094f1730ef6629f48c3ef8ceb0
Piotr Wegrzyn <piotro@piotro.eu>
2023-12-16 21:28:09 +0100

    Try remove conflict (tmp)

--- coreblocks/frontend/fetch.py
+++ coreblocks/frontend/fetch.py
@@ -32,7 +32,7 @@ class Fetch(Elaboratable):
 
         self.verify_branch = Method(i=self.gp.get(FetchLayouts).branch_verify)
         self.stall_exception = Method()
-        self.stall_exception.add_conflict(self.verify_branch, Priority.LEFT)
+       # self.stall_exception.add_conflict(self.verify_branch, Priority.LEFT)
 
         # PC of the last fetched instruction. For now only used in tests.
         self.pc = Signal(self.gp.isa.xlen)
@@ -132,7 +132,7 @@ class UnalignedFetch(Elaboratable):
 
         self.verify_branch = Method(i=self.gp.get(FetchLayouts).branch_verify)
         self.stall_exception = Method()
-        self.stall_exception.add_conflict(self.verify_branch, Priority.LEFT)
+        #self.stall_exception.add_conflict(self.verify_branch, Priority.LEFT)
 
         # PC of the last fetched instruction. For now only used in tests.
         self.pc = Signal(self.gp.isa.xlen)
