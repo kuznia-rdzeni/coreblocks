eae574f8b5c4db0e6d10f690376032a4af5def1f
github-action-benchmark <github@users.noreply.github.com>
2023-12-21 20:20:51 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for fe081910ca0d7b4092f7e952ba140f55c3dd3c06

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1703189559647,
+  "lastUpdate": 1703190051577,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -5375,6 +5375,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "nsichneu",
             "value": 0.2393345134151435,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe081910ca0d7b4092f7e952ba140f55c3dd3c06",
+          "message": "Source locations for transactions and methods (#542)",
+          "timestamp": "2023-12-21T21:04:53+01:00",
+          "tree_id": "2b4aec9d6aa59fa33bb63ffe64564368118cee56",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/fe081910ca0d7b4092f7e952ba140f55c3dd3c06"
+        },
+        "date": 1703190049800,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nsichneu",
+            "value": 0.2393345134151435,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.3262149437190531,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.32227830884465813,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.4985200128202711,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.23461730602371625,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.2562424141349566,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.2760578944944976,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.43425182528066597,
             "unit": "Instructions Per Cycle"
           }
         ]
