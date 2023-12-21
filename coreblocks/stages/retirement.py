c52a03c1f57d0b8aef793c2ff7f68ec5436ce4a8
Piotr Wegrzyn <piotro@piotro.eu>
2023-12-21 10:46:52 +0100

    Use condition

--- coreblocks/stages/retirement.py
+++ coreblocks/stages/retirement.py
@@ -1,10 +1,12 @@
 from amaranth import *
+
+from transactron.core import Method, Transaction, TModule
+from transactron.lib.simultaneous import condition
+
+from coreblocks.params.genparams import GenParams
 from coreblocks.params.dependencies import DependencyManager
 from coreblocks.params.isa import ExceptionCause
 from coreblocks.params.keys import GenericCSRRegistersKey
-
-from transactron.core import Method, Transaction, TModule
-from coreblocks.params.genparams import GenParams
 from coreblocks.structs_common.csr_generic import CSRAddress, DoubleCounterCSR
 
 
@@ -68,24 +70,24 @@ class Retirement(Elaboratable):
             with m.If(rp_dst):  # don't put rp0 to free list - reserved to no-return instructions
                 self.free_rf_put(m, rp_dst)
 
-        def retire_instr(rob_entry) -> Value:
+        def retire_instr(rob_entry):
             # set rl_dst -> rp_dst in R-RAT
             rat_out = self.r_rat_commit(m, rl_dst=rob_entry.rob_data.rl_dst, rp_dst=rob_entry.rob_data.rp_dst)
 
+            # free old rp_dst from overwritten R-RAT mapping
+            free_phys_reg(rat_out.old_rp_dst)
+
             self.instret_csr.increment(m)
 
-            # free old rp_dst from overwritten R-RAT mapping
-            return rat_out.old_rp_dst
-
-        def flush_instr(rob_entry) -> Value:
+        def flush_instr(rob_entry):
             # get original rp_dst mapped to instruction rl_dst in R-RAT
             rat_out = self.r_rat_peek(m, rl_dst=rob_entry.rob_data.rl_dst)
 
+            # free the "new" instruction rp_dst - result is flushed
+            free_phys_reg(rob_entry.rob_data.rp_dst)
+
             # restore original rl_dst->rp_dst mapping in F-RAT
             self.rename(m, rl_s1=0, rl_s2=0, rl_dst=rob_entry.rob_data.rl_dst, rp_dst=rat_out.old_rp_dst)
-
-            # free the "new" instruction rp_dst - result is flushed
-            return rob_entry.rob_data.rp_dst
 
         with m.FSM("NORMAL") as fsm:
             with m.State("NORMAL"):
@@ -134,12 +136,11 @@ class Retirement(Elaboratable):
                         m.d.av_comb += commit.eq(1)
 
                     # Methods cannot be called multiple times from the same Transaction >:(
-                    rp_to_free = Signal(self.gen_params.phys_regs_bits)
-                    with m.If(commit):
-                        m.d.av_comb += rp_to_free.eq(retire_instr(rob_entry))
-                    with m.Else():
-                        m.d.av_comb += rp_to_free.eq(flush_instr(rob_entry))
-                    free_phys_reg(rp_to_free)
+                    with condition(m, priority=False) as cond:
+                        with cond(commit):
+                            retire_instr(rob_entry)
+                        with cond(~commit):  # Not using default, because we want to block if condition is not ready
+                            flush_instr(rob_entry)
 
             with m.State("TRAP_FLUSH"):
                 with Transaction().body(m):
@@ -147,8 +148,7 @@ class Retirement(Elaboratable):
                     rob_entry = self.rob_retire(m)
                     core_empty = self.instr_decrement(m)
 
-                    free_reg = flush_instr(rob_entry)
-                    free_phys_reg(free_reg)
+                    flush_instr(rob_entry)
 
                     with m.If(core_empty):
                         m.next = "TRAP_RESUME"
