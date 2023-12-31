18e928c4f4c748288ae2e7c4522a81a05c99b387
Lekcyjna <309016@uwr.edu.pl>
2023-12-31 19:04:15 +0100

    Start of removing pytest warnings.

--- test/frontend/test_decode.py
+++ test/frontend/test_decode.py
@@ -1,47 +1,37 @@
-from amaranth import Elaboratable, Module
-
 from transactron.lib import AdapterTrans, FIFO
 
-from ..common import TestCaseWithSimulator, TestbenchIO
+from ..common import TestCaseWithSimulator, TestbenchIO, SimpleTestCircuit, ModuleConnector
 
 from coreblocks.frontend.decode import Decode
 from coreblocks.params import GenParams, FetchLayouts, DecodeLayouts, OpType, Funct3, Funct7
 from coreblocks.params.configurations import test_core_config
 
 
-class TestElaboratable(Elaboratable):
-    def __init__(self, gen_params: GenParams):
-        self.gen_params = gen_params
-
-    def elaborate(self, platform):
-        m = Module()
+class TestFetch(TestCaseWithSimulator):
+    def setUp(self) -> None:
+        self.gen_params = GenParams(test_core_config.replace(start_pc=24))
 
         fifo_in = FIFO(self.gen_params.get(FetchLayouts).raw_instr, depth=2)
         fifo_out = FIFO(self.gen_params.get(DecodeLayouts).decoded_instr, depth=2)
 
-        self.io_in = TestbenchIO(AdapterTrans(fifo_in.write))
-        self.io_out = TestbenchIO(AdapterTrans(fifo_out.read))
+        self.fifo_in_write = TestbenchIO(AdapterTrans(fifo_in.write))
+        self.fifo_out_read = TestbenchIO(AdapterTrans(fifo_out.read))
 
         self.decode = Decode(self.gen_params, fifo_in.read, fifo_out.write)
-
-        m.submodules.decode = self.decode
-        m.submodules.io_in = self.io_in
-        m.submodules.io_out = self.io_out
-        m.submodules.fifo_in = fifo_in
-        m.submodules.fifo_out = fifo_out
-
-        return m
-
-
-class TestFetch(TestCaseWithSimulator):
-    def setUp(self) -> None:
-        self.gen_params = GenParams(test_core_config.replace(start_pc=24))
-        self.test_module = TestElaboratable(self.gen_params)
+        self.m = SimpleTestCircuit(
+            ModuleConnector(
+                decode=self.decode,
+                fifo_in=fifo_in,
+                fifo_out=fifo_out,
+                fifo_in_write=self.fifo_in_write,
+                fifo_out_read=self.fifo_out_read,
+            )
+        )
 
     def decode_test_proc(self):
         # testing an OP_IMM instruction (test copied from test_decoder.py)
-        yield from self.test_module.io_in.call(instr=0x02A28213)
-        decoded = yield from self.test_module.io_out.call()
+        yield from self.fifo_in_write.call(instr=0x02A28213)
+        decoded = yield from self.fifo_out_read.call()
 
         self.assertEqual(decoded["exec_fn"]["op_type"], OpType.ARITHMETIC)
         self.assertEqual(decoded["exec_fn"]["funct3"], Funct3.ADD)
@@ -52,8 +42,8 @@ class TestFetch(TestCaseWithSimulator):
         self.assertEqual(decoded["imm"], 42)
 
         # testing an OP instruction (test copied from test_decoder.py)
-        yield from self.test_module.io_in.call(instr=0x003100B3)
-        decoded = yield from self.test_module.io_out.call()
+        yield from self.fifo_in_write.call(instr=0x003100B3)
+        decoded = yield from self.fifo_out_read.call()
 
         self.assertEqual(decoded["exec_fn"]["op_type"], OpType.ARITHMETIC)
         self.assertEqual(decoded["exec_fn"]["funct3"], Funct3.ADD)
@@ -63,8 +53,8 @@ class TestFetch(TestCaseWithSimulator):
         self.assertEqual(decoded["regs_l"]["rl_s2"], 3)
 
         # testing an illegal
-        yield from self.test_module.io_in.call(instr=0x0)
-        decoded = yield from self.test_module.io_out.call()
+        yield from self.fifo_in_write.call(instr=0x0)
+        decoded = yield from self.fifo_out_read.call()
 
         self.assertEqual(decoded["exec_fn"]["op_type"], OpType.EXCEPTION)
         self.assertEqual(decoded["exec_fn"]["funct3"], Funct3._EILLEGALINSTR)
@@ -73,8 +63,8 @@ class TestFetch(TestCaseWithSimulator):
         self.assertEqual(decoded["regs_l"]["rl_s1"], 0)
         self.assertEqual(decoded["regs_l"]["rl_s2"], 0)
 
-        yield from self.test_module.io_in.call(instr=0x0, access_fault=1)
-        decoded = yield from self.test_module.io_out.call()
+        yield from self.fifo_in_write.call(instr=0x0, access_fault=1)
+        decoded = yield from self.fifo_out_read.call()
 
         self.assertEqual(decoded["exec_fn"]["op_type"], OpType.EXCEPTION)
         self.assertEqual(decoded["exec_fn"]["funct3"], Funct3._EINSTRACCESSFAULT)
@@ -84,5 +74,5 @@ class TestFetch(TestCaseWithSimulator):
         self.assertEqual(decoded["regs_l"]["rl_s2"], 0)
 
     def test(self):
-        with self.run_simulation(self.test_module) as sim:
+        with self.run_simulation(self.m) as sim:
             sim.add_sync_process(self.decode_test_proc)
