d71afc0d2edd860adb22002d195861d5348824cb
github-action-benchmark <github@users.noreply.github.com>
2024-01-06 11:08:59 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for f242864779e4f874eff33cc3493b13a8ed8a6d28

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704538877275,
+  "lastUpdate": 1704539339755,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -5631,6 +5631,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "statemate",
             "value": 0.2783650349000975,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f242864779e4f874eff33cc3493b13a8ed8a6d28",
+          "message": "Move dependency related classes to Transactron (#544)",
+          "timestamp": "2024-01-06T11:52:19+01:00",
+          "tree_id": "421e4d6fd56748288c653381b8f2cef3a6fb1739",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/f242864779e4f874eff33cc3493b13a8ed8a6d28"
+        },
+        "date": 1704539338072,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ud",
+            "value": 0.3332441948567099,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.34174027385235617,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.4343032651645274,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.2783650349000975,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.26240061481242527,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.25211480106460527,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.2420362948846237,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.50015132026935,
             "unit": "Instructions Per Cycle"
           }
         ]
