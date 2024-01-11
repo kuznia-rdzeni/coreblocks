fd47595b100aa165c5ad1c295023f62b48ad674c
github-action-benchmark <github@users.noreply.github.com>
2024-01-11 18:56:03 +0000

    add Fmax and LCs (basic) (customBiggerIsBetter) benchmark result for 7af6a66e7f6b7e365ad13db8aaaefa2bcf92df0e

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704999338361,
+  "lastUpdate": 1704999363033,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -17115,6 +17115,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 720,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 988,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 6477,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "piotro@piotro.eu",
+            "name": "piotro888",
+            "username": "piotro888"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7af6a66e7f6b7e365ad13db8aaaefa2bcf92df0e",
+          "message": "Retirement test fix (#562)",
+          "timestamp": "2024-01-11T19:48:20+01:00",
+          "tree_id": "3f109ffcf7c0415e2c26d28dcef09b472d2eb4a0",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/7af6a66e7f6b7e365ad13db8aaaefa2bcf92df0e"
+        },
+        "date": 1704999361219,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 48.44,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 31112,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 688,
             "unit": "LUT"
           },
           {
