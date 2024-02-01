d74adec5dcc7544eabd9da36056f2ab5e8bbffe1
github-action-benchmark <github@users.noreply.github.com>
2024-02-01 12:05:42 +0000

    add Fmax and LCs (full) (customBiggerIsBetter) benchmark result for 6fd151fbe2c1ffb4b37c2b38d50b62c7268f4336

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706789021187,
+  "lastUpdate": 1706789142724,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -22181,6 +22181,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Device utilisation: (ECP5)",
             "value": 23927,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1476,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1096,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 8538,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6fd151fbe2c1ffb4b37c2b38d50b62c7268f4336",
+          "message": "Update amaranth version to the v0.4.x head (#576)",
+          "timestamp": "2024-02-01T12:57:51+01:00",
+          "tree_id": "945f8df5108d25609b1e3673a697a409fd69f9c8",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/6fd151fbe2c1ffb4b37c2b38d50b62c7268f4336"
+        },
+        "date": 1706789140679,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 44.26,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 24122,
             "unit": "LUT4"
           },
           {
