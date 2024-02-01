9a2d9468ce5dc9f32a306054b1025f545d1a95ca
github-action-benchmark <github@users.noreply.github.com>
2024-02-01 12:17:09 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 6fd151fbe2c1ffb4b37c2b38d50b62c7268f4336

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706789142724,
+  "lastUpdate": 1706789829351,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -6463,6 +6463,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "ud",
             "value": 0.24238811346240433,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6fd151fbe2c1ffb4b37c2b38d50b62c7268f4336",
+          "message": "Update amaranth version to the v0.4.x head (#576)",
+          "timestamp": "2024-02-01T12:57:51+01:00",
+          "tree_id": "945f8df5108d25609b1e3673a697a409fd69f9c8",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/6fd151fbe2c1ffb4b37c2b38d50b62c7268f4336"
+        },
+        "date": 1706789827378,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "statemate",
+            "value": 0.2152619862603825,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.1814903648969226,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.24238811346240433,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.16937939273068717,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.464452895145921,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.3008663337319172,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.29959791363234617,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.2707173800277587,
             "unit": "Instructions Per Cycle"
           }
         ]
