b93af9f85833f24f618050b873e39ccbb05a0346
github-action-benchmark <github@users.noreply.github.com>
2024-02-05 10:56:44 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 7d2222e9637c6dc1339a7cc710f38b212078e393

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707130064103,
+  "lastUpdate": 1707130604361,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -6783,6 +6783,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "statemate",
             "value": 0.2152619862603825,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7d2222e9637c6dc1339a7cc710f38b212078e393",
+          "message": "Use DependencyManager as the global object (#581)",
+          "timestamp": "2024-02-05T11:40:44+01:00",
+          "tree_id": "14521b8d2073cd2a6efc23962b37c7751a9a1554",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/7d2222e9637c6dc1339a7cc710f38b212078e393"
+        },
+        "date": 1707130602258,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "statemate",
+            "value": 0.2152619862603825,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.3008663337319172,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.464452895145921,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.2707173800277587,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.29959791363234617,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.1814903648969226,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.24238811346240433,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.16937939273068717,
             "unit": "Instructions Per Cycle"
           }
         ]
