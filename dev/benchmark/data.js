89deb74c5268f25097d388392bc03eaa176802f5
github-action-benchmark <github@users.noreply.github.com>
2024-02-12 11:11:03 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 1b7b733bdc52d066344d260bd19972afe5056936

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707735824250,
+  "lastUpdate": 1707736263418,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -7039,6 +7039,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "crc32",
             "value": 0.3008663337319172,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1b7b733bdc52d066344d260bd19972afe5056936",
+          "message": "Create DependencyManager for gen_verilog.py, regression tests and synthesis (#584)",
+          "timestamp": "2024-02-12T11:55:16+01:00",
+          "tree_id": "f103211c6898e3509f5dbf452001d5ca0361dfc2",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/1b7b733bdc52d066344d260bd19972afe5056936"
+        },
+        "date": 1707736260921,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nettle-sha256",
+            "value": 0.464452895145921,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.29959791363234617,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.2707173800277587,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.2152619862603825,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.16937939273068717,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.24238811346240433,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.3008663337319172,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.1814903648969226,
             "unit": "Instructions Per Cycle"
           }
         ]
