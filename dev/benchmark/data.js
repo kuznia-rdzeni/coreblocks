376740c7fc9e0cb3c1bb7b5a17943825e4444cd6
github-action-benchmark <github@users.noreply.github.com>
2024-02-12 14:19:02 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 226cda0daa69092f3a93130dab4b76dbb1e82d35

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707747144669,
+  "lastUpdate": 1707747542761,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -7231,6 +7231,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "ud",
             "value": 0.24238811346240433,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "226cda0daa69092f3a93130dab4b76dbb1e82d35",
+          "message": "Enable hardware metrics in Coreblocks (#586)",
+          "timestamp": "2024-02-12T15:03:16+01:00",
+          "tree_id": "5e312ef0cb2a85e0f55c61451c88e9df74539228",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/226cda0daa69092f3a93130dab4b76dbb1e82d35"
+        },
+        "date": 1707747540929,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "crc32",
+            "value": 0.3008663337319172,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.24238811346240433,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.16937939273068717,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.2707173800277587,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.464452895145921,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.29959791363234617,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.1814903648969226,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.2152619862603825,
             "unit": "Instructions Per Cycle"
           }
         ]
