d60d141f1f0f51639091616fd08d22b2bf470cc6
github-action-benchmark <github@users.noreply.github.com>
2024-02-12 11:01:35 +0000

    add Fmax and LCs (basic) (customBiggerIsBetter) benchmark result for 1b7b733bdc52d066344d260bd19972afe5056936

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707729628470,
+  "lastUpdate": 1707735695839,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -19036,6 +19036,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 720,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 956,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 6524,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1b7b733bdc52d066344d260bd19972afe5056936",
+          "message": "Create DependencyManager for gen_verilog.py, regression tests and synthesis (#584)",
+          "timestamp": "2024-02-12T11:55:16+01:00",
+          "tree_id": "f103211c6898e3509f5dbf452001d5ca0361dfc2",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/1b7b733bdc52d066344d260bd19972afe5056936"
+        },
+        "date": 1707735694169,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 53.14,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 19273,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 688,
             "unit": "LUT"
           },
           {
