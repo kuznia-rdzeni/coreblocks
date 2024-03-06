242bf3533728c910ffe7470370e42e353e6f2283
github-action-benchmark <github@users.noreply.github.com>
2024-03-06 17:36:46 +0000

    add Fmax and LCs (full) (customBiggerIsBetter) benchmark result for cfe3369e26bd3c67ca42396c356b0b5f2576fc15

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709746471045,
+  "lastUpdate": 1709746606869,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -25750,6 +25750,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 1476,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1096,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 8538,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cfe3369e26bd3c67ca42396c356b0b5f2576fc15",
+          "message": "Fix the pysim regression. (#604)",
+          "timestamp": "2024-03-06T18:28:23+01:00",
+          "tree_id": "63e6f8c1515a52234afc700f8c0ae700f7253924",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/cfe3369e26bd3c67ca42396c356b0b5f2576fc15"
+        },
+        "date": 1709746604159,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 45.38,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 25121,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1444,
             "unit": "LUT"
           },
           {
