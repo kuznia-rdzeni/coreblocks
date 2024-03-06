745fd1ab5e758017a9b21f1f59b3c1d667f5827f
github-action-benchmark <github@users.noreply.github.com>
2024-03-06 17:43:53 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for cfe3369e26bd3c67ca42396c356b0b5f2576fc15

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709746606869,
+  "lastUpdate": 1709747033736,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -7871,6 +7871,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "slre",
             "value": 0.1814903648969226,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cfe3369e26bd3c67ca42396c356b0b5f2576fc15",
+          "message": "Fix the pysim regression. (#604)",
+          "timestamp": "2024-03-06T18:28:23+01:00",
+          "tree_id": "63e6f8c1515a52234afc700f8c0ae700f7253924",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/cfe3369e26bd3c67ca42396c356b0b5f2576fc15"
+        },
+        "date": 1709747031722,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ud",
+            "value": 0.24238811346240433,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.464452895145921,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.29959791363234617,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.3008663337319172,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.1814903648969226,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.16937939273068717,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.2707173800277587,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.2152619862603825,
             "unit": "Instructions Per Cycle"
           }
         ]
