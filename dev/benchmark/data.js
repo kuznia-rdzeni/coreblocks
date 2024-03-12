f9475bc661b4c2d2ea6e3a5c6cb1fbf2874be62b
github-action-benchmark <github@users.noreply.github.com>
2024-03-12 17:27:05 +0000

    add Fmax and LCs (basic) (customBiggerIsBetter) benchmark result for 58249da625a3b981af7d4bafab97b47b6e73f811

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710263169588,
+  "lastUpdate": 1710264425401,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -21183,6 +21183,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 720,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 956,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 6524,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "58249da625a3b981af7d4bafab97b47b6e73f811",
+          "message": "Enable division and bit manipulation extensions in benchmarks (#616)",
+          "timestamp": "2024-03-12T18:21:11+01:00",
+          "tree_id": "20a2240852458b68d61a12a6267d73f3b6ede8e7",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/58249da625a3b981af7d4bafab97b47b6e73f811"
+        },
+        "date": 1710264423130,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 52.47,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 21600,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 688,
             "unit": "LUT"
           },
           {
