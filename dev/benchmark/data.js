9311d1818b4793508edad63e73677b76169108fb
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 23:22:50 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 29e522aa0e280df39d56d893f097fc8e05f9d915

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711063090366,
+  "lastUpdate": 1711063370528,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -8831,6 +8831,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "statemate",
             "value": 0.21524395351898612,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29e522aa0e280df39d56d893f097fc8e05f9d915",
+          "message": "Allow parallel compilation of the verilated model (#623)",
+          "timestamp": "2024-03-22T00:11:36+01:00",
+          "tree_id": "329564344f4ef260e22edc8d4f5bf6e02c0cf7ab",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/29e522aa0e280df39d56d893f097fc8e05f9d915"
+        },
+        "date": 1711063367371,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "crc32",
+            "value": 0.26365515005807827,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.23775978551326782,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.18115635899199292,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.3246413600430343,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.21524395351898612,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.29959791363234617,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.4158309618409448,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.29204002838551374,
             "unit": "Instructions Per Cycle"
           }
         ]
