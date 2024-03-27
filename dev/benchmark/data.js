02be0ede1816282262490a1ea40b32d4981eeb61
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 16:07:24 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for a222ebdd0d575f9a96d50e2705f7123a496b3835

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711555454938,
+  "lastUpdate": 1711555644195,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -9023,6 +9023,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "crc32",
             "value": 0.26365515005807827,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a222ebdd0d575f9a96d50e2705f7123a496b3835",
+          "message": "Add selecting network circuit (#621)",
+          "timestamp": "2024-03-27T16:56:15+01:00",
+          "tree_id": "7299cbd4cb488eeca3682ae2172db9472493ee8a",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/a222ebdd0d575f9a96d50e2705f7123a496b3835"
+        },
+        "date": 1711555641934,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "statemate",
+            "value": 0.21524395351898612,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.23775978551326782,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.4158309618409448,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.29204002838551374,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.26365515005807827,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.3246413600430343,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.18115635899199292,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.29959791363234617,
             "unit": "Instructions Per Cycle"
           }
         ]
