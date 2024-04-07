bede08ba613e0b336eda2aa38f7923915ab9f6b2
Lekcyjna <309016@uwr.edu.pl>
2024-04-07 12:06:51 +0200

    Fix logging tests.

--- test/transactron/testing/test_log.py
+++ test/transactron/testing/test_log.py
@@ -76,10 +76,11 @@ class TestLog(TestCaseWithSimulator):
         with self.run_simulation(m) as sim:
             sim.add_sync_process(proc)
 
-        assert "WARNING:test_logger:test/transactron/testing/test_log.py:21] Log triggered under Amaranth If value+3=0x2d" in caplog.text
+        print(caplog.text)
+        assert "WARNING  test_logger:logging.py:87 test/transactron/testing/test_log.py:22] Log triggered under Amaranth If value+3=0x2d" in caplog.text
         for i in range(0, 50, 2):
             expected_msg = (
-                "WARNING:test_logger:test/transactron/testing/test_log.py:23] "
+                "WARNING  test_logger:logging.py:87 test/transactron/testing/test_log.py:24] "
                 + f"Input is even! input={i}, counter={i + 2}"
             )
             assert expected_msg in caplog.text
@@ -96,7 +97,7 @@ class TestLog(TestCaseWithSimulator):
                 sim.add_sync_process(proc)
 
         extected_out = (
-            "ERROR:test_logger:test/transactron/testing/test_log.py:40] "
+            "ERROR    test_logger:logging.py:87 test/transactron/testing/test_log.py:41] "
             + "Input is different than output! input=0x1 output=0x0"
         )
         assert extected_out in caplog.text
@@ -112,5 +113,5 @@ class TestLog(TestCaseWithSimulator):
             with self.run_simulation(m) as sim:
                 sim.add_sync_process(proc)
 
-        extected_out = "ERROR:test_logger:test/transactron/testing/test_log.py:61] Output differs"
+        extected_out = "ERROR    test_logger:logging.py:87 test/transactron/testing/test_log.py:62] Output differs"
         assert extected_out in caplog.text
