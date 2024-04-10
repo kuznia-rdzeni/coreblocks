295393be6d390f61a3f7edea31b0b4e98d6a02da
github-action-benchmark <github@users.noreply.github.com>
2024-04-10 08:20:18 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for d1646ab76fb340402e2c3ccfc68b375903a49c4a

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712737024530,
+  "lastUpdate": 1712737218078,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -9968,6 +9968,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "aha-mont64",
             "value": 0.38398161016601284,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.2437939792267849,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.31428496807135303,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.5642657981099021,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d1646ab76fb340402e2c3ccfc68b375903a49c4a",
+          "message": "Drop `unittest` from `TestCaseWithSimulator` (#642)",
+          "timestamp": "2024-04-10T10:08:01+02:00",
+          "tree_id": "5e1ee13c9791067d0a26268ae433985dad7cd7b4",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/d1646ab76fb340402e2c3ccfc68b375903a49c4a"
+        },
+        "date": 1712737215407,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "statemate",
+            "value": 0.2551784802224618,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "aha-mont64",
+            "value": 0.38398161016601284,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.18830806309955814,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.27916937796305963,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.29316662943800825,
             "unit": "Instructions Per Cycle"
           },
           {
