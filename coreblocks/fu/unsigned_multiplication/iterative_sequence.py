067862db19062e50bc4b4e9502f2c3c953202c10
Mateusz Bilyk <korbeusz@gmail.com>
2024-04-25 15:00:41 +0200

    valid array signaling

--- coreblocks/fu/unsigned_multiplication/iterative_sequence.py
+++ coreblocks/fu/unsigned_multiplication/iterative_sequence.py
@@ -28,52 +28,82 @@ class IterativeSequenceMul(Elaboratable):
         self.i1 = Signal(unsigned(n))
         self.i2 = Signal(unsigned(n))
         self.result = Signal(unsigned(n * 2))
+        self.valid = Signal(unsigned(1))
+        self.getting_result = Signal(unsigned(1))
 
         self.pipeline_array = [[[Signal(unsigned(n*2)) for _ in range(n)] for _ in range(n)] for _ in range(n)]
-
+        self.valid_array = [Signal(unsigned(1)) for _ in range(n)]
     def elaborate(self, platform=None):
         m = Module()
-
+        
+        
         number_of_chunks = self.n // self.dsp_width
         number_of_multiplications = number_of_chunks * number_of_chunks
         number_of_steps = math.ceil(number_of_multiplications / self.dsp_number)
-	
-        m.d.sync += self.step.eq((self.step + 1) % number_of_steps)
-        
-        with m.If(self.step == number_of_steps):
-            m.d.sync += self.ready.eq(1)
-            
-        with m.If(self.step < number_of_steps):
-            m.d.sync += self.ready.eq(0)
-            m.d.sync += self.step.eq(self.step+1)
-            
-        with m.If(self.issue == 1):
-            m.d.sync += self.step.eq(0)
-            m.d.sync += self.ready.eq(0)
-            
-        m.d.sync += self.first_mul_number.eq(( self.first_mul_number + self.dsp_number) % number_of_multiplications)
+        result_lvl = int(math.log(number_of_chunks, 2))
 
-        
-        for i in range(number_of_multiplications):
-            a = (i // number_of_chunks)
-            b = (i % number_of_chunks)
-            chunk_i1 = self.i1[a*self.dsp_width: (a+1)*self.dsp_width]
-            chunk_i2 = self.i2[b*self.dsp_width: (b+1)*self.dsp_width]
-            with m.If((i >= self.first_mul_number) & (i <  self.first_mul_number + self.dsp_number )):
-                m.d.sync += self.pipeline_array[0][a][b].eq(chunk_i1*chunk_i2)
-	
-        shift_size = self.dsp_width
-        for i in range(1, int(math.log(number_of_chunks, 2))+1):
-            shift_size = shift_size << 1
-            for j in range(number_of_chunks >> i):
-                for k in range(number_of_chunks >> i):
-                    ll = self.pipeline_array[i-1][2*j][2*k]
-                    lu = self.pipeline_array[i-1][2*j][2*k+1]
-                    ul = self.pipeline_array[i-1][2*j+1][2*k]
-                    uu = self.pipeline_array[i-1][2*j+1][2*k+1]
-                    m.d.sync += self.pipeline_array[i][j][k].eq(ll+((ul+lu) << (shift_size >> 1)) + (uu << shift_size) )
-        result_lvl = int(math.log(number_of_chunks, 2)) 
-        m.d.sync += self.result.eq(self.pipeline_array[result_lvl][0][0])
+        with m.If(~self.valid | self.getting_result):
+            m.d.sync += self.step.eq((self.step + 1) % number_of_steps)
+            
+            for i in range(1,result_lvl+1):
+                m.d.sync += self.valid_array[i].eq(self.valid_array[i-1])
+
+            with m.If(self.step == number_of_steps):
+                m.d.sync += self.ready.eq(1)
+
+            with m.If(self.step == number_of_steps - 1):
+                m.d.sync += self.valid_array[0].eq(1)
+            with m.Else():
+                m.d.sync += self.valid_array[0].eq(0)
+
+                
+            with m.If(self.step < number_of_steps):
+                m.d.sync += self.ready.eq(0)
+                m.d.sync += self.step.eq(self.step+1)
+                
+            with m.If(self.issue == 1):
+                m.d.sync += self.step.eq(0)
+                m.d.sync += self.ready.eq(0)
+            
+                
+            m.d.sync += self.first_mul_number.eq(( self.first_mul_number + self.dsp_number) % number_of_multiplications)
+
+            
+            for i in range(number_of_multiplications):
+                a = (i // number_of_chunks)
+                b = (i % number_of_chunks)
+                chunk_i1 = self.i1[a*self.dsp_width: (a+1)*self.dsp_width]
+                chunk_i2 = self.i2[b*self.dsp_width: (b+1)*self.dsp_width]
+                with m.If((i >= self.first_mul_number) & (i <  self.first_mul_number + self.dsp_number )):
+                    m.d.sync += self.pipeline_array[0][a][b].eq(chunk_i1*chunk_i2)
+    	
+            shift_size = self.dsp_width
+            for i in range(1, int(math.log(number_of_chunks, 2))+1):
+                shift_size = shift_size << 1
+                for j in range(number_of_chunks >> i):
+                    for k in range(number_of_chunks >> i):
+                        ll = self.pipeline_array[i-1][2*j][2*k]
+                        lu = self.pipeline_array[i-1][2*j][2*k+1]
+                        ul = self.pipeline_array[i-1][2*j+1][2*k]
+                        uu = self.pipeline_array[i-1][2*j+1][2*k+1]
+                        m.d.sync += self.pipeline_array[i][j][k].eq(ll+((ul+lu) << (shift_size >> 1)) + (uu << shift_size) )
+             
+            m.d.sync += self.result.eq(self.pipeline_array[result_lvl][0][0])
+            m.d.sync += self.valid.eq(self.valid_array[result_lvl])
+
+        with m.Else():
+            for i in range(self.n):
+                for j in range(self.n):
+                    for k in range(self.n):
+                        m.d.sync += self.pipeline_array[i][j][k].eq(self.pipeline_array[i][j][k])
+            m.d.sync += self.result.eq(self.result)
+            m.d.sync += self.valid.eq(self.valid)
+            for i in range(self.n):
+                m.d.sync += self.valid_array[i].eq(self.valid_array[i])
+            m.d.sync += self.step.eq(self.step)
+            m.d.sync += self.ready.eq(self.ready)
+            m.d.sync += self.first_mul_number.eq(self.first_mul_number)
+            m.d.sync += self.last_mul_number.eq(self.last_mul_number)
         return m
         
 class IterativeUnsignedMul(MulBaseUnsigned):
@@ -106,6 +136,7 @@ class IterativeUnsignedMul(MulBaseUnsigned):
 
         @def_method(m, self.issue, ready = mul.ready)
         def _(arg):
+            m.d.comb += self.method_working.eq(1)
             m.d.sync += mul.i1.eq(arg.i1)
             m.d.sync += mul.i2.eq(arg.i2)
             m.d.sync += mul.issue.eq(1)
@@ -134,20 +165,33 @@ def testbench(t):
     def process():
 
         test_cases = [(random.randint(1, 0xFFFF), random.randint(1, 0xFFFF)) for _ in range(t)]
-        #test_cases = [(500,100)]
+        test_cases = [(65535,65535),(500,100),(8902,123),(2131,323)]
         expected_results = [(a * b) for a, b in test_cases]
         time = 0
         module_latency = 1
-	
-        for test_idx, (test_val1, test_val2) in enumerate(test_cases + [(0, 0)] * module_latency):
-            while (yield mul_module.ready) == 0:
+        test_it=0
+        for time in range(30):         
+            ready_signal = yield mul_module.ready
+            if ready_signal == 1:
+                test_val1,test_val2 = test_cases[test_it]
+                test_it = test_it + 1
+                print(f"giving values: {test_val1}, {test_val2} = {test_val1 * test_val2}")
+                yield mul_module.i1.eq(test_val1)
+                yield mul_module.i2.eq(test_val2)
+                yield mul_module.issue.eq(1)
+            else:
                 yield mul_module.issue.eq(0)
-                time = time + 1
-                yield
-            #print(f"issuing test: {test_idx} time: {time}")
-            yield mul_module.i1.eq(test_val1)
-            yield mul_module.i2.eq(test_val2)
-            yield mul_module.issue.eq(1)
+            
+
+            result_sig = yield mul_module.result
+            valid_sig = yield mul_module.valid
+            ready_signal = yield mul_module.ready
+            if valid_sig == 1 and random.randint(0, 1) == 1:
+                yield mul_module.getting_result.eq(1)
+            else:
+                yield mul_module.getting_result.eq(0)
+            print(f" time: {time} result: {result_sig} valid_sig: {valid_sig} ready_sig: {ready_signal}")
+            yield
             """
             for i in range(n):
                 for j in range(n):
@@ -157,14 +201,17 @@ def testbench(t):
                         if val !=0:
                             print(f"pipeline_array[{i}][{j}][{k}] = {val}")
             """
-            yield
-            time = time + 1
+            """
+            while True:
+                valid_signal = yield mul_module.valid
+                if valid_signal == 1:
+
             if test_idx >= module_latency:
                 expected = expected_results[test_idx - module_latency]
                 result = yield mul_module.result
                 assert result == expected, f"Test case {(test_idx - module_latency)} failed: Expected {expected}, got {result}"
                 print(f"Test case {(test_idx - module_latency)}: Expected = {expected}, got {result}")
-            
+            """
             
 
     sim.add_sync_process(process)
@@ -185,7 +232,7 @@ def transactron_testbench():
     sim.run()
 if __name__ == "__main__":
     t = 100
-    #testbench(t)
-    transactron_testbench()
+    testbench(t)
+    #transactron_testbench()
 
 
