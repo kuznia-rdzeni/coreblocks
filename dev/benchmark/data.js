190a3bfd583fbbbea59c58a7eac43c79571b0266
github-action-benchmark <github@users.noreply.github.com>
2024-07-10 09:25:13 +0000

    add Fmax and LCs (full) (customBiggerIsBetter) benchmark result for 0b76faef4d1c8c6691efdc4324965d564c1c0948

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720603511306,
+  "lastUpdate": 1720603513272,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -38936,6 +38936,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 1944,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1152,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 9177,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0b76faef4d1c8c6691efdc4324965d564c1c0948",
+          "message": "Update the version of amaranth-stubs (#715)",
+          "timestamp": "2024-07-10T11:12:30+02:00",
+          "tree_id": "91097f0bdaff2954b1534aa1adf125772a76085f",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/0b76faef4d1c8c6691efdc4324965d564c1c0948"
+        },
+        "date": 1720603512597,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 43.37,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 31394,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1976,
             "unit": "LUT"
           },
           {
