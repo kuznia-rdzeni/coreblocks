ad941974e1909546bb3d9b1d2e01cdc4aac0afaf
github-action-benchmark <github@users.noreply.github.com>
2024-09-25 09:05:03 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 123e69c4e67168ddf9312a2139ebd1137ab0a2ff

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727254079177,
+  "lastUpdate": 1727255103270,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -13399,6 +13399,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/5b96b1a064760af38c1f8522c0dbc06ffb939736"
         },
         "date": 1727254073373,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.4168072362013762,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.5126114415514502,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.3373813468043159,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.6547182775996335,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.3605570079176623,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.2903413743150469,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.32594552472771426,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.4314500724103311,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "123e69c4e67168ddf9312a2139ebd1137ab0a2ff",
+          "message": "Clean up `MemoryBank` (#730)",
+          "timestamp": "2024-09-25T10:52:44+02:00",
+          "tree_id": "d8f3125a472311a4b8e8b0829fb18cbaa40a86af",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/123e69c4e67168ddf9312a2139ebd1137ab0a2ff"
+        },
+        "date": 1727255099171,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
