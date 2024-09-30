6ba84c370de45c72916bade4e8c35db8de8f37f3
Mateusz Marszalek <matti.marszalek@gmail.com>
2024-10-01 00:37:02 +0200

    Review changes for fpu_rounding_module and fpu_common

--- coreblocks/func_blocks/fu/fpu/fpu_rounding_module.py
+++ coreblocks/func_blocks/fu/fpu/fpu_rounding_module.py
@@ -1,38 +1,10 @@
 from amaranth import *
-from amaranth.lib.wiring import Component, In, Out, Signature
 from transactron import TModule, Method, def_method
 from coreblocks.func_blocks.fu.fpu.fpu_common import (
     RoundingModes,
     FPUParams,
     FPURoundingParams,
 )
-
-
-class FPURoundingSignature(Signature):
-    """FPU Rounding module signature
-
-    Parameters
-    ----------
-    fpu_params: FPUParams
-        FPU parameters
-    """
-
-    def __init__(self, *, fpu_params: FPUParams):
-        super().__init__(
-            {
-                "in_sign": In(1),
-                "in_sig": In(fpu_params.sig_width),
-                "in_exp": In(fpu_params.exp_width),
-                "rounding_mode": In(3),
-                "guard_bit": In(1),
-                "sticky_bit": In(1),
-                "in_errors": In(3),
-                "out_sign": In(1),
-                "out_sig": Out(fpu_params.sig_width),
-                "out_exp": Out(fpu_params.exp_width),
-                "out_error": Out(3),
-            }
-        )
 
 
 class FPURoudningMethodLayout:
@@ -49,11 +21,11 @@ class FPURoudningMethodLayout:
             ("sign", 1),
             ("sig", fpu_params.sig_width),
             ("exp", fpu_params.exp_width),
-            ("guard_bit", 1),
+            ("round_bit", 1),
             ("sticky_bit", 1),
-            ("rounding_mode", 3),
-            ("errors", 5),
-            ("input_nan", 1),
+            ("rounding_mode", Shape.cast(RoundingModes)),
+            ("invalid_operation", 1),
+            ("division_by_zero", 1),
             ("input_inf", 1),
         ]
         self.rounding_out_layout = [
@@ -64,12 +36,23 @@ class FPURoudningMethodLayout:
         ]
 
 
-class FPUrounding(Component):
+class FPUrounding(Elaboratable):
+    """FPU Rounding module
 
-    fpu_rounding: FPURoundingSignature
+    Parameters
+    ----------
+    fpu_rounding_params: FPURoundingParams
+        FPU rounding module parameters
+
+    Attributes
+    ----------
+    rounding_request: Method
+        Transactional method for initiating rounding of a floating point number.
+        Takes 'rounding_in_layout' as argument
+        Returns rounded number and errors as 'rounding_out_layout'
+    """
 
     def __init__(self, *, fpu_rounding_params: FPURoundingParams):
-        super().__init__({"fpu_rounding": Out(FPURoundingSignature(fpu_params=fpu_rounding_params.fpu_params))})
 
         self.fpu_rounding_params = fpu_rounding_params
         self.method_layouts = FPURoudningMethodLayout(fpu_params=self.fpu_rounding_params.fpu_params)
@@ -77,181 +60,163 @@ class FPUrounding(Component):
             i=self.method_layouts.rounding_in_layout,
             o=self.method_layouts.rounding_out_layout,
         )
-        self.rtval = {}
-        self.max_exp = C(
-            2 ** (self.fpu_rounding_params.fpu_params.exp_width) - 1,
-            unsigned(self.fpu_rounding_params.fpu_params.exp_width),
-        )
-        self.max_normal_exp = C(
-            2 ** (self.fpu_rounding_params.fpu_params.exp_width) - 2,
-            unsigned(self.fpu_rounding_params.fpu_params.exp_width),
-        )
-        self.quiet_nan = C(
-            2 ** (self.fpu_rounding_params.fpu_params.sig_width - 1),
-            unsigned(self.fpu_rounding_params.fpu_params.sig_width),
-        )
-        self.max_sig = C(
-            2 ** (self.fpu_rounding_params.fpu_params.sig_width) - 1,
-            unsigned(self.fpu_rounding_params.fpu_params.sig_width),
-        )
-        self.add_one = Signal()
-        self.inc_rtnte = Signal()
-        self.inc_rtnta = Signal()
-        self.inc_rtpi = Signal()
-        self.inc_rtmi = Signal()
-
-        self.rounded_sig = Signal(self.fpu_rounding_params.fpu_params.sig_width + 1)
-        self.normalised_sig = Signal(self.fpu_rounding_params.fpu_params.sig_width)
-        self.rounded_exp = Signal(self.fpu_rounding_params.fpu_params.exp_width + 1)
-
-        self.final_guard_bit = Signal()
-        self.final_sticky_bit = Signal()
-
-        self.overflow = Signal()
-        self.underflow = Signal()
-        self.inexact = Signal()
-        self.tininess = Signal()
-        self.is_inf = Signal()
-        self.is_nan = Signal()
-        self.input_not_special = Signal()
-        self.rounded_inexact = Signal()
-
-        self.final_exp = Signal(self.fpu_rounding_params.fpu_params.exp_width)
-        self.final_sig = Signal(self.fpu_rounding_params.fpu_params.sig_width)
-        self.final_sign = Signal()
-        self.final_errors = Signal(5)
 
     def elaborate(self, platform):
         m = TModule()
 
+        max_exp = C(
+            2 ** (self.fpu_rounding_params.fpu_params.exp_width) - 1,
+            unsigned(self.fpu_rounding_params.fpu_params.exp_width),
+        )
+        max_normal_exp = C(
+            2 ** (self.fpu_rounding_params.fpu_params.exp_width) - 2,
+            unsigned(self.fpu_rounding_params.fpu_params.exp_width),
+        )
+        max_sig = C(
+            2 ** (self.fpu_rounding_params.fpu_params.sig_width) - 1,
+            unsigned(self.fpu_rounding_params.fpu_params.sig_width),
+        )
+        add_one = Signal()
+        inc_rtnte = Signal()
+        inc_rtnta = Signal()
+        inc_rtpi = Signal()
+        inc_rtmi = Signal()
+
+        rounded_sig = Signal(self.fpu_rounding_params.fpu_params.sig_width + 1)
+        normalised_sig = Signal(self.fpu_rounding_params.fpu_params.sig_width)
+        rounded_exp = Signal(self.fpu_rounding_params.fpu_params.exp_width + 1)
+
+        final_round_bit = Signal()
+        final_sticky_bit = Signal()
+
+        overflow = Signal()
+        underflow = Signal()
+        inexact = Signal()
+        tininess = Signal()
+
+        final_exp = Signal(self.fpu_rounding_params.fpu_params.exp_width)
+        final_sig = Signal(self.fpu_rounding_params.fpu_params.sig_width)
+        final_sign = Signal()
+        final_errors = Signal(5)
+
         @def_method(m, self.rounding_request)
         def _(arg):
 
-            m.d.comb += self.inc_rtnte.eq(
+            m.d.av_comb += inc_rtnte.eq(
                 (arg.rounding_mode == RoundingModes.ROUND_NEAREST_EVEN)
-                & (arg.guard_bit & (arg.sticky_bit | arg.sig[0]))
+                & (arg.round_bit & (arg.sticky_bit | arg.sig[0]))
             )
-            m.d.comb += self.inc_rtnta.eq((arg.rounding_mode == RoundingModes.ROUND_NEAREST_AWAY) & (arg.guard_bit))
-            m.d.comb += self.inc_rtpi.eq(
-                (arg.rounding_mode == RoundingModes.ROUND_UP) & (~arg.sign & (arg.guard_bit | arg.sticky_bit))
+            m.d.av_comb += inc_rtnta.eq((arg.rounding_mode == RoundingModes.ROUND_NEAREST_AWAY) & (arg.round_bit))
+            m.d.av_comb += inc_rtpi.eq(
+                (arg.rounding_mode == RoundingModes.ROUND_UP) & (~arg.sign & (arg.round_bit | arg.sticky_bit))
             )
-            m.d.comb += self.inc_rtmi.eq(
-                (arg.rounding_mode == RoundingModes.ROUND_DOWN) & (arg.sign & (arg.guard_bit | arg.sticky_bit))
+            m.d.av_comb += inc_rtmi.eq(
+                (arg.rounding_mode == RoundingModes.ROUND_DOWN) & (arg.sign & (arg.round_bit | arg.sticky_bit))
             )
 
-            m.d.comb += self.add_one.eq(self.inc_rtmi | self.inc_rtnta | self.inc_rtnte | self.inc_rtpi)
+            m.d.av_comb += add_one.eq(inc_rtmi | inc_rtnta | inc_rtnte | inc_rtpi)
 
             if self.fpu_rounding_params.is_rounded:
 
-                m.d.comb += self.normalised_sig.eq(arg.sig)
-                m.d.comb += self.final_guard_bit.eq(arg.guard_bit)
-                m.d.comb += self.final_sticky_bit.eq(arg.sticky_bit)
-                m.d.comb += self.rounded_exp.eq(arg.exp)
+                m.d.av_comb += normalised_sig.eq(arg.sig)
+                m.d.av_comb += final_round_bit.eq(arg.round_bit)
+                m.d.av_comb += final_sticky_bit.eq(arg.sticky_bit)
+                m.d.av_comb += rounded_exp.eq(arg.exp)
 
             else:
 
-                m.d.comb += self.rounded_sig.eq(arg.sig + self.add_one)
+                m.d.av_comb += rounded_sig.eq(arg.sig + add_one)
 
-                with m.If(self.rounded_sig[-1]):
+                with m.If(rounded_sig[-1]):
 
-                    m.d.comb += self.normalised_sig.eq(self.rounded_sig >> 1)
-                    m.d.comb += self.final_guard_bit.eq(self.rounded_sig[0])
-                    m.d.comb += self.final_sticky_bit.eq(arg.guard_bit | arg.sticky_bit)
-                    m.d.comb += self.rounded_exp.eq(arg.exp + 1)
+                    m.d.av_comb += normalised_sig.eq(rounded_sig >> 1)
+                    m.d.av_comb += final_round_bit.eq(rounded_sig[0])
+                    m.d.av_comb += final_sticky_bit.eq(arg.round_bit | arg.sticky_bit)
+                    m.d.av_comb += rounded_exp.eq(arg.exp + 1)
 
                 with m.Else():
-                    m.d.comb += self.normalised_sig.eq(self.rounded_sig)
-                    m.d.comb += self.final_guard_bit.eq(arg.guard_bit)
-                    m.d.comb += self.final_sticky_bit.eq(arg.sticky_bit)
-                    m.d.comb += self.rounded_exp.eq(arg.exp)
+                    m.d.av_comb += normalised_sig.eq(rounded_sig)
+                    m.d.av_comb += final_round_bit.eq(arg.round_bit)
+                    m.d.av_comb += final_sticky_bit.eq(arg.sticky_bit)
+                    m.d.av_comb += rounded_exp.eq(arg.exp)
 
-            m.d.comb += self.rounded_inexact.eq(self.final_guard_bit | self.final_sticky_bit)
-            m.d.comb += self.is_nan.eq(arg.errors[0] | arg.input_nan)
-            m.d.comb += self.is_inf.eq(arg.errors[1] | arg.input_inf)
-            m.d.comb += self.input_not_special.eq(~(self.is_nan) & ~(self.is_inf))
-            m.d.comb += self.overflow.eq(self.input_not_special & (self.rounded_exp >= self.max_exp))
-            m.d.comb += self.tininess.eq(
-                (self.rounded_exp == 0) & (self.rounded_inexact | self.rounded_sig.any()) & (~self.normalised_sig[-1])
-            )
-            m.d.comb += self.inexact.eq(self.overflow | (self.input_not_special & self.rounded_inexact))
-            m.d.comb += self.underflow.eq(self.tininess & self.inexact)
+            rounded_inexact = final_round_bit | final_sticky_bit
+            is_nan = arg.invalid_operation | ((arg.exp == max_exp) & (arg.sig.any()))
+            is_inf = arg.division_by_zero | arg.input_inf
+            input_not_special = ~(is_nan) & ~(is_inf)
+            m.d.av_comb += overflow.eq(input_not_special & (rounded_exp >= max_exp))
+            m.d.av_comb += tininess.eq((rounded_exp == 0) & (~normalised_sig[-1]))
+            m.d.av_comb += inexact.eq(overflow | (input_not_special & rounded_inexact))
+            m.d.av_comb += underflow.eq(tininess & inexact)
 
-            with m.If(self.is_nan):
+            with m.If(is_nan | is_inf):
 
-                m.d.comb += self.final_exp.eq(self.max_exp)
-                m.d.comb += self.final_sig.eq(arg.sig)
-                m.d.comb += self.final_sign.eq(arg.sign)
+                m.d.av_comb += final_exp.eq(arg.exp)
+                m.d.av_comb += final_sig.eq(arg.sig)
+                m.d.av_comb += final_sign.eq(arg.sign)
 
-            with m.Elif(self.is_inf):
+            with m.Elif(overflow):
 
-                m.d.comb += self.final_exp.eq(self.max_exp)
-                m.d.comb += self.final_sig.eq(arg.sig)
-                m.d.comb += self.final_sign.eq(arg.sign)
+                with m.Switch(arg.rounding_mode):
+                    with m.Case(RoundingModes.ROUND_NEAREST_AWAY, RoundingModes.ROUND_NEAREST_EVEN):
 
-            with m.Elif(self.overflow):
+                        m.d.av_comb += final_exp.eq(max_exp)
+                        m.d.av_comb += final_sig.eq(0)
+                        m.d.av_comb += final_sign.eq(arg.sign)
 
-                with m.If(
-                    (arg.rounding_mode == RoundingModes.ROUND_NEAREST_AWAY)
-                    | (arg.rounding_mode == RoundingModes.ROUND_NEAREST_EVEN)
-                ):
+                    with m.Case(RoundingModes.ROUND_ZERO):
 
-                    m.d.comb += self.final_exp.eq(self.max_exp)
-                    m.d.comb += self.final_sig.eq(0)
-                    m.d.comb += self.final_sign.eq(arg.sign)
+                        m.d.av_comb += final_exp.eq(max_normal_exp)
+                        m.d.av_comb += final_sig.eq(max_sig)
+                        m.d.av_comb += final_sign.eq(arg.sign)
 
-                with m.If(arg.rounding_mode == RoundingModes.ROUND_ZERO):
+                    with m.Case(RoundingModes.ROUND_DOWN):
 
-                    m.d.comb += self.final_exp.eq(self.max_normal_exp)
-                    m.d.comb += self.final_sig.eq(self.max_sig)
-                    m.d.comb += self.final_sign.eq(arg.sign)
+                        with m.If(arg.sign):
 
-                with m.If(arg.rounding_mode == RoundingModes.ROUND_DOWN):
+                            m.d.av_comb += final_exp.eq(max_exp)
+                            m.d.av_comb += final_sig.eq(0)
+                            m.d.av_comb += final_sign.eq(arg.sign)
 
-                    with m.If(arg.sign):
+                        with m.Else():
 
-                        m.d.comb += self.final_exp.eq(self.max_exp)
-                        m.d.comb += self.final_sig.eq(0)
-                        m.d.comb += self.final_sign.eq(arg.sign)
+                            m.d.av_comb += final_exp.eq(max_normal_exp)
+                            m.d.av_comb += final_sig.eq(max_sig)
+                            m.d.av_comb += final_sign.eq(arg.sign)
 
-                    with m.Else():
+                    with m.Case(RoundingModes.ROUND_UP):
 
-                        m.d.comb += self.final_exp.eq(self.max_normal_exp)
-                        m.d.comb += self.final_sig.eq(self.max_sig)
-                        m.d.comb += self.final_sign.eq(arg.sign)
+                        with m.If(arg.sign):
 
-                with m.If(arg.rounding_mode == RoundingModes.ROUND_UP):
+                            m.d.av_comb += final_exp.eq(max_normal_exp)
+                            m.d.av_comb += final_sig.eq(max_sig)
+                            m.d.av_comb += final_sign.eq(arg.sign)
 
-                    with m.If(arg.sign):
+                        with m.Else():
 
-                        m.d.comb += self.final_exp.eq(self.max_normal_exp)
-                        m.d.comb += self.final_sig.eq(self.max_sig)
-                        m.d.comb += self.final_sign.eq(arg.sign)
-
-                    with m.Else():
-
-                        m.d.comb += self.final_exp.eq(self.max_exp)
-                        m.d.comb += self.final_sig.eq(0)
-                        m.d.comb += self.final_sign.eq(arg.sign)
+                            m.d.av_comb += final_exp.eq(max_exp)
+                            m.d.av_comb += final_sig.eq(0)
+                            m.d.av_comb += final_sign.eq(arg.sign)
 
             with m.Else():
-                with m.If((self.rounded_exp == 0) & (self.normalised_sig[-1] == 1)):
-                    m.d.comb += self.final_exp.eq(1)
+                with m.If((rounded_exp == 0) & (normalised_sig[-1] == 1)):
+                    m.d.av_comb += final_exp.eq(1)
                 with m.Else():
-                    m.d.comb += self.final_exp.eq(self.rounded_exp)
-                m.d.comb += self.final_sig.eq(self.normalised_sig)
-                m.d.comb += self.final_sign.eq(arg.sign)
+                    m.d.av_comb += final_exp.eq(rounded_exp)
+                m.d.av_comb += final_sig.eq(normalised_sig)
+                m.d.av_comb += final_sign.eq(arg.sign)
 
-            m.d.comb += self.final_errors[0].eq(arg.errors[0])
-            m.d.comb += self.final_errors[1].eq(arg.errors[1])
-            m.d.comb += self.final_errors[2].eq(self.overflow)
-            m.d.comb += self.final_errors[3].eq(self.underflow)
-            m.d.comb += self.final_errors[4].eq(self.inexact)
+            m.d.av_comb += final_errors[0].eq(arg.invalid_operation)
+            m.d.av_comb += final_errors[1].eq(arg.division_by_zero)
+            m.d.av_comb += final_errors[2].eq(overflow)
+            m.d.av_comb += final_errors[3].eq(underflow)
+            m.d.av_comb += final_errors[4].eq(inexact)
 
-            self.rtval["exp"] = self.final_exp
-            self.rtval["sig"] = self.final_sig
-            self.rtval["sign"] = self.final_sign
-            self.rtval["errors"] = self.final_errors
-
-            return self.rtval
+            return {
+                "exp": final_exp,
+                "sig": final_sig,
+                "sign": final_sign,
+                "errors": final_errors,
+            }
 
         return m
