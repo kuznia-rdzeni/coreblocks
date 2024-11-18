86f1b42536f54f1b6a95a90f890aec12c29f67ea
Mateusz Marszalek <matti.marszalek@gmail.com>
2024-11-18 22:45:36 +0100

    Fixed formatting

--- test/func_blocks/fu/test_lza.py
+++ test/func_blocks/fu/test_lza.py
@@ -14,9 +14,7 @@ class TestLZA(TestCaseWithSimulator):
         def elaborate(self, platform):
             m = TModule()
             m.submodules.lza = lza = self.lza_module = LZAModule(fpu_params=self.params)
-            m.submodules.predict = self.predict_request_adapter = TestbenchIO(
-                AdapterTrans(lza.predict_request)
-            )
+            m.submodules.predict = self.predict_request_adapter = TestbenchIO(AdapterTrans(lza.predict_request))
             return m
 
     class HelpValues:
@@ -24,9 +22,9 @@ class TestLZA(TestCaseWithSimulator):
             self.test_val_sig_a_1 = 16368512
             self.test_val_sig_b_1 = 409600
             self.test_val_sig_a_2 = 0
-            self.test_val_sig_b_2 = (2**24)-1
-            self.test_val_sig_a_3 = (2**24)//2
-            self.test_val_sig_b_3 = (2**24)//2
+            self.test_val_sig_b_2 = (2**24) - 1
+            self.test_val_sig_a_3 = (2**24) // 2
+            self.test_val_sig_b_3 = (2**24) // 2
             self.test_val_sig_a_4 = 12582912
             self.test_val_sig_b_4 = 12550144
             self.test_val_sig_a_5 = 16744448
@@ -34,101 +32,87 @@ class TestLZA(TestCaseWithSimulator):
             self.test_val_sig_a_6 = 8421376
             self.test_val_sig_b_6 = 8421376
 
-
-
-
     def test_manual(self):
         params = FPUParams(sig_width=24, exp_width=8)
         help_values = TestLZA.HelpValues(params)
         lza = TestLZA.LZAModuleTest(params)
+
         def lza_test():
             test_cases = [
-            {"sig_a":help_values.test_val_sig_a_1,
-            "sig_b":help_values.test_val_sig_b_1,
-            "carry":0,
-            },
-            {"sig_a":help_values.test_val_sig_a_1,
-            "sig_b":help_values.test_val_sig_b_1,
-            "carry":1,
-            },
-            {"sig_a":help_values.test_val_sig_a_2,
-            "sig_b":help_values.test_val_sig_b_2,
-            "carry":0,
-            },
-            {"sig_a":help_values.test_val_sig_a_2,
-            "sig_b":help_values.test_val_sig_b_2,
-            "carry":1,
-            },
-            {"sig_a":help_values.test_val_sig_a_3,
-            "sig_b":help_values.test_val_sig_b_3,
-            "carry":0,
-            },
-            {"sig_a":help_values.test_val_sig_a_3,
-            "sig_b":help_values.test_val_sig_b_3,
-            "carry":1,
-            },
-            {"sig_a":help_values.test_val_sig_a_4,
-            "sig_b":help_values.test_val_sig_b_4,
-            "carry":0,
-            },
-            {"sig_a":help_values.test_val_sig_a_4,
-            "sig_b":help_values.test_val_sig_b_4,
-            "carry":1,
-            },
-            {"sig_a":help_values.test_val_sig_a_5,
-            "sig_b":help_values.test_val_sig_b_5,
-            "carry":1,
-            },
-            {"sig_a":help_values.test_val_sig_a_5,
-            "sig_b":help_values.test_val_sig_b_5,
-            "carry":1,
-            },
-            {"sig_a":help_values.test_val_sig_a_6,
-            "sig_b":help_values.test_val_sig_b_6,
-            "carry":0,
-            },
-            {"sig_a":help_values.test_val_sig_a_6,
-            "sig_b":help_values.test_val_sig_b_6,
-            "carry":1,
-            },]
+                {
+                    "sig_a": help_values.test_val_sig_a_1,
+                    "sig_b": help_values.test_val_sig_b_1,
+                    "carry": 0,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_1,
+                    "sig_b": help_values.test_val_sig_b_1,
+                    "carry": 1,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_2,
+                    "sig_b": help_values.test_val_sig_b_2,
+                    "carry": 0,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_2,
+                    "sig_b": help_values.test_val_sig_b_2,
+                    "carry": 1,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_3,
+                    "sig_b": help_values.test_val_sig_b_3,
+                    "carry": 0,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_3,
+                    "sig_b": help_values.test_val_sig_b_3,
+                    "carry": 1,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_4,
+                    "sig_b": help_values.test_val_sig_b_4,
+                    "carry": 0,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_4,
+                    "sig_b": help_values.test_val_sig_b_4,
+                    "carry": 1,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_5,
+                    "sig_b": help_values.test_val_sig_b_5,
+                    "carry": 1,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_5,
+                    "sig_b": help_values.test_val_sig_b_5,
+                    "carry": 1,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_6,
+                    "sig_b": help_values.test_val_sig_b_6,
+                    "carry": 0,
+                },
+                {
+                    "sig_a": help_values.test_val_sig_a_6,
+                    "sig_b": help_values.test_val_sig_b_6,
+                    "carry": 1,
+                },
+            ]
             expected_results = [
-                {
-                    "shift_amount":13,
-            "is_zero":0},
-                {
-                    "shift_amount":13,
-            "is_zero":0},
-                {
-                    "shift_amount":23,
-            "is_zero":0},
-                {
-                    "shift_amount":0,
-            "is_zero":1},
-                {
-                    "shift_amount":0,
-            "is_zero":0},
-                {
-                    "shift_amount":23,
-            "is_zero":0},
-                {
-                    "shift_amount":0,
-            "is_zero":0},
-                {
-                    "shift_amount":0,
-            "is_zero":0},
-                {
-                    "shift_amount":0,
-            "is_zero":0},
-                {
-                    "shift_amount":0,
-            "is_zero":0},
-                {
-                    "shift_amount":7,
-            "is_zero":0},
-                {
-                    "shift_amount":7,
-            "is_zero":0},
-            
+                {"shift_amount": 13, "is_zero": 0},
+                {"shift_amount": 13, "is_zero": 0},
+                {"shift_amount": 23, "is_zero": 0},
+                {"shift_amount": 0, "is_zero": 1},
+                {"shift_amount": 0, "is_zero": 0},
+                {"shift_amount": 23, "is_zero": 0},
+                {"shift_amount": 0, "is_zero": 0},
+                {"shift_amount": 0, "is_zero": 0},
+                {"shift_amount": 0, "is_zero": 0},
+                {"shift_amount": 0, "is_zero": 0},
+                {"shift_amount": 7, "is_zero": 0},
+                {"shift_amount": 7, "is_zero": 0},
             ]
             for i in range(len(test_cases)):
                 resp = yield from lza.predict_request_adapter.call(test_cases[i])
