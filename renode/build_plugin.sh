312612cb8cc180bf03f03dd33af1930134e3071f
Arusekk <floss@arusekk.pl>
2025-01-10 13:41:56 +0100

    Portability

--- renode/build_plugin.sh
+++ renode/build_plugin.sh
@@ -9,7 +9,8 @@ if [ -z "$RENODE_ROOT" ]; then
   echo "RENODE_ROOT not set; trying to guess from PATH"
   RENODE_ROOT=$(renode --disable-gui --console -e 'python "import os;print(os.getcwd());os._exit(0)"' |tail -1)
 fi
-COSIM_DIR="$RENODE_ROOT/plugins/VerilatorIntegrationLibrary"
+COSIM_DIR="$(find "$RENODE_ROOT" -name '*integration-library.cmake')"
+COSIM_DIR="${COSIM_DIR%/*}"
 
 CSOURCES=$(find "$COSIM_DIR" -name '*.cpp' | grep -v renode_cfu |grep -v buses)
 
