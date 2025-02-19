215428c07cc59c6b9035440f3873d0969a319ef0
github-action-benchmark <github@users.noreply.github.com>
2025-02-19 21:41:18 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 047aec07a52e63a61b7231bef7dc55460fae676a

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739374414065,
+  "lastUpdate": 1740001278034,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -16023,6 +16023,68 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/047aec07a52e63a61b7231bef7dc55460fae676a"
         },
         "date": 1739374399719,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.40775334123348045,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.5395756191076446,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.37073663391335454,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.644078324656886,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.3492994129726513,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.2936540530986333,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.33073446531141404,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.44572925523358686,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Marek Materzok",
+            "username": "tilk",
+            "email": "tilk@tilk.eu"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "047aec07a52e63a61b7231bef7dc55460fae676a",
+          "message": "Update transactron and pyright (#802)",
+          "timestamp": "2025-02-12T15:18:54Z",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/047aec07a52e63a61b7231bef7dc55460fae676a"
+        },
+        "date": 1740001273446,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
