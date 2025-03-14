2000ca9dbe33d74b8b72b88381cf0409155a2e3e
github-action-benchmark <github@users.noreply.github.com>
2025-03-14 16:35:23 +0000

    add Fmax and LCs (basic) (customBiggerIsBetter) benchmark result for 4efbc6a69e7589ab4203f5f50b4538c30e5ad8ba

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741970121188,
+  "lastUpdate": 1741970123052,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -35933,6 +35933,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 1372,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1408,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 4313,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "piotro@piotro.eu",
+            "name": "Piotr Węgrzyn",
+            "username": "piotro888"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4efbc6a69e7589ab4203f5f50b4538c30e5ad8ba",
+          "message": "Refector Wishbone to ComponentInterface (#808)",
+          "timestamp": "2025-03-14T17:20:21+01:00",
+          "tree_id": "22aa26e2bdf3301cf5df48f7e10ff740f995d5ff",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/4efbc6a69e7589ab4203f5f50b4538c30e5ad8ba"
+        },
+        "date": 1741970122286,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 46.58,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 15668,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1404,
             "unit": "LUT"
           },
           {
