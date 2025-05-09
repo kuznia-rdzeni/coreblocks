a7dde643638c404a8390cf117a5ad70c5783dae3
github-action-benchmark <github@users.noreply.github.com>
2025-05-09 11:05:56 +0000

    add Fmax and LCs (full) (customBiggerIsBetter) benchmark result for e69d349668221859293b07715fe8b414072b92d2

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746788755520,
+  "lastUpdate": 1746788756691,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -48483,6 +48483,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Device utilisation: (ECP5)",
             "value": 27132,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1888,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1936,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 6806,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "piotro@piotro.eu",
+            "name": "Piotr Węgrzyn",
+            "username": "piotro888"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e69d349668221859293b07715fe8b414072b92d2",
+          "message": "gen_verilog: use amaranth-yosys by default (#815)",
+          "timestamp": "2025-05-09T12:50:27+02:00",
+          "tree_id": "e9ecf68ddaf37a836877c79dde605d1d7f2156d2",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/e69d349668221859293b07715fe8b414072b92d2"
+        },
+        "date": 1746788756357,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 33.54,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 29860,
             "unit": "LUT4"
           },
           {
