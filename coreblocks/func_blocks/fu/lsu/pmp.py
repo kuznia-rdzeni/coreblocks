be4d3ddb24696f75f428eb386c2ec9505bdf95e4
Artur Bieniek <ar2rekb@gmail.com>
2025-05-23 02:02:33 +0200

    Complete PMP implementation

--- coreblocks/func_blocks/fu/lsu/pmp.py
+++ coreblocks/func_blocks/fu/lsu/pmp.py
@@ -1,5 +1,3 @@
-from functools import reduce
-from operator import or_
 from amaranth import *
 from amaranth.lib import data
 from coreblocks.interface.keys import CSRInstancesKey
@@ -7,6 +5,8 @@ from transactron.utils.dependencies import DependencyContext
 from coreblocks.params import *
 from transactron.utils import HasElaborate
 from transactron.core import TModule, Transaction
+from transactron.utils.amaranth_ext.coding import PriorityEncoder
+from transactron.utils.amaranth_ext.functions import count_trailing_zeros
 
 
 class PMPLayout(data.StructLayout):
@@ -35,33 +35,39 @@ class PMPChecker(Elaboratable):
         m = TModule()
         self.dependency_manager = DependencyContext.get()
         m_csr = self.dependency_manager.get_dependency(CSRInstancesKey()).m_mode
-        # Możliwe że w niektórych testach to jest niedostępne?
-        # Trzeba dodać w samych testach CSRInstancesKey!!!
 
-        with Transaction(name="PMP CSR read").body(m):
+        with Transaction(name="PMP_CSR_read").body(m):
             pmpxcfg_val = [cfg.read(m).data for cfg in m_csr.pmpxcfg]
             pmpaddrx_val = [addr.read(m).data for addr in m_csr.pmpaddrx]
-            outputs = [Signal(PMPLayout()) for _ in pmpxcfg_val]
+            outputs = Array(Signal(PMPLayout()) for _ in pmpxcfg_val)
+            matchings = Array(Signal() for _ in pmpxcfg_val)
             for i, (cfg, addr) in enumerate(zip(pmpxcfg_val, pmpaddrx_val)):
                 a_flag = (cfg & 0b11000) >> 3
-                matching = False
+                matching = Signal(init=0)
                 with m.If(a_flag == 1):
-                    # A=1 - Top of range - od wartości poprzedniego do tego adr
+                    # A=1 - Top of range - od wartości poprzedniego do tego addr
                     start = pmpaddrx_val[i - 1] if i > 0 else 0
                     end = addr
-                    with m.If((self.addr >= start) & (self.addr <= end)):
-                        matching = True
+                    with m.If((self.addr >= start) & (self.addr < end)):
+                        matching.eq(1)
                 with m.Elif(a_flag == 2):
                     # A=2 - NA4 - region 4 bajtowy
-                    with m.If(self.addr == addr):
-                        matching = True
+                    with m.If(self.addr >> 2 == addr):
+                        matching.eq(1)
                 with m.Elif(a_flag == 3):
-                    # A=3 - NAPOT - region 8*2^(tyle na jakiej pozycji jest pierwsze zero od prawej)
-                    # TODO
+                    # A=3 - NAPOT - region 2^(3 + tyle na jakiej pozycji jest pierwsze zero od prawej)
+                    fzero = count_trailing_zeros(~addr)
+                    size = 1 << (fzero + 3)
+                    start = addr - (fzero - 1)
+                    end = start + size
+                    with m.If((self.addr >= start) & (self.addr < end)):
+                        matching.eq(1)
                     pass
-                with m.If(matching):
-                    m.d.comb += outputs[i].eq(cfg & 111)
-            # TODO: Ma wchodzić tylko jeden region i koniec dalej nie sprawdzamy
-            # Jak to zrobić??
-            m.d.comb += self.result.eq(reduce(or_, [Value.cast(o) for o in outputs], 0))
+                m.d.comb += outputs[i].eq(cfg & 111)
+                m.d.comb += matchings[i].eq(matching)
+            m.submodules.enc_select = enc_select = PriorityEncoder(width=len(pmpxcfg_val))
+            # Trzeba dodać do submodules żeby nie leciały warningi że ten PriorityEncoder nie jest używany!!
+            select_vector = Cat(matchings)
+            m.d.comb += enc_select.i.eq(select_vector)
+            m.d.sync += self.result.eq(outputs[enc_select.o])
         return m
