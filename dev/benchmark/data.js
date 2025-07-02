dc04395c7d4eb640896a2f3a775343c4f3fdd38c
github-action-benchmark <github@users.noreply.github.com>
2025-07-02 11:37:37 +0000

    add Fmax and LCs (basic) (customBiggerIsBetter) benchmark result for e3f5556717864779cae5ae6eb959835b40ff8d93

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751456255053,
+  "lastUpdate": 1751456257049,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -37854,6 +37854,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 1422,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1552,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 6545,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e3f5556717864779cae5ae6eb959835b40ff8d93",
+          "message": "Superscalar RF (#827)",
+          "timestamp": "2025-07-02T13:15:46+02:00",
+          "tree_id": "dd1b5cc56a2f318a8b1631b9901fb46a72bab157",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/e3f5556717864779cae5ae6eb959835b40ff8d93"
+        },
+        "date": 1751456256185,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 50.9,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 20836,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1390,
             "unit": "LUT"
           },
           {
