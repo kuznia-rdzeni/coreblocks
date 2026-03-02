7a9f030243db466d0628d00ef06ed88ae8edb6df
Marek Materzok <tilk@tilk.eu>
2026-03-02 14:42:40 +0100

    Start refactor

--- coreblocks/scheduler/scheduler.py
+++ coreblocks/scheduler/scheduler.py
@@ -2,13 +2,13 @@ from collections.abc import Sequence
 
 from amaranth import *
 
-from transactron import Method, Transaction, TModule
+from transactron import Method, Required, Transaction, TModule
 from transactron.lib import FIFO
 from transactron.lib.connectors import Connect
 from transactron.utils import assign, AssignType
 from transactron.utils.dependencies import DependencyContext
 
-from coreblocks.interface.layouts import SchedulerLayouts
+from coreblocks.interface.layouts import DecodeLayouts, RATLayouts, RFLayouts, ROBLayouts, SchedulerLayouts
 from coreblocks.params import GenParams
 from coreblocks.arch.optypes import OpType
 from coreblocks.interface.keys import CoreStateKey
@@ -23,34 +23,29 @@ class RegAllocation(Elaboratable):
     the instruction result). A part of the scheduling process.
     """
 
-    def __init__(self, *, get_instr: Method, push_instr: Method, get_free_reg: Method, gen_params: GenParams):
+    get_instr: Required[Method]
+    push_instr: Required[Method]
+    get_free_reg: Required[Method]
+
+    def __init__(self, *, gen_params: GenParams):
         """
         Parameters
         ----------
-        get_instr: Method
-            Method providing decoded instructions to be scheduled for execution. Uses
-            `SchedulerLayouts.reg_alloc_in`.
-        push_instr: Method
-            Method used for pushing the serviced instruction to the next step. Uses `SchedulerLayouts.reg_alloc_out`.
-        get_free_reg: Method
-             Method providing the ID of a currently free physical register.
         gen_params: GenParams
             Core generation parameters.
         """
         self.gen_params = gen_params
         layouts = gen_params.get(SchedulerLayouts)
-        self.input_layout = layouts.reg_alloc_in
-        self.output_layout = layouts.reg_alloc_out
 
-        self.get_instr = get_instr
-        self.push_instr = push_instr
-        self.get_free_reg = get_free_reg
+        self.get_instr = Method(o=layouts.reg_alloc_in)
+        self.push_instr = Method(i=layouts.reg_alloc_out)
+        self.get_free_reg = Method(o=[("ident", gen_params.phys_regs_bits)])
 
     def elaborate(self, platform):
         m = TModule()
 
         free_reg = Signal(self.gen_params.phys_regs_bits)
-        data_out = Signal(self.output_layout)
+        data_out = Signal(self.push_instr.layout_in)
 
         with Transaction().body(m):
             instr = self.get_instr(m)
@@ -70,20 +65,22 @@ class InstructionTagger(Elaboratable):
     `Scheduler` driver of `CheckpointRAT` `tag` stage. See `CheckpointRAT.tag` for description.
     """
 
-    def __init__(self, *, get_instr: Method, push_instr: Method, crat_tag: Method, gen_params: GenParams):
+    get_free_reg: Required[Method]
+    get_instr: Required[Method]
+    push_instr: Required[Method]
+
+    def __init__(self, *, gen_params: GenParams):
         self.gen_params = gen_params
         layouts = gen_params.get(SchedulerLayouts)
-        self.input_layout = layouts.instr_tag_in
-        self.output_layout = layouts.instr_tag_out
 
-        self.get_instr = get_instr
-        self.push_instr = push_instr
-        self.crat_tag = crat_tag
+        self.get_instr = Method(o=layouts.instr_tag_in)
+        self.push_instr = Method(i=layouts.instr_tag_out)
+        self.crat_tag = Method(i=gen_params.get(RATLayouts).crat_tag_in, o=gen_params.get(RATLayouts).crat_tag_out)
 
     def elaborate(self, platform):
         m = TModule()
 
-        data_out = Signal(self.output_layout)
+        data_out = Signal(self.push_instr.layout_in)
 
         with Transaction().body(m):
             instr = self.get_instr(m)
@@ -421,68 +418,65 @@ class Scheduler(Elaboratable):
     Instruction without any supporting RS will get stuck and block the scheduler pipeline.
     """
 
+    get_instr: Required[Method]
+    """
+    Method providing decoded instructions to be scheduled for execution. It has layout as described
+    by `SchedulerLayouts.scheduler_in`. 
+    """
+
+    get_free_reg: Required[Method]
+    """Provides the ID of a currently free physical register."""
+
+    crat_rename: Required[Method]
+    """Renames the source register in C-RAT."""
+
+    crat_tag: Required[Method]
+    """Tags instructions to checkpoints in C-RAT."""
+
+    crat_active_tags: Required[Method]
+    """Gets information about tags that are on current speculation path from C-RAT."""
+
+    rob_put: Required[Method]
+    """Gets a free entry in ROB."""
+
+    rf_read_req1: Required[Method]
+    """Requests value of first source register."""
+
+    rf_read_req2: Required[Method]
+    """Requests value of second source register."""
+
+    rf_read_resp1: Required[Method]
+    """Gets requested value of first source register and information if it is valid."""
+
+    rf_read_resp2: Required[Method]
+    """Gets requested value of second source register and information if it is valid."""
+
     def __init__(
         self,
         *,
-        get_instr: Method,
-        get_free_reg: Method,
-        crat_rename: Method,
-        crat_tag: Method,
-        crat_active_tags: Method,
-        rob_put: Method,
-        rf_read_req1: Method,
-        rf_read_req2: Method,
-        rf_read_resp1: Method,
-        rf_read_resp2: Method,
-        reservation_stations: Sequence[tuple[FuncBlock, set[OpType]]],
-        gen_params: GenParams
+        gen_params: GenParams,
+        reservation_stations: Sequence[tuple[FuncBlock, set[OpType]]]
     ):
         """
         Parameters
         ----------
-        get_instr: Method
-            Method providing decoded instructions to be scheduled for execution. It has
-            layout as described by `DecodeLayouts.decoded_instr`.
-        get_free_reg: Method
-            Method providing the ID of a currently free physical register.
-        crat_rename: Method
-            Method used for renaming the source register in C-RAT. Uses `RATLayouts.crat_rename_in`
-            and `RATLayouts.crat_rename_out`.
-        crat_tag: Method
-            Method used for tagging instruction to checkpoints in C-RAT.
-        crat_active_tags: Method
-            Method used to get information about tags that are on current speculation path from C-RAT.
-        rob_put: Method
-            Method used for getting a free entry in ROB. Uses `ROBLayouts.data_layout`.
-        rf_read_req1: Method
-            Method used for requesting value of first source register and information if it is valid.
-            Uses `RFLayouts.rf_read_out`.
-        rf_read_req2: Method
-            Method used for requesting value of second source register and information if it is valid.
-            Uses `RFLayouts.rf_read_out`.
-        rf_read_resp1: Method
-            Method used for getting value of first source register and information if it is valid.
-            Uses `RFLayouts.rf_read_out` and `RFLayouts.rf_read_in`.
-        rf_read_resp2: Method
-            Method used for getting value of second source register and information if it is valid.
-            Uses `RFLayouts.rf_read_out` and `RFLayouts.rf_read_in`.
-        reservation_stations: Sequence[FuncBlock]
-            Sequence of units with RS interfaces to which instructions should be inserted.
         gen_params: GenParams
             Core generation parameters.
+        reservation_stations: Sequence[FuncBlock]
+            Sequence of units with RS interfaces to which instructions should be inserted.
         """
         self.gen_params = gen_params
         self.layouts = self.gen_params.get(SchedulerLayouts)
-        self.get_instr = get_instr
-        self.get_free_reg = get_free_reg
-        self.crat_rename = crat_rename
-        self.crat_active_tags = crat_active_tags
-        self.crat_tag = crat_tag
-        self.rob_put = rob_put
-        self.rf_read_req1 = rf_read_req1
-        self.rf_read_req2 = rf_read_req2
-        self.rf_read_resp1 = rf_read_resp1
-        self.rf_read_resp2 = rf_read_resp2
+        self.get_instr = Method(o=self.layouts.scheduler_in)
+        self.get_free_reg = Method(o=[("ident", gen_params.phys_regs_bits)])
+        self.crat_rename = Method(i=gen_params.get(RATLayouts).crat_rename_in, o=gen_params.get(RATLayouts).crat_rename_out)
+        self.crat_active_tags = Method(o=gen_params.get(RATLayouts).get_active_tags_out)
+        self.crat_tag = Method(i=gen_params.get(RATLayouts).crat_tag_in, o=gen_params.get(RATLayouts).crat_tag_out)
+        self.rob_put = Method(i=gen_params.get(ROBLayouts).put_layout, o=gen_params.get(ROBLayouts).put_out_layout)
+        self.rf_read_req1 = Method(i=gen_params.get(RFLayouts).rf_read_in)
+        self.rf_read_req2 = Method(i=gen_params.get(RFLayouts).rf_read_in)
+        self.rf_read_resp1 = Method(i=gen_params.get(RFLayouts).rf_read_in, o=gen_params.get(RFLayouts).rf_read_out)
+        self.rf_read_resp2 = Method(i=gen_params.get(RFLayouts).rf_read_in, o=gen_params.get(RFLayouts).rf_read_out)
         self.rs = reservation_stations
 
     def elaborate(self, platform):
@@ -490,20 +484,16 @@ class Scheduler(Elaboratable):
 
         # This could be ideally changed to Connect, but unfortunately causes comb loop
         m.submodules.alloc_rename_buf = alloc_rename_buf = FIFO(self.layouts.reg_alloc_out, 2)
-        m.submodules.reg_alloc = RegAllocation(
-            get_instr=self.get_instr,
-            push_instr=alloc_rename_buf.write,
-            get_free_reg=self.get_free_reg,
-            gen_params=self.gen_params,
-        )
+        m.submodules.reg_alloc = reg_alloc = RegAllocation(gen_params=self.gen_params)
+        reg_alloc.get_instr.provide(self.get_instr)
+        reg_alloc.push_instr.provide(alloc_rename_buf.write)
+        reg_alloc.get_free_reg.provide(self.get_free_reg)
 
         m.submodules.instr_tag_buf = instr_tag_buf = FIFO(self.layouts.instr_tag_out, 2)
-        m.submodules.instr_tag = InstructionTagger(
-            get_instr=alloc_rename_buf.read,
-            push_instr=instr_tag_buf.write,
-            crat_tag=self.crat_tag,
-            gen_params=self.gen_params,
-        )
+        m.submodules.instr_tag = instr_tag = InstructionTagger(gen_params=self.gen_params)
+        instr_tag.get_instr.provide(alloc_rename_buf.read)
+        instr_tag.push_instr.provide(instr_tag_buf.write)
+        instr_tag.crat_tag.provide(self.crat_tag)
 
         m.submodules.rename_out_buf = rename_out_buf = Connect(self.layouts.renaming_out)
         m.submodules.renaming = Renaming(
