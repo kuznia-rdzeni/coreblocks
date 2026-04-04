a6c93919a3d8fb00d5a321c872354cfcbb896a12
Dawid Pawliczek <dawidpawliczek4@gmail.com>
2026-04-04 20:45:47 +0200

    Add pmp grain to configuration
    # Conflicts:
    #	coreblocks/params/configurations.py

--- coreblocks/params/configurations.py
+++ coreblocks/params/configurations.py
@@ -121,6 +121,8 @@ class _CoreConfigurationDataClass:
         Number of implemented HPM counters (mhpmcounter3..mhpmcounter31).
     pmp_register_count: int
         Number of Physical Memory Protection CSR entries. Valid values are: 0, 16, and 64.
+    pmp_grain: int
+        Grain parameter of Physical Memory Protection. pmp_grain must be >= icache_line_bytes_log + 2
     allow_partial_extensions: bool
         Allow partial support of extensions.
     extra_verification: bool
@@ -184,6 +186,7 @@ class _CoreConfigurationDataClass:
     hpm_counters_count: int = 0
 
     pmp_register_count: int = 0
+    pmp_grain: int = 3
 
     allow_partial_extensions: bool = False
 
@@ -222,6 +225,7 @@ tiny_core_config = CoreConfiguration(
     icache_enable=False,
     user_mode=False,
     supervisor_mode=False,
+    pmp_grain=0,
 )
 
 # Basic core config with minimal additions required for Linux
