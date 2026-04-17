2c48d4c53e8762885ca6e656f765bca753f84658
github-action-benchmark <github@users.noreply.github.com>
2026-04-17 08:58:10 +0000

    add Fmax and LCs (basic) (customBiggerIsBetter) benchmark result for 78c1585c1de1989f45dac10785f6e148b25f561d

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776416287871,
+  "lastUpdate": 1776416290209,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -42384,6 +42384,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as DFF: (ECP5)",
             "value": 4563,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "78c1585c1de1989f45dac10785f6e148b25f561d",
+          "message": "implement S mode CSRs (#878)",
+          "timestamp": "2026-04-17T10:31:09+02:00",
+          "tree_id": "4ebf989a1d8ff5b07258a4b578738d062d56b5e9",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/78c1585c1de1989f45dac10785f6e148b25f561d"
+        },
+        "date": 1776416289454,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 47.4,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 17743,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1380,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1324,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 4672,
             "unit": "LUT"
           }
         ]
