2d586c7558656ce613df33dc1b067f11f0f987e3
Marek Materzok <tilk@tilk.eu>
2026-04-23 21:27:10 +0200

    Generalized instruction metric (#912)

--- coreblocks/func_blocks/fu/common/func_base.py
+++ coreblocks/func_blocks/fu/common/func_base.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
-from amaranth import Elaboratable
-from transactron import Method, TModule
+from amaranth import Elaboratable, Signal
+from transactron import Method, TModule, def_method
+from transactron.lib import HwMetric
+from transactron.utils import DependencyContext, assign, extend_layout
 from coreblocks.params import GenParams
+from coreblocks.interface.keys import InstructionTaggedCounterKey
 from coreblocks.interface.layouts import FuncUnitLayouts
 from coreblocks.func_blocks.interface.func_protocols import FuncUnit
 from coreblocks.func_blocks.fu.common.fu_decoder import DecoderManager
@@ -21,14 +24,29 @@ class FuncUnitBase(ABC, FuncUnit, Elaboratable, Generic[_T_DecoderManager]):
         self.gen_params = gen_params
         self.layouts = gen_params.get(FuncUnitLayouts)
         self.issue = Method(i=self.layouts.issue)
+        self.issue_decoded = Method(i=extend_layout(self.layouts.issue, ("decode_fn", fn.Fn)))
         self.push_result = Method(i=self.layouts.push_result)
+        self.increment_counter = HwMetric.wrap_method(Method(i=[("tag", fn.Fn)]))
         self.fn = fn
-        self.decoder = fn.get_decoder(gen_params)
+
+        if HwMetric.metrics_enabled():
+            dm = DependencyContext.get()
+            dm.add_dependency(InstructionTaggedCounterKey(), (self.__class__.__name__, self.increment_counter))
 
     @abstractmethod
     def elaborate(self, platform) -> TModule:
         m = TModule()
 
-        m.submodules.decoder = self.decoder
+        m.submodules.decoder = decoder = self.fn.get_decoder(self.gen_params)
+
+        @def_method(m, self.issue)
+        def _(arg):
+            new_arg = Signal(self.issue_decoded.layout_in)
+            m.d.av_comb += decoder.exec_fn.eq(arg.exec_fn)
+            m.d.av_comb += assign(new_arg, arg)
+            m.d.av_comb += new_arg.decode_fn.eq(decoder.decode_fn)
+            self.issue_decoded(m, new_arg)
+
+            self.increment_counter(m, decoder.decode_fn)
 
         return m
