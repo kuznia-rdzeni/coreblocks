ff6d3651abe37034faa1cfe2f28c95a6f76cd5a5
Marek Materzok <tilk@tilk.eu>
2026-04-27 18:20:20 +0200

    Superscalar R-RAT (#914)

--- test/core_structs/test_rat.py
+++ test/core_structs/test_rat.py
@@ -7,6 +7,8 @@ from coreblocks.params.configurations import test_core_config
 from collections import deque
 from random import Random
 
+import pytest
+
 
 class TestFrontendRegisterAliasTable(TestCaseWithSimulator):
     def gen_input(self):
@@ -51,31 +53,40 @@ class TestFrontendRegisterAliasTable(TestCaseWithSimulator):
             sim.add_testbench(self.do_rename)
 
 
+@pytest.mark.parametrize("ports", [1, 2, 4])
 class TestRetirementRegisterAliasTable(TestCaseWithSimulator):
-    def gen_input(self):
-        for _ in range(self.test_steps):
-            rl = self.rand.randrange(self.gen_params.isa.reg_cnt)
-            rp = self.rand.randrange(1, 2**self.gen_params.phys_regs_bits) if rl != 0 else 0
+    async def clear_chosen(self, sim: TestbenchContext):
+        while True:
+            self.chosen = set()
+            await sim.tick()
 
-            self.to_execute_list.append({"rl": rl, "rp": rp})
+    def do_commit(self, i: int):
+        async def tb(sim: TestbenchContext):
+            # wait until R-RAT clears itself after reset
+            await self.tick(sim, self.gen_params.isa.reg_cnt)
+            for _ in range(self.test_steps):
+                await sim.delay(1e-12 * i)
+                rl = self.rand.choice(list(set(range(self.gen_params.isa.reg_cnt)) - self.chosen))
+                rp = self.rand.randrange(1, 2**self.gen_params.phys_regs_bits) if rl != 0 else 0
+                self.chosen.add(rl)
 
-    async def do_commit(self, sim: TestbenchContext):
-        # wait until R-RAT clears itself after reset
-        await self.tick(sim, self.gen_params.isa.reg_cnt)
-        for _ in range(self.test_steps):
-            to_execute = self.to_execute_list.pop()
-            if self.rand.randrange(2):
-                peek_res = await self.m.peek.call(sim, rl_dst=to_execute["rl"])
-                assert peek_res.old_rp_dst == self.expected_entries[to_execute["rl"]]
-            res = await self.m.commit.call(sim, rl_dst=to_execute["rl"], rp_dst=to_execute["rp"])
-            assert res.old_rp_dst == self.expected_entries[to_execute["rl"]]
+                if self.rand.randrange(2):
+                    peek_res = await self.m.peek[i].call(sim, rl_dst=rl)
+                    assert peek_res.old_rp_dst == self.expected_entries[rl]
+                else:
+                    res = await self.m.commit[i].call(sim, rl_dst=rl, rp_dst=rp)
+                    assert res.old_rp_dst == self.expected_entries[rl]
 
-            self.expected_entries[to_execute["rl"]] = to_execute["rp"]
+                    self.expected_entries[rl] = rp
 
-    def test_single(self):
+        return tb
+
+    def test_single(self, ports: int):
         self.rand = Random(0)
         self.test_steps = 2000
-        self.gen_params = GenParams(test_core_config.replace(phys_regs_bits=5, rob_entries_bits=6))
+        self.gen_params = GenParams(
+            test_core_config.replace(phys_regs_bits=5, rob_entries_bits=6, retirement_superscalarity=ports)
+        )
         m = SimpleTestCircuit(RRAT(gen_params=self.gen_params))
         self.m = m
 
@@ -85,6 +96,7 @@ class TestRetirementRegisterAliasTable(TestCaseWithSimulator):
         self.to_execute_list = deque()
         self.expected_entries = [0 for _ in range(self.log_regs)]
 
-        self.gen_input()
         with self.run_simulation(m) as sim:
-            sim.add_testbench(self.do_commit)
+            for i in range(ports):
+                sim.add_testbench(self.do_commit(i))
+                sim.add_testbench(self.clear_chosen, background=True)
