7a39c5e6cb10874ee6a3f605f460198de1fed87f
Mateusz Marszalek <matti.marszalek@gmail.com>
2026-05-11 01:42:23 +0200

    Added signals

--- coreblocks/func_blocks/fu/fpu/dr_division.py
+++ coreblocks/func_blocks/fu/fpu/dr_division.py
@@ -1,5 +1,6 @@
 from amaranth import *
 from transactron import TModule, Method, def_method
+from transactron.utils import from_method_layout
 from coreblocks.func_blocks.fu.fpu.otfc import *
 from coreblocks.func_blocks.fu.fpu.qsf_tables import *
 from coreblocks.func_blocks.fu.fpu.fpu_qsf import *
@@ -69,9 +70,10 @@ class DrDivModule(Elaboratable):
         Returns 'division_run_out_layout' as argument
     """
 
-    def __init__(self, *, div_params: DrDivParams):
-        self.div_params  = div_params
-        self.otfc_params = OTFCParams(result_width=self.div_params.result_width) 
+    def __init__(self, *, div_params: DrDivParams, qsf_params: QSFParams):
+        self.div_params = div_params
+        self.qsf_params = qsf_params
+        self.otfc_params = OTFCParams(result_width=self.div_params.result_width)
         self.method_layouts = DrDivMethodLayout(dr_div_params=div_params)
         self.div_init = Method(i=self.method_layouts.division_init_in_layout)
         self.div_run = Method(o=self.method_layouts.division_run_out_layout)
@@ -79,4 +81,18 @@ class DrDivModule(Elaboratable):
     def elaborate(self, platform):
         m = Tmodule()
         m.submodule.otfc = otfc = OTFCModule(otfc_params=self.otfc_params)
-        m.submodule.
+        m.submodule.qsf = qsf = QSFModule(qsf_params=self.qsf_params)
+
+        additional_iterations = 2
+        max_divisor = 2 ** (self.div_params.op_width) - 1
+
+        counter = Signal(
+            range(0, self.div_params.iterations + additional_iterations + 1)
+        )
+        residual = Signal(3 + self.div_params.op_width)
+        adjusted_divisor = Signal(self.div_params.op_width + 1)
+
+        two_p = Signal(range(0, (2 * max_divisor) + 1))
+        one_p = Signal(range(0, max_divisor + 1))
+        m_one_p = Signal(range(-max_divisor, 1))
+        m_two_p = Signal(range(-2 * max_divisor, 1))
