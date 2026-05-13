e5f9f66142c8e66dd31d1ca20eaf1efb58e98a59
github-action-benchmark <github@users.noreply.github.com>
2026-05-13 16:25:08 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for c178cbac7c2b610114108387b28dcc061b9445b1

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778430138995,
+  "lastUpdate": 1778689508243,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -22037,6 +22037,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/e2fdf283f41ee7d7823d60f36270d4b8a453d1ad"
         },
         "date": 1778430126093,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.4310548345625105,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.5544192447280725,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.3821075915511284,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.6529039609007227,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.3609059761135394,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.30483586342368985,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.3308119011598588,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.44964532430803467,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c178cbac7c2b610114108387b28dcc061b9445b1",
+          "message": "[MMU 1/3] Implement L1 TLB - fully associative page table entry cache (#942)",
+          "timestamp": "2026-05-13T17:54:02+02:00",
+          "tree_id": "fe165d34507d6619ac046ba145cbb52daac840e4",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/c178cbac7c2b610114108387b28dcc061b9445b1"
+        },
+        "date": 1778689498613,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
