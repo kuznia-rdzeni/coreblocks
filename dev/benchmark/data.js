6ff48d38dd1dadace4ca2446faf68883a79e423d
github-action-benchmark <github@users.noreply.github.com>
2026-05-13 16:25:13 +0000

    add Fmax and LCs (full) (customBiggerIsBetter) benchmark result for c178cbac7c2b610114108387b28dcc061b9445b1

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778689510827,
+  "lastUpdate": 1778689513499,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -61782,6 +61782,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as DFF: (ECP5)",
             "value": 10184,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c178cbac7c2b610114108387b28dcc061b9445b1",
+          "message": "[MMU 1/3] Implement L1 TLB - fully associative page table entry cache (#942)",
+          "timestamp": "2026-05-13T17:54:02+02:00",
+          "tree_id": "fe165d34507d6619ac046ba145cbb52daac840e4",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/c178cbac7c2b610114108387b28dcc061b9445b1"
+        },
+        "date": 1778689512694,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 41.24,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 50016,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 3550,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 2632,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 10174,
             "unit": "LUT"
           }
         ]
