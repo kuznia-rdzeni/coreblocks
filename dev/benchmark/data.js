7137543dc065dd18118988244f64cf0096180abe
github-action-benchmark <github@users.noreply.github.com>
2026-06-09 07:23:17 +0000

    add Fmax and LCs (full) (customBiggerIsBetter) benchmark result for 810cbe8ec2b64252a3039efa4ebfb122a38905bb

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780989795202,
+  "lastUpdate": 1780989797381,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -64683,6 +64683,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Device utilisation: (ECP5)",
             "value": 49555,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 3560,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 2600,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 10353,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "810cbe8ec2b64252a3039efa4ebfb122a38905bb",
+          "message": "migrate from riscof to riscv-arch-tests (#955)",
+          "timestamp": "2026-06-09T08:39:57+02:00",
+          "tree_id": "7d12bc8fe891f249c9e488fa471ab6e73f76de73",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/810cbe8ec2b64252a3039efa4ebfb122a38905bb"
+        },
+        "date": 1780989796776,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 41.05,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 49688,
             "unit": "LUT4"
           },
           {
