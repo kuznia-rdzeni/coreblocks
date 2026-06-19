ce5cc70ec1588ceb234e2ce7218cc3b9ebcc845c
Jakub Janeczko <kuba@ev1.pl>
2026-06-19 21:02:46 +0200

    non-zero asid

--- coreblocks/params/core_configuration.py
+++ coreblocks/params/core_configuration.py
@@ -190,7 +190,7 @@ class _CoreConfigurationDataClass:
     user_mode: bool = True
     supervisor_mode: bool = True
 
-    asidlen: int = 0
+    asidlen: int | None = None
     supported_vm_schemes: Collection[SatpMode] = (SatpMode.BARE, SatpMode.SV32)
     phys_addr_bits: int | None = None
     hpm_counters_count: int = 0
