82388cde8c4ca5fbe08985032aca5c620f9f9f98
Jakub Janeczko <kuba@ev1.pl>
2026-07-05 20:13:26 +0200

    cleanup

--- coreblocks/priv/pmp.py
+++ coreblocks/priv/pmp.py
@@ -86,7 +86,7 @@ class AreaPMPChecker(Elaboratable):
 
         if n == 0:
             m.d.comb += self.uniform.eq(1)
-            m.d.comb += assign(self.matching_entry, {"r": 1, "w": 1, "x": 1, "l": 0})
+            m.d.comb += self.matching_entry.eq(PMPLayoutFull().const({"r": 1, "w": 1, "x": 1, "l": 0}))
             return m
 
         cfgs = [data.View(PMPCfgLayout(), self.csr.pmpxcfg[i].value) for i in range(n)]
@@ -94,6 +94,10 @@ class AreaPMPChecker(Elaboratable):
 
         size_class_masks = [(1 << max(sc - 2, grain)) - 1 for sc in self.size_classes]
         size_class_mask = Array(size_class_masks)[self.size_class]
+
+        paddr = Signal(self.gen_params.phys_addr_bits)
+        m.d.comb += paddr.eq(self.paddr)
+        m.d.comb += paddr[: max(grain + 2, min(self.size_classes))].eq(0)
 
         matches_any = Signal(n)
         matches_all = Signal(n)
@@ -110,12 +114,8 @@ class AreaPMPChecker(Elaboratable):
                 with m.Case(PMPAFlagEncoding.TOR):
                     bottom = addr_vals[i - 1][grain:] if i > 0 else 0
                     addr_hi = (self.paddr[2:] | size_class_mask) >> grain
-                    m.d.comb += matches_any[i].eq(
-                        (bottom <= addr_hi) & (self.paddr[2 + grain :] < addr_vals[i][grain:])
-                    )
-                    m.d.comb += matches_all[i].eq(
-                        (bottom <= self.paddr[2 + grain :]) & (addr_hi < addr_vals[i][grain:])
-                    )
+                    m.d.comb += matches_any[i].eq((bottom <= addr_hi) & (paddr[2 + grain :] < addr_vals[i][grain:]))
+                    m.d.comb += matches_all[i].eq((bottom <= paddr[2 + grain :]) & (addr_hi < addr_vals[i][grain:]))
                 if grain == 0:
                     with m.Case(PMPAFlagEncoding.NA4):
                         m.d.comb += napot_mask.eq(0)
@@ -124,15 +124,15 @@ class AreaPMPChecker(Elaboratable):
                     m.d.comb += napot_mask[start_bit:].eq(addr_vals[i][start_bit:] ^ (addr_vals[i][start_bit:] + 1))
                     m.d.comb += is_napot.eq(1)
 
-            m.d.comb += napot_mask[:start_bit].eq(~0)
+            m.d.comb += napot_mask[:grain].eq(~0)
 
             with m.If(is_napot):
                 common_mask = size_class_mask | napot_mask
-                m.d.comb += matches_any[i].eq((self.paddr[2:] & ~common_mask) == (addr_vals[i] & ~common_mask))
+                m.d.comb += matches_any[i].eq((paddr[2:] & ~common_mask) == (addr_vals[i] & ~common_mask))
 
                 # technically should be (common_mask == napot_mask) & matches_any[i], but the implication
                 # is not required for the rest of the circuit (we are only looking at first any-matching)
-                m.d.comb += matches_all[i].eq(common_mask == napot_mask)  # same as napot_mask >=
+                m.d.comb += matches_all[i].eq(common_mask == napot_mask)  # same as napot_log2 >= size_class_log2
 
             m.d.comb += assign(
                 all_data[i],
@@ -149,14 +149,16 @@ class AreaPMPChecker(Elaboratable):
 
         default_input = Signal(layout)
         m.d.comb += default_input.matches_all.eq(1)
+        m.d.comb += default_input.entry_data.eq(PMPLayoutFull().const({"r": 0, "w": 0, "x": 0, "l": 0}))
 
-        # entry fully covers the match if all entries before the fully matching one don't match any
-        # this is the same as checking the the first any-matching entry also is fully matching
+        # entry fully covers the match if all entries before the fully matching one don't match any bytes.
+        # this is the same as checking the the first any-matching entry is also fully matching
         selected = OneHotMux.create(
             m, [(matches_any[i], all_data[i]) for i in range(n)], priority=True, default_input=default_input
         )
 
-        # hardcode uniform signal if we know via grain size that that is the case for size reduction
+        # hardcode uniform signal if we know via grain size that it is always uniform
+        # this allows the toolchain to remove half of the comparators for TOR
         if all(sc <= grain + 2 for sc in self.size_classes):
             m.d.comb += self.uniform.eq(1)
         else:
@@ -220,7 +222,7 @@ class PMPChecker(Elaboratable):
 
         m.submodules.pmp_area_checker = pmp_area_checker = AreaPMPChecker(self.gen_params, [self.access_size_log])
 
-        m.d.comb += pmp_area_checker.paddr[2:].eq(self.paddr[2:])
+        m.d.comb += pmp_area_checker.paddr.eq(self.paddr)
         m.d.comb += pmp_area_checker.size_class.eq(0)
 
         with m.If(~pmp_area_checker.uniform):
