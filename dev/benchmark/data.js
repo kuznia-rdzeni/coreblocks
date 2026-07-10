231d3634bae38a08c96137764b1fe4e3cd63658e
github-action-benchmark <github@users.noreply.github.com>
2026-07-10 06:50:17 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 7fdf1bc7055a06c3250278af53956945eb718b89

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783624099093,
+  "lastUpdate": 1783666217706,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -24597,6 +24597,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/4102498d3ce2e1bda03f21886ed0695851e08dbd"
         },
         "date": 1783624081920,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.4486953069382838,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.6047801963294921,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.37129390940316226,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.6493506493506493,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.35847370537509493,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.3060531192093885,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.3432821873773525,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.4457149159895584,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7fdf1bc7055a06c3250278af53956945eb718b89",
+          "message": "RS: use OneHotMux (#1013)",
+          "timestamp": "2026-07-10T07:56:37+02:00",
+          "tree_id": "e029285e2484709feb770156938bd79581a73810",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/7fdf1bc7055a06c3250278af53956945eb718b89"
+        },
+        "date": 1783666208022,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
