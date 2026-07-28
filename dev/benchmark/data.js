6511de75ac156445fd37a59794838e26d3bd41aa
github-action-benchmark <github@users.noreply.github.com>
2026-07-28 19:54:48 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for d9145a21cb4541e1eeb306c7a273ee82816e821b

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785235299666,
+  "lastUpdate": 1785268488140,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -25749,6 +25749,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/36179d9cfc4c1aa1656cbde82e495a320a3ea6de"
         },
         "date": 1785235280657,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.44844731541673366,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.5951740300321152,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.3661826587675101,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.6450701307924483,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.35531157138749,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.30097743800508947,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.3402741852043456,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.4437417112544016,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9145a21cb4541e1eeb306c7a273ee82816e821b",
+          "message": "Enum refactor (#1022)",
+          "timestamp": "2026-07-28T20:57:55+02:00",
+          "tree_id": "8e5164adb6e04a3010a03e02234b176b78d52731",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/d9145a21cb4541e1eeb306c7a273ee82816e821b"
+        },
+        "date": 1785268476080,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
