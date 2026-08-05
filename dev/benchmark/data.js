f4e891e52e66fec26c580c4595a38f4b9b28b644
github-action-benchmark <github@users.noreply.github.com>
2026-08-05 08:52:50 +0000

    add Fmax and LCs (basic, XC7K480T) (customBiggerIsBetter) benchmark result for aa853adf9dd47d03a72b024d91a47b75d01d8b58

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785919967723,
+  "lastUpdate": 1785919969831,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -72597,6 +72597,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Block RAM used",
             "value": 0,
+            "unit": "RAMB36E1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa853adf9dd47d03a72b024d91a47b75d01d8b58",
+          "message": "add missing U-bit check for non-leaf PTEs (#1039)",
+          "timestamp": "2026-08-05T09:47:13+02:00",
+          "tree_id": "026090f31ebc02b5922767aba7f5ec4d72bf6f4d",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/aa853adf9dd47d03a72b024d91a47b75d01d8b58"
+        },
+        "date": 1785919969354,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 45.01,
+            "unit": "MHz"
+          },
+          {
+            "name": "LUT used",
+            "value": 17477,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUT DFF used",
+            "value": 7970,
+            "unit": "LUT"
+          },
+          {
+            "name": "Slice carry chains used",
+            "value": 304,
+            "unit": "Slice"
+          },
+          {
+            "name": "Block RAM used",
+            "value": 3,
             "unit": "RAMB36E1"
           }
         ]
