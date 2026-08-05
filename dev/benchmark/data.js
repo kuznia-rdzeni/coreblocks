b84e45710038b2f8be5d603cb437acb4e04c9d28
github-action-benchmark <github@users.noreply.github.com>
2026-08-05 14:07:22 +0000

    add Fmax and LCs (basic, XC7K480T) (customBiggerIsBetter) benchmark result for 4f11685cc5b8c1cf52a95894a584314ccd8900f5

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785938839394,
+  "lastUpdate": 1785938842177,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -72906,6 +72906,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Block RAM used",
             "value": 0,
+            "unit": "RAMB36E1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4f11685cc5b8c1cf52a95894a584314ccd8900f5",
+          "message": "Fix wave tracing for cocotb (#1038)",
+          "timestamp": "2026-08-05T15:07:27+02:00",
+          "tree_id": "44ce5735b815860f7ee6bef0cb5cdbd8e275f785",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/4f11685cc5b8c1cf52a95894a584314ccd8900f5"
+        },
+        "date": 1785938841425,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 48.69,
+            "unit": "MHz"
+          },
+          {
+            "name": "LUT used",
+            "value": 17297,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUT DFF used",
+            "value": 7970,
+            "unit": "LUT"
+          },
+          {
+            "name": "Slice carry chains used",
+            "value": 304,
+            "unit": "Slice"
+          },
+          {
+            "name": "Block RAM used",
+            "value": 3,
             "unit": "RAMB36E1"
           }
         ]
