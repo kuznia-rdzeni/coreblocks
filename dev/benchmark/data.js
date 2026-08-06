311f7a32cba9e43db56c5961c40e5487a6289077
github-action-benchmark <github@users.noreply.github.com>
2026-08-06 14:26:26 +0000

    add Fmax and LCs (basic, ECP5) (customBiggerIsBetter) benchmark result for 6c5344202f54b4193af9dc257103fcac2267ba11

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786026383766,
+  "lastUpdate": 1786026385871,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -53462,6 +53462,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as DFF: (ECP5)",
             "value": 8170,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6c5344202f54b4193af9dc257103fcac2267ba11",
+          "message": "SimpleTestCircuit in bus test (#1048)",
+          "timestamp": "2026-08-06T15:18:55+02:00",
+          "tree_id": "bd4115c8f8aad2b4dc705a640847d645d40dd298",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/6c5344202f54b4193af9dc257103fcac2267ba11"
+        },
+        "date": 1786026385458,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 62.95,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 7321,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 556,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1224,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 2711,
             "unit": "LUT"
           }
         ]
