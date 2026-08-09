3dd63ab2f6ef3602ea148c4f209eada9b36d7cf6
github-action-benchmark <github@users.noreply.github.com>
2026-08-09 09:07:54 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 56bf09a6eb8df753ed0192576d544d8d9021c5fb

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786178223020,
+  "lastUpdate": 1786266474655,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -26453,6 +26453,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/b19400ae56b7007fd24d8fc96589466482151939"
         },
         "date": 1786178194472,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.44844731541673366,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.5951740300321152,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.3661826587675101,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.6450701307924483,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.35531157138749,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.30097743800508947,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.3402741852043456,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.4437417112544016,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "56bf09a6eb8df753ed0192576d544d8d9021c5fb",
+          "message": "Update riscv arch test (#1047)",
+          "timestamp": "2026-08-09T10:10:01+02:00",
+          "tree_id": "ee0e09a4859f24d618c9bac2d3aa07856e87c092",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/56bf09a6eb8df753ed0192576d544d8d9021c5fb"
+        },
+        "date": 1786266463844,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
