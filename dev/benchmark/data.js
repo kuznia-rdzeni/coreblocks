7896aad617e95b3582e25a06ea8772158dce888a
github-action-benchmark <github@users.noreply.github.com>
2026-08-14 11:09:34 +0000

    add Fmax and LCs (full, ECP5) (customBiggerIsBetter) benchmark result for b36d732ee51e0660c1a08b1865832d39e1df80e7

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786705772082,
+  "lastUpdate": 1786705774436,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -73431,6 +73431,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Device utilisation: (ECP5)",
             "value": 62334,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 4084,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 3428,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 13754,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b36d732ee51e0660c1a08b1865832d39e1df80e7",
+          "message": "make SSI edge triggered, as defined in ISA (#1058)",
+          "timestamp": "2026-08-14T12:09:06+02:00",
+          "tree_id": "d6161127cc9923c2a737db9634c0c53a317dc9ed",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/b36d732ee51e0660c1a08b1865832d39e1df80e7"
+        },
+        "date": 1786705773771,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 39.33,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 60279,
             "unit": "LUT4"
           },
           {
