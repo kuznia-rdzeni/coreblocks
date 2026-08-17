a4363466c32b856d1d4efbb9dad8809bb277e3f8
github-action-benchmark <github@users.noreply.github.com>
2026-08-17 09:00:08 +0000

    add Fmax and LCs (tiny, XC7K480T) (customBiggerIsBetter) benchmark result for 603d60d04ea4baa1cfdcd23f448456f4d7c5c921

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786957205856,
+  "lastUpdate": 1786957208632,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -77781,6 +77781,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUT used",
             "value": 7140,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUT DFF used",
+            "value": 2758,
+            "unit": "LUT"
+          },
+          {
+            "name": "Slice carry chains used",
+            "value": 126,
+            "unit": "Slice"
+          },
+          {
+            "name": "Block RAM used",
+            "value": 0,
+            "unit": "RAMB36E1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "piotro@piotro.eu",
+            "name": "Aria Węgrzyn",
+            "username": "awariac"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "603d60d04ea4baa1cfdcd23f448456f4d7c5c921",
+          "message": "Change all shebangs to /usr/bin/env (#1064)",
+          "timestamp": "2026-08-17T10:00:15+02:00",
+          "tree_id": "502eb36ffb87df89fd19c7b889b10d436dfb7d77",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/603d60d04ea4baa1cfdcd23f448456f4d7c5c921"
+        },
+        "date": 1786957207922,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 74.02,
+            "unit": "MHz"
+          },
+          {
+            "name": "LUT used",
+            "value": 7175,
             "unit": "LUT"
           },
           {
