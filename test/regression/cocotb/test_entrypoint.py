424369953ad95fb92aca77be345c5bce73c7b8cd
Jakub Janeczko <kuba@ev1.pl>
2026-08-18 04:02:44 +0200

    format

--- test/regression/cocotb/test_entrypoint.py
+++ test/regression/cocotb/test_entrypoint.py
@@ -2,7 +2,9 @@ from test.regression.cocotb import CocotbSimulation, generate_tests
 from test.regression.test_regression import run_test
 from test.regression.conftest import get_all_test_names
 
+
 async def do_test(dut, test_name):
     await run_test(CocotbSimulation(dut), test_name)
+
 
 generate_tests(do_test, list(get_all_test_names()))
