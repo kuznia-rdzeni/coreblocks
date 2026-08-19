1db47e9532200dc94b565e82223d85865b689f1f
github-action-benchmark <github@users.noreply.github.com>
2026-08-19 07:52:35 +0000

    add Fmax and LCs (tiny, ECP5) (customBiggerIsBetter) benchmark result for 075dbc29727a9e664eb4c3d3fd714b40ba7ad4e1

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787125952455,
+  "lastUpdate": 1787125955002,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -77834,6 +77834,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as ram: (ECP5)",
             "value": 1096,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 2762,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "075dbc29727a9e664eb4c3d3fd714b40ba7ad4e1",
+          "message": "frontend: decouple checkpoint creation from instruction type (#1067)",
+          "timestamp": "2026-08-19T08:58:08+02:00",
+          "tree_id": "00449346cca25c0ff2b0daa34f1abdaf3c4c5c1c",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/075dbc29727a9e664eb4c3d3fd714b40ba7ad4e1"
+        },
+        "date": 1787125954285,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 57.63,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 7553,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 694,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1092,
             "unit": "LUT"
           },
           {
