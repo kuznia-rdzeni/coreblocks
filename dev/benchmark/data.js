78263b9878a85fcd51c1ccc8fa9d590497fff2f5
github-action-benchmark <github@users.noreply.github.com>
2026-08-26 08:44:10 +0000

    add Fmax and LCs (full, ECP5) (customBiggerIsBetter) benchmark result for 43b52c84715a41b0574f64c6a315b5156d9cc26f

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787733847977,
+  "lastUpdate": 1787733850733,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -75699,6 +75699,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Device utilisation: (ECP5)",
             "value": 60598,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 4234,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 3628,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 14262,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43b52c84715a41b0574f64c6a315b5156d9cc26f",
+          "message": "restore RVVI functionality (#1074)",
+          "timestamp": "2026-08-26T09:43:16+02:00",
+          "tree_id": "11811b802df926d654c987724a1bd5a3b9d7c4b1",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/43b52c84715a41b0574f64c6a315b5156d9cc26f"
+        },
+        "date": 1787733849854,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 40.26,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 62368,
             "unit": "LUT4"
           },
           {
