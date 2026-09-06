c7722798b66888f6ab75bddf66c8b4734d0bcc3f
github-action-benchmark <github@users.noreply.github.com>
2026-09-06 23:37:06 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 0c73d759ca3c222303b6bca74691d3bd09ff1049

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788726052354,
+  "lastUpdate": 1788737826102,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -28117,6 +28117,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/38064efbde0d07b8a90153abe87c14276f4b5e73"
         },
         "date": 1788726018901,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.5817876108550312,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.6332168747017992,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.5223000987166831,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.6621861757871236,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.39366855929087863,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.48412786764516985,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.49734006643082823,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.5340697687218607,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c73d759ca3c222303b6bca74691d3bd09ff1049",
+          "message": "Align regression ELF segments to 64B (#1090)",
+          "timestamp": "2026-09-07T00:36:14+02:00",
+          "tree_id": "e6a20bddf95b80a929ea37fce2bf3c1b8a9a048c",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/0c73d759ca3c222303b6bca74691d3bd09ff1049"
+        },
+        "date": 1788737813261,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
