0b6f72d184f7f25c1e85810b995ccd6b059ef85f
github-action-benchmark <github@users.noreply.github.com>
2026-09-06 23:37:09 +0000

    add Fmax and LCs (tiny, ECP5) (customBiggerIsBetter) benchmark result for 0c73d759ca3c222303b6bca74691d3bd09ff1049

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788737826102,
+  "lastUpdate": 1788737828897,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -83386,6 +83386,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Device utilisation: (ECP5)",
             "value": 9577,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 728,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1228,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 3383,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0c73d759ca3c222303b6bca74691d3bd09ff1049",
+          "message": "Align regression ELF segments to 64B (#1090)",
+          "timestamp": "2026-09-07T00:36:14+02:00",
+          "tree_id": "e6a20bddf95b80a929ea37fce2bf3c1b8a9a048c",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/0c73d759ca3c222303b6bca74691d3bd09ff1049"
+        },
+        "date": 1788737828087,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 55.77,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 9515,
             "unit": "LUT4"
           },
           {
