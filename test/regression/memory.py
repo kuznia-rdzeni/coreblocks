0c73d759ca3c222303b6bca74691d3bd09ff1049
Jakub Urbańczyk <xthaid@gmail.com>
2026-09-06 23:36:14 +0100

    Align regression ELF segments to 64B (#1090)

--- test/regression/memory.py
+++ test/regression/memory.py
@@ -4,9 +4,6 @@ from dataclasses import dataclass
 from elftools.elf.constants import P_FLAGS
 from elftools.elf.elffile import ELFFile, Segment
 
-from coreblocks.params.configurations import CoreConfiguration
-from transactron.utils import align_to_power_of_two, align_down_to_power_of_two
-
 __all__ = [
     "ReplyStatus",
     "SegmentFlags",
@@ -111,7 +108,6 @@ class CoreMemoryModel:
 def load_segment(
     segment: Segment,
     *,
-    do_workarounds: bool = True,
     disable_write_protection: bool = False,
     force_executable: bool = False,
 ) -> RandomAccessMemory:
@@ -121,6 +117,9 @@ def load_segment(
 
     seg_start = paddr
     seg_end = paddr + memsz
+
+    assert seg_start % 0x40 == 0, f"Segment start {seg_start:#x} is not 64-byte-aligned"
+    assert seg_end % 0x40 == 0, f"Segment end {seg_end:#x} is not 64-byte-aligned"
 
     data = segment.data()
 
@@ -135,32 +134,12 @@ def load_segment(
     if flags_raw & P_FLAGS.PF_X or force_executable:
         flags |= SegmentFlags.EXECUTABLE
 
-    if do_workarounds:
-        config = CoreConfiguration()
-        if flags & SegmentFlags.EXECUTABLE:
-            # align instruction section to full icache lines
-            align_bits = config.icache_line_bytes_log
-            # workaround for fetching/stalling issue
-            extend_end = 2**config.icache_line_bytes_log
-        else:
-            align_bits = 0
-            extend_end = 0
-
-        align_data_front = seg_start - align_down_to_power_of_two(seg_start, align_bits)
-        align_data_back = align_to_power_of_two(seg_end, align_bits) - seg_end + extend_end
-
-        data = b"\x00" * align_data_front + data + b"\x00" * align_data_back
-
-        seg_start = align_down_to_power_of_two(seg_start, align_bits)
-        seg_end = align_to_power_of_two(seg_end, align_bits) + extend_end
-
     return RandomAccessMemory(range(seg_start, seg_end), flags, data)
 
 
 def load_segments_from_elf(
     file_path: str,
     *,
-    do_workarounds: bool = True,
     disable_write_protection: bool = False,
     force_executable: bool = False,
 ) -> list[RandomAccessMemory]:
@@ -174,7 +153,6 @@ def load_segments_from_elf(
             segments.append(
                 load_segment(
                     segment,
-                    do_workarounds=do_workarounds,
                     disable_write_protection=disable_write_protection,
                     force_executable=force_executable,
                 )
