4e317ba9fb8e51fba5ace7fa1d743aea96ecd35f
github-action-benchmark <github@users.noreply.github.com>
2026-09-07 12:16:44 +0000

    add Fmax and LCs (basic, ECP5) (customBiggerIsBetter) benchmark result for 2241c65c240fe87c49e52ccddc856981b7659bd0

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788783401747,
+  "lastUpdate": 1788783404006,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -56612,6 +56612,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 1742,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 2356,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 9295,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2241c65c240fe87c49e52ccddc856981b7659bd0",
+          "message": " regression: split the simulation framework from the suites which use it  (#1085)",
+          "timestamp": "2026-09-07T13:15:04+02:00",
+          "tree_id": "6e8b7408852a3720924361a38f1fd34e5c49c2c5",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/2241c65c240fe87c49e52ccddc856981b7659bd0"
+        },
+        "date": 1788783403500,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 46.07,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 27693,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1746,
             "unit": "LUT"
           },
           {
