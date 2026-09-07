40150ef0d4721f3c6dae6334c5c0b054ff5a4f92
github-action-benchmark <github@users.noreply.github.com>
2026-09-07 12:19:08 +0000

    add Fmax and LCs (tiny, ECP5) (customBiggerIsBetter) benchmark result for 2e7cb2e7eacaf1dd384cca16df7402bfa4f265c6

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788783545068,
+  "lastUpdate": 1788783548435,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -84318,6 +84318,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 724,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 1228,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 3383,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34948061+lekcyjna123@users.noreply.github.com",
+            "name": "lekcyjna123",
+            "username": "lekcyjna123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2e7cb2e7eacaf1dd384cca16df7402bfa4f265c6",
+          "message": "Fix typo in licence, align with EU template. (#1092)",
+          "timestamp": "2026-09-07T13:19:05+02:00",
+          "tree_id": "b4b346a971e3dcf1a920c2d82a07b835450977ce",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/2e7cb2e7eacaf1dd384cca16df7402bfa4f265c6"
+        },
+        "date": 1788783547761,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 59.33,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 9423,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 728,
             "unit": "LUT"
           },
           {
