5e49f8b72eaf07aecbd258ece067f400315402af
github-action-benchmark <github@users.noreply.github.com>
2026-09-10 12:09:27 +0000

    add Fmax and LCs (basic, XC7K480T) (customBiggerIsBetter) benchmark result for 036faec5f67405a890eb357af337c3e2527b3aca

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789042164615,
+  "lastUpdate": 1789042167507,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -80522,6 +80522,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUT DFF used",
             "value": 9084,
+            "unit": "LUT"
+          },
+          {
+            "name": "Slice carry chains used",
+            "value": 380,
+            "unit": "Slice"
+          },
+          {
+            "name": "Block RAM used",
+            "value": 3,
+            "unit": "RAMB36E1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "036faec5f67405a890eb357af337c3e2527b3aca",
+          "message": "arch: narrow CfiType to the CFI type alone & add return-address-stack action (#1096)",
+          "timestamp": "2026-09-10T13:07:31+02:00",
+          "tree_id": "7415e299ef2be68209c9140d1e7d47b27217d223",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/036faec5f67405a890eb357af337c3e2527b3aca"
+        },
+        "date": 1789042166696,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 46.49,
+            "unit": "MHz"
+          },
+          {
+            "name": "LUT used",
+            "value": 20447,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUT DFF used",
+            "value": 9076,
             "unit": "LUT"
           },
           {
