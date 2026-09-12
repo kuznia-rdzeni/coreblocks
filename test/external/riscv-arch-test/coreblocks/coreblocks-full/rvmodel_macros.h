dae3b4cd62cc7ba198e57fbcc74b4c79eaa28948
Jakub Janeczko <kuba@ev1.pl>
2026-09-12 10:18:00 +0200

    update more

--- test/external/riscv-arch-test/coreblocks/coreblocks-full/rvmodel_macros.h
+++ test/external/riscv-arch-test/coreblocks/coreblocks-full/rvmodel_macros.h
@@ -105,16 +105,4 @@
   lw _R1, 0(_R2);                                                           \
   sw _R1, 0(_R2);
 
-#define RVMODEL_SET_MSW_INT(_R1, _R2)             \
-  li _R1, 1                                      ;\
-  li _R2, RVMODEL_MSIP_ADDRESS                   ;\
-  sw _R1, 0(_R2)
-
-#define RVMODEL_CLR_MSW_INT(_R1, _R2)             \
-  li _R2, RVMODEL_MSIP_ADDRESS                   ;\
-  sw zero, 0(_R2)
-
-#define RVMODEL_SET_SSW_INT(_R1, _R2)
-#define RVMODEL_CLR_SSW_INT(_R1, _R2)
-
 #endif // _RVMODEL_MACROS_H
