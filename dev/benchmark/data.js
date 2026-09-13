6dc3c32be4386db9e7d50d596daecb09c24199e3
github-action-benchmark <github@users.noreply.github.com>
2026-09-13 15:21:45 +0000

    add Fmax and LCs (full, ECP5) (customBiggerIsBetter) benchmark result for 8414befa34fcdb4e4d6d32876be3db1bf86f4688

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789312902864,
+  "lastUpdate": 1789312905067,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -78306,6 +78306,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as DFF: (ECP5)",
             "value": 16774,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Janeczko Jakub",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8414befa34fcdb4e4d6d32876be3db1bf86f4688",
+          "message": "add verilator control (#1100)",
+          "timestamp": "2026-09-13T15:56:14+02:00",
+          "tree_id": "b28fcf4229f30924464f960d07b320121746e765",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/8414befa34fcdb4e4d6d32876be3db1bf86f4688"
+        },
+        "date": 1789312904437,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 35.84,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 68620,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 4570,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 3740,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 17693,
             "unit": "LUT"
           }
         ]
