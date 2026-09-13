3325c71e6ccd72e7097b01b0b380b982e71a89e1
github-action-benchmark <github@users.noreply.github.com>
2026-09-13 16:50:49 +0000

    add Fmax and LCs (full, XC7K480T) (customBiggerIsBetter) benchmark result for 0d6fc2d4d2b4b28fedcfd00175b3f30fa2784082

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789318246821,
+  "lastUpdate": 1789318248892,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -83857,6 +83857,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Slice carry chains used",
             "value": 1084,
+            "unit": "Slice"
+          },
+          {
+            "name": "Block RAM used",
+            "value": 5,
+            "unit": "RAMB36E1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0d6fc2d4d2b4b28fedcfd00175b3f30fa2784082",
+          "message": "Use verilator from dockerhub (#1103)",
+          "timestamp": "2026-09-13T14:58:29+02:00",
+          "tree_id": "edb1be3ab4b062d887589397a5bc623427a7d3a0",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/0d6fc2d4d2b4b28fedcfd00175b3f30fa2784082"
+        },
+        "date": 1789318248395,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 43.55,
+            "unit": "MHz"
+          },
+          {
+            "name": "LUT used",
+            "value": 53269,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUT DFF used",
+            "value": 17746,
+            "unit": "LUT"
+          },
+          {
+            "name": "Slice carry chains used",
+            "value": 1079,
             "unit": "Slice"
           },
           {
