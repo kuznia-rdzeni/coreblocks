77963c1ef56ba4b6b0fca47bc9fa11fe0930ec12
github-action-benchmark <github@users.noreply.github.com>
2026-09-14 23:19:57 +0000

    add Fmax and LCs (tiny, XC7K480T) (customBiggerIsBetter) benchmark result for b649321ccb7e8cbf73681cd55fb6111b58230ce5

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789427994877,
+  "lastUpdate": 1789427997041,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -88791,6 +88791,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Slice carry chains used",
             "value": 154,
+            "unit": "Slice"
+          },
+          {
+            "name": "Block RAM used",
+            "value": 0,
+            "unit": "RAMB36E1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b649321ccb7e8cbf73681cd55fb6111b58230ce5",
+          "message": "sim: a simulation backend written in C++ (#1082)",
+          "timestamp": "2026-09-15T00:15:52+02:00",
+          "tree_id": "9e16e60c0b1a48aa0185240cbd60bf7afc88731d",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/b649321ccb7e8cbf73681cd55fb6111b58230ce5"
+        },
+        "date": 1789427996444,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 66.6,
+            "unit": "MHz"
+          },
+          {
+            "name": "LUT used",
+            "value": 8666,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUT DFF used",
+            "value": 3421,
+            "unit": "LUT"
+          },
+          {
+            "name": "Slice carry chains used",
+            "value": 153,
             "unit": "Slice"
           },
           {
