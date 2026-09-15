1df9486dece3d225807b0a20c9252a1319e69b02
github-action-benchmark <github@users.noreply.github.com>
2026-09-15 14:06:08 +0000

    add Performance (IPC) (customBiggerIsBetter) benchmark result for 844951aee360fa22c8a64aa4c1f84cc2123ca5cd

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789428001860,
+  "lastUpdate": 1789481168670,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -28821,6 +28821,70 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/b649321ccb7e8cbf73681cd55fb6111b58230ce5"
         },
         "date": 1789427977607,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "aha-mont64",
+            "value": 0.5619359814886232,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "crc32",
+            "value": 0.7811574390521759,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "minver",
+            "value": 0.5749540875650653,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nettle-sha256",
+            "value": 0.6851633586346707,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "nsichneu",
+            "value": 0.3937007230687528,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "slre",
+            "value": 0.503692946427776,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "statemate",
+            "value": 0.513953228777139,
+            "unit": "Instructions Per Cycle"
+          },
+          {
+            "name": "ud",
+            "value": 0.5251243363765755,
+            "unit": "Instructions Per Cycle"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "844951aee360fa22c8a64aa4c1f84cc2123ca5cd",
+          "message": "Increase synthesis benchmark time limit (#1107)",
+          "timestamp": "2026-09-15T14:49:34+02:00",
+          "tree_id": "e662def5d9a70c6055a0ec3cea5c68ca684dd34d",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/844951aee360fa22c8a64aa4c1f84cc2123ca5cd"
+        },
+        "date": 1789481155206,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
