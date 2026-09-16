a0faa730b82ab14809bf9c7f5a6ef23692e29b6a
github-action-benchmark <github@users.noreply.github.com>
2026-09-16 22:20:26 +0000

    add Fmax and LCs (basic, ECP5) (customBiggerIsBetter) benchmark result for 68a88ffe46b49eedf220c17291d63bccd9471904

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789597223459,
+  "lastUpdate": 1789597225936,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -58081,6 +58081,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "LUTs used as carry: (ECP5)",
             "value": 1746,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as ram: (ECP5)",
+            "value": 2416,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUTs used as DFF: (ECP5)",
+            "value": 9426,
+            "unit": "LUT"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xthaid@gmail.com",
+            "name": "Jakub Urbańczyk",
+            "username": "xThaid"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "68a88ffe46b49eedf220c17291d63bccd9471904",
+          "message": "frontend/bpu: introduce a uniform predictor component protocol (#1105)",
+          "timestamp": "2026-09-16T23:01:28+02:00",
+          "tree_id": "70ddf51a86ec7f889d420222d6f21c2b5f272da8",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/68a88ffe46b49eedf220c17291d63bccd9471904"
+        },
+        "date": 1789597225266,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 49.45,
+            "unit": "MHz"
+          },
+          {
+            "name": "Device utilisation: (ECP5)",
+            "value": 29846,
+            "unit": "LUT4"
+          },
+          {
+            "name": "LUTs used as carry: (ECP5)",
+            "value": 1742,
             "unit": "LUT"
           },
           {
