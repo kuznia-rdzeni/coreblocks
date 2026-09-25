83b102520b4e25d6b15031f22e144ca5bd0b4c15
github-action-benchmark <github@users.noreply.github.com>
2026-09-25 16:00:53 +0000

    add Fmax and LCs (tiny, XC7K480T) (customBiggerIsBetter) benchmark result for 2a62950d0a01af3b0e55eaabfd040d2d95f5c53d

--- dev/benchmark/data.js
+++ dev/benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790352051064,
+  "lastUpdate": 1790352053413,
   "repoUrl": "https://github.com/kuznia-rdzeni/coreblocks",
   "entries": {
     "Performance (IPC)": [
@@ -91881,6 +91881,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Slice carry chains used",
             "value": 160,
+            "unit": "Slice"
+          },
+          {
+            "name": "Block RAM used",
+            "value": 0,
+            "unit": "RAMB36E1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kuba@ev1.pl",
+            "name": "Jakub Janeczko",
+            "username": "qbojj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2a62950d0a01af3b0e55eaabfd040d2d95f5c53d",
+          "message": "fix double shadow (#1116)",
+          "timestamp": "2026-09-25T16:56:58+02:00",
+          "tree_id": "f463f1a014b3f50c1b00d292dde29b2c2e44c7c9",
+          "url": "https://github.com/kuznia-rdzeni/coreblocks/commit/2a62950d0a01af3b0e55eaabfd040d2d95f5c53d"
+        },
+        "date": 1790352052793,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Max clock frequency (Fmax)",
+            "value": 67.28,
+            "unit": "MHz"
+          },
+          {
+            "name": "LUT used",
+            "value": 9054,
+            "unit": "LUT"
+          },
+          {
+            "name": "LUT DFF used",
+            "value": 3664,
+            "unit": "LUT"
+          },
+          {
+            "name": "Slice carry chains used",
+            "value": 158,
             "unit": "Slice"
           },
           {
